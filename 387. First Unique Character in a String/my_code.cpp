606a83184296158ac426754eb95f6250afcc4274
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-01-22 20:45:48 +0300

    Create my_code.cpp

---
+++ 387. First Unique Character in a String/my_code.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+  const int power_English_alphabet = 26;
+	array<int, power_English_alphabet> char_to_count;
+	char_to_count.fill(0);
+
+	for (const char chr : s) {
+		++char_to_count[chr - 'a'];
+	}
+
+	for (int i = 0; i < s.size(); ++i) {
+        // s[i] - 'a' == chr - 'a'
+		if (char_to_count[s[i] - 'a'] == 1) { return i; }
+	}
+
+	return -1;
+    }
+};
