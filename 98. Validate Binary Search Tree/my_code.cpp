412f5b58c94132b39efa9387cc072cdf89254a58
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-06-28 21:50:56 +0300

    Update my_code.cpp

--- 98. Validate Binary Search Tree/my_code.cpp
+++ 98. Validate Binary Search Tree/my_code.cpp
@@ -12,12 +12,12 @@ private:
         if(root == nullptr){return true;} // если root == nullptr, значит, дошли до листа, при это ветвь дерева,
                                           //  по которой дошли до леста, удовлетворяет требованиям BST, поэтому возвращаем истину
 
-        if(root->val >= LocalMax || root->val <= LocalMin){return false;}
+        if(root->val >= LocalMax || root->val <= LocalMin){return false;} // проверка на НЕПРАВИЛЬНЫЕ требования BST
         
         bool f = CheckDefinition(root->left, root->val, LocalMin); // начало обхода левого поддерева
         if(!f){return f;}
         bool s = CheckDefinition(root->right,LocalMax, root->val); // начало обхода правого поддерева
-        return s;
+        return s; 
     }
 
 
