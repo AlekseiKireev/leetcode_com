4a8cedb83afa6dbaffd11d29d0f51fe2eb2e47ab
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-06-28 21:47:19 +0300

    Update my_code.cpp

--- 98. Validate Binary Search Tree/my_code.cpp
+++ 98. Validate Binary Search Tree/my_code.cpp
@@ -2,21 +2,22 @@ class Solution {
 
 private:
 
+    // функция, на основе preorder-обхода
     bool CheckDefinition(
     TreeNode* root, 
     long long LocalMax = numeric_limits<long long>::max(), 
     long long LocalMin = numeric_limits<long long>::min()
                         ){
         
-        if(root == nullptr){return true;}
-
+        if(root == nullptr){return true;} // если root == nullptr, значит, дошли до листа, при это ветвь дерева,
+                                          //  по которой дошли до леста, удовлетворяет требованиям BST, поэтому возвращаем истину
 
         if(root->val >= LocalMax || root->val <= LocalMin){return false;}
         
-        bool f = CheckDefinition(root->left, root->val, LocalMin);
+        bool f = CheckDefinition(root->left, root->val, LocalMin); // начало обхода левого поддерева
         if(!f){return f;}
-        bool s = CheckDefinition(root->right,LocalMax, root->val);
-        return f * s;
+        bool s = CheckDefinition(root->right,LocalMax, root->val); // начало обхода правого поддерева
+        return s;
     }
 
 
