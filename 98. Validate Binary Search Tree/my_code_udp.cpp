2e01ca91da0cbf73e59397c4bc69b1c9be716924
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-06-29 00:09:03 +0300

    Create my_code_udp.cpp

---
+++ 98. Validate Binary Search Tree/my_code_udp.cpp
@@ -0,0 +1,29 @@
+class Solution {
+
+private:
+
+    bool CheckDefinition(
+    TreeNode* root, 
+    TreeNode* LocalMax = nullptr, 
+    TreeNode* LocalMin = nullptr
+                        ){
+        
+        if(root == nullptr){return true;}
+
+
+        if(LocalMax && root->val >= LocalMax->val){return false;}
+        if(LocalMin && root->val <= LocalMin->val){return false;}
+        
+        bool f = CheckDefinition(root->left, root, LocalMin);
+        if(!f){return f;}
+        bool s = CheckDefinition(root->right,LocalMax, root);
+        return s;
+    }
+
+
+public:
+    bool isValidBST(TreeNode* root) {
+        return CheckDefinition(root);
+
+    }
+};
