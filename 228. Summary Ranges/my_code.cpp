277a8009e6660a51956537f13b5e14d6a0c5f51d
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-06-30 15:55:18 +0300

    Update my_code.cpp

--- 228. Summary Ranges/my_code.cpp
+++ 228. Summary Ranges/my_code.cpp
@@ -6,7 +6,7 @@ vector<string> summaryRanges(const vector<int>& nums) {
  
     for (int i = 0; i < nums.size(); ++i) {
  
-        int start_ranges = nums[i];
+        const int start_ranges = nums[i];
         //  nums[i] < nums[i+1] --> в 1 + nums[i] переполнения не будет, в то
         // время как в nums[i + 1]  - nums[i]  будет 
         while (i + 1 < nums.size() && nums[i + 1] == 1 + nums[i] ) { ++i; } 
