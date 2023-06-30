49d5c426b9c0eb147477b3c0af4abc6ecf6ccccc
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-06-30 19:10:00 +0300

    Update my_code_with_move_stringstream.cpp

--- 557. Reverse Words in a String III/my_code_with_move_stringstream.cpp
+++ 557. Reverse Words in a String III/my_code_with_move_stringstream.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        stringstream ss(move(s));
-        
+        stringstream ss(move(s));    
 
         string ans; ans.reserve(s.size());
         string buffer;
