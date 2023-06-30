9940561f9744575b56050be6cace67099f34de55
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-06-30 05:28:54 +0300

    Create my_code_Inorder.cpp

---
+++ 98. Validate Binary Search Tree/my_code_Inorder.cpp
@@ -0,0 +1,30 @@
+class Solution {
+
+private:
+
+    long long prev_node = numeric_limits<long long>::min(); // == самый левый узел, именно так можно было бы задать ему значение по умолчанию, 
+                                                            // дабы избежать использования numeric_limits, ибо если бы у val был бы тип не int а long long, 
+                                                            // то так бы делать было нельзя
+
+    bool CheckDefinition(
+    TreeNode* root){
+        
+        if(root == nullptr){return true;}
+        
+        bool f = CheckDefinition(root->left);
+        if(!f){return f;}
+
+        if(prev_node >= root->val){return false;}
+        else{prev_node = root->val;}
+
+        bool s = CheckDefinition(root->right);
+        return s;
+    }
+
+
+public:
+    bool isValidBST(TreeNode* root) {
+        return CheckDefinition(root);
+
+    }
+};
