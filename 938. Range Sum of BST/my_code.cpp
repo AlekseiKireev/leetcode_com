a3732d18b6b64b81ecb12881c81e99f2be860e24
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-07-02 01:07:34 +0300

    Create my_code.cpp

---
+++ 938. Range Sum of BST/my_code.cpp
@@ -0,0 +1,22 @@
+class Solution {
+
+    private:
+
+    int ans = 0;
+public:
+
+    void Inorder(TreeNode* root, int low, int high){
+
+        if(root == nullptr){return;}
+
+        Inorder(root->left, low, high);
+        if(low<= root->val && root->val <= high){ans+= root->val;}
+        Inorder(root->right, low, high);
+    }
+
+    int rangeSumBST(TreeNode* root, int low, int high) {
+        
+        Inorder(root, low, high);
+        return ans;
+    }
+};
