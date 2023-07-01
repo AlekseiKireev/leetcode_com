f5ce2b91af8e05bdd802c0e46679397bf0e58996
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-07-01 12:32:57 +0300

    Create my_code.cpp

---
+++ 101. Symmetric Tree/my_code.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+
+    bool ChechSymmetricTree(TreeNode* LeftSubTree, TreeNode* RightSubTree){
+
+        if(LeftSubTree == nullptr && RightSubTree == nullptr){return true;}
+
+        if(LeftSubTree == nullptr || RightSubTree == nullptr){return false;}
+
+        if(LeftSubTree->val != RightSubTree->val){return false;}
+
+        //equiv return ChechSymmetricTree(LeftSubTree->left, RightSubTree->right) && ChechSymmetricTree(LeftSubTree->right, RightSubTree->left);
+        const bool f = ChechSymmetricTree(LeftSubTree->left, RightSubTree->right);
+        if(!f){return f;}
+        const bool s = ChechSymmetricTree(LeftSubTree->right, RightSubTree->left);
+        return s;
+    }
+
+
+    bool isSymmetric(TreeNode* root) {
+        
+        if(root == nullptr){return true;}
+
+        return ChechSymmetricTree(root->left, root->right);
+    }
+};
