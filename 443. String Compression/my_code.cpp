9b5e60af4542361a9b9d025a33a129b8541e7bbe
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-07-02 13:35:31 +0300

    Update my_code.cpp

--- 443. String Compression/my_code.cpp
+++ 443. String Compression/my_code.cpp
@@ -10,22 +10,22 @@ public:
             char prev_char = chars[i]; 
             chars[NewLength] = prev_char;
 
-            // count -- количество повторяющихся символов prev_char, которые расположены в диапозоне [i, j] : i <= j
-            int count = 1; // 1, так как учитываем prev_char
+            // CountRepeat -- количество повторяющихся символов prev_char, которые расположены в диапозоне [i, j] : i <= j
+            int CountRepeat = 1; // 1, так как учитываем prev_char
             ++NewLength; // инкрементируем, так как учитываем prev_char
 
-
+            // обновление CountRepeat
             while(i+1 < chars.size() && prev_char == chars[i+1]){
                 
-                ++count;
+                ++CountRepeat;
                 ++i;
                 
             }
 
 
-            if(count > 1){
+            if(CountRepeat > 1){
 
-                for(const char ch : to_string(count)){
+                for(const char ch : to_string(CountRepeat)){
                     
                     chars[NewLength] = ch;
                     ++NewLength;
