1d9294782f3f1de981a0be888e5755bc0de010ab
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-07-03 00:52:17 +0300

    Update my_code_MVP.cpp

--- 470. Implement Rand10() Using Rand7()/my_code_MVP.cpp
+++ 470. Implement Rand10() Using Rand7()/my_code_MVP.cpp
@@ -1,3 +1,3 @@
     int rand10() {
-        return 1 + (rand7() + rand7()+ rand7()+ rand7()+ rand7())%10;
+        return 1 + (rand7() + rand7()+ rand7()+ rand7() + rand7())%10; // если сумм rand7() будет меньше -- будет ошибка!
     }
