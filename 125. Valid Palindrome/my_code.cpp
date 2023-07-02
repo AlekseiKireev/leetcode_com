829d2e27c801ae1695d1751d911ddcd4761dc6a5
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-07-02 14:42:59 +0300

    Create my_code.cpp

---
+++ 125. Valid Palindrome/my_code.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+bool isPalindrome(string s) {
+
+	int l = 0;
+	int r = s.size() - 1;
+
+	while (l < r) {
+
+    // отрбрасываем 
+		while (l < r && !isalnum(s[l])) {++l;}
+
+		while (l < r && !isalnum(s[r])) {--r;}
+
+    /* // equiv:
+
+    for(; l < r && !isalnum(s[l]); ++l);
+
+		for(; l < r && !isalnum(s[r]); --r);
+    
+    */
+		if (tolower(s[l]) != tolower(s[r])) { return false; }
+		++l; --r;
+
+	}
+
+	return true;
+}
+};
