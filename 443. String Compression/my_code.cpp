9f68ad237224891e85cc904fd9334eb14c184b26
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-07-02 13:26:42 +0300

    Update my_code.cpp

--- 443. String Compression/my_code.cpp
+++ 443. String Compression/my_code.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
     int compress(vector<char>& chars) {
-  
+
+        // искомое
         int NewLength = 0;
 
         for(int i = 0; i < chars.size() ; ++i){
 
             char prev_char = chars[i]; 
             chars[NewLength] = prev_char;
-            
+
+            // count -- количество повторяющихся символов prev_char, которые расположены в диапозоне [i, j] : i <= j
             int count = 1; // 1, так как учитываем prev_char
             ++NewLength; // инкрементируем, так как учитываем prev_char
 
