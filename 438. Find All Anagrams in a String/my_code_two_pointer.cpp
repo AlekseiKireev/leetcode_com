7e1ea042ee695bbece4cdd944205f4d7a7b0fa60
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-07-04 17:29:52 +0300

    Update my_code_two_pointer.cpp

--- 438. Find All Anagrams in a String/my_code_two_pointer.cpp
+++ 438. Find All Anagrams in a String/my_code_two_pointer.cpp
@@ -1,1 +1,27 @@
+class Solution {
+ public:
+  vector<int> findAnagrams(string s, string p) {
+    
+    vector<int> count(128);
+    for (const char c : p) {++count[c];}
 
+
+    int required = p.length();
+    vector<int> ans;
+
+    for (int l = 0, r = 0; r < s.length(); ++r) {
+
+      if (--count[s[r]] >= 0) {--required;}
+      
+      while (required == 0) {
+
+        if (r - l + 1 == p.length()) {ans.push_back(l);}
+
+        if (++count[s[l++]] > 0) {++required;}
+        
+      }
+    }
+
+    return ans;
+  }
+};
