d4b04c6a739cd9c11f9fc7618835cb7db2a4dd73
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-07-04 15:25:09 +0300

    Create my_code.cpp

---
+++ 438. Find All Anagrams in a String/my_code.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+
+    //  p == Angr == Anagram
+    vector<int> findAnagrams(string S, string Angr) {
+        
+        int S_len = S.size();
+        int Angr_len = Angr.size();
+
+        vector<int> CharSToCount(26, 0); // == window
+        vector<int> CharAngrToCount(26, 0); 
+
+        for(int i = 0; i < Angr_len; ++i){
+            // s and p consist of lowercase English letters.
+            
+        }
+
+    }
+};
