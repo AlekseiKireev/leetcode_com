bea4a4a75d22435673dd366fe97155bdc84fb156
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-07-05 20:18:32 +0300

    Create 22b.cpp

---
+++ 22. Generate Parentheses/22b.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+
+const int ZERO = []() { 
+ ios_base::sync_with_stdio(false);
+ cin.tie(nullptr);
+ return 0;
+}();
+
+void generateParentheses(vector<char>& cur, int toOpen, int toClose, vector<string>& ans) {
+
+    if (toOpen == 0 && toClose == 0) {
+        ans.push_back(string(cur.begin(), cur.end()));
+        return;
+    }
+    if (toOpen > 0) {
+        cur.push_back('(');
+        generateParentheses(cur, toOpen - 1, toClose + 1, ans);
+        cur.pop_back();
+    }
+    if (toClose > 0) {
+        cur.push_back(')');
+        generateParentheses(cur, toOpen, toClose - 1, ans);
+        cur.pop_back();
+    }
+}
+
+double factorial(int n) {
+    if (n == 0) {return 1;}
+    else {return n * factorial(n - 1);}
+}
+
+vector<string> generateParenthesis(int n) {
+    int ansSize = round(factorial(2 * n) / factorial(n) / factorial(n) / (n + 1));
+    vector<string> ans; ans.reserve(ansSize);
+
+    vector<char> cur;
+    generateParentheses(cur, n, 0, ans);
+
+    assert(ans.size() == ansSize);
+    return ans;
+}
+};
+
+/*
+Дмитрий Козырев
+​Если знать, что число скобочных последовательностей это C(2n,n)/(n+1), то можно сделать reserve у ans
+
+Дмитрий Козырев
+​Можно еще поставить assert на значение n чтобы знать насколько большое n они дали
+*/
