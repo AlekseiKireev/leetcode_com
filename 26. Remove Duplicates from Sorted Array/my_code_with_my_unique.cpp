a02c672ae898c011653a6efaa6ac1c4d92225eac
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-07-06 18:33:31 +0300

    Update my_code_with_my_unique.cpp

--- 26. Remove Duplicates from Sorted Array/my_code_with_my_unique.cpp
+++ 26. Remove Duplicates from Sorted Array/my_code_with_my_unique.cpp
@@ -4,10 +4,11 @@ public:
 // реализация на основе Possible implementation из https://en.cppreference.com/w/cpp/algorithm/unique
 template<class ForwardIt>
 ForwardIt UNIQUE(ForwardIt first, ForwardIt last) {
-    if (first == last) {return last;}
+    
+    if (first == last) {return last;} // check for empty nums
  
-    ForwardIt result = first;
-    for (;first != last; ++first){
+    ForwardIt result = first; // result в конечном итоге должен указывать на такой итератор, что слева от result лежат все уникальные значения из {first, ..., prev(last)}
+    for (;first != last; ++first){ // first = prev(end(nums)) -- extreme values for first
        
         if ((*result != *first)) {
             ++result;
@@ -15,7 +16,7 @@ ForwardIt UNIQUE(ForwardIt first, ForwardIt last) {
         }
     }
  
-    return ++result;
+    return ++result; // если не инкременитровать result, то он будет указывать на последний [максимальный] уникальный элемент из {first, ..., prev(last)}, что противоречит определению
 }
 
     int removeDuplicates(vector<int>& nums) {
