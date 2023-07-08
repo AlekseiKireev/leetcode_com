bff143b826830a04259cb9607d3956fc686ee675
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-07-08 12:05:47 +0300

    Update UPD_unordered_set.cpp

--- 523. Continuous Subarray Sum/UPD_unordered_set.cpp
+++ 523. Continuous Subarray Sum/UPD_unordered_set.cpp
@@ -2,11 +2,12 @@ class Solution {
 public:
   bool checkSubarraySum(const vector<int>& a, int k) { 
 
-    // error: 96 / 98 testcases passed
+   
     /*
     if(a.size() > k){return true;} // принцип Дирихле. По сути, это бы стработало, если a[j] > 0 
-    nums = [1,0,1,0,1]
-    k = 4
+    // error: 96 / 98 testcases passed
+    // nums = [1,0,1,0,1]
+    // k = 4
     */
 
      unordered_set<int> seen;
