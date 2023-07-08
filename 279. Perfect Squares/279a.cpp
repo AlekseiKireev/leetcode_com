80e7033b12af08400ec51ad2ab73c41322a062d8
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-07-09 02:21:38 +0300

    Update 279a.cpp

--- 279. Perfect Squares/279a.cpp
+++ 279. Perfect Squares/279a.cpp
@@ -7,12 +7,11 @@ class Solution {
 
   int numSquares(int n, int rem) {
     assert(rem >= 1);
-    if (perfectSquare(n)) {
-      return 1;
-    }
-    if (rem == 1) {
-      return 5;
-    }
+    
+    if (perfectSquare(n)) {return 1;}
+    
+    if (rem == 1) {return 5;}
+    
     int ans = 5;
     for (int first = 1; first * first <= n; first++) {
       ans = min(ans, 1 + numSquares(n - first * first, rem - 1));
