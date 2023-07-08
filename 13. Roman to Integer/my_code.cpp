dff13f09de192b40eaf398fb8e79bc593d69b304
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-07-08 17:24:33 +0300

    Create my_code.cpp

---
+++ 13. Roman to Integer/my_code.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int romanToInt(string_view s) { 
+
+        unordered_map<char, int> romanToValue = {
+           {'I', 1},
+           {'V', 5},
+           {'X', 10},
+           {'L', 50},
+           {'C', 100},
+           {'D', 500},
+           {'M', 1000}
+           };
+
+        int prev = romanToValue[s.front()];
+        int ans = 0;
+
+        for(const char ch : s){
+            
+            const int val = romanToValue[ch];
+            
+            ans += val;
+            
+            if(val > prev){ans -= 2 * prev;}
+
+            prev = val;
+        }
+
+        return ans;
+    }
+} ;
