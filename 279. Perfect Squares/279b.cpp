0032f1693ac03b3fc733dc28f16d1ff84bb4642e
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-07-09 14:40:06 +0300

    Update 279b.cpp

--- 279. Perfect Squares/279b.cpp
+++ 279. Perfect Squares/279b.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
   int numSquares(int n) {
 
-   vector<int> nSq(1 + n, 5);
+   vector<int> nSq(1 + n, 5); // 5 - локальный максимум, так как ответ не может превышать 4
    nSq[0] = 0;
 
      for (int sum = 1; sum <= n; sum++) {
