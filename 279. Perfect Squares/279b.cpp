7981da21ae741af3927cf0ddec0c3896336b382f
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-07-09 15:36:49 +0300

    Update 279b.cpp

--- 279. Perfect Squares/279b.cpp
+++ 279. Perfect Squares/279b.cpp
@@ -4,10 +4,12 @@ public:
 
     // nSq[i] == наименьшее число натуральных слагаемых в квадратном разложении числа i : i \in |N --> искомое == nSq.back() == nSq[n]
    vector<int> nSq(1 + n, 5); // 5 - локальный максимум, так как ответ не может превышать 4 ввиду "Теорема Лагранжа о сумме четырёх квадратов"
+    // необходим, чтобы гарантировано изначально выполнилось условие в:  nSq[sum] = min(nSq[sum], 1 + nSq[sum - first * first])
+    
    nSq[0] = 0; // 0 = 0^2 + 0^2 + 0^2 + 0^2 --> в разложении нет ни одного натруального числа --> nSq[0] = 0
 
      for (int sum = 1; sum <= n; sum++) {
-       for (int first = 1; first * first <= sum; first++) { // first * first <= sum <-->  0 <= sum - first * first
+       for (int first = 1; first * first <= sum; first++) { // first * first <= sum <-->  0 <= sum - first * first -- что используется далее в nSq[sum - first * first]
          nSq[sum] = min(nSq[sum], 1 + nSq[sum - first * first]);  
        } 
     }
