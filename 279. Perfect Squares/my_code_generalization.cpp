03074c201314a389c6ebaf4764c796a0da9bdfa0
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-07-09 17:39:53 +0300

    Update my_code_generalization.cpp

--- 279. Perfect Squares/my_code_generalization.cpp
+++ 279. Perfect Squares/my_code_generalization.cpp
@@ -3,22 +3,22 @@ public:
 	int numSquares(int N) {
 
 		vector<int> DP(N + 1, 101); // DP[i] == наименьшее число слагаемых в кубическом разложении числа i : i \in |N --> искомое == DP.back()
-		// заполнение 101 необходимо чтобы хоть раз зайти в "if (DP[i] > DP[i - cube] + 1) { DP[i] = DP[i - cube] + 1; }"
+		// заполнение 101 необходимо чтобы хоть раз зайти в "if (DP[i] > DP[i - square] + 1) { DP[i] = DP[i - square] + 1; }"
 		DP[0] = 0; // виртуальнео разложение, необходимо чтобы далее удобно работать в натуральных числах
 
 		for (int i = 1; i < N + 1; ++i) {
 			
-			int cube = 1;
+			int square = 1;
 			
-			for (int step_cube = 1; cube <= i; ++step_cube) {
+			for (int step_square = 1; square <= i; ++step_square) {
 
-				if (cube == i) { DP[i] = 1; break; }
-				if (DP[i] > DP[i - cube] + 1) { DP[i] = DP[i - cube] + 1; } // ищем наиболее оптимальные предшестчвующие кубические разложения для DP[i] 
+				if (square == i) { DP[i] = 1; break; }
+				if (DP[i] > DP[i - square] + 1) { DP[i] = DP[i - square] + 1; } // ищем наиболее оптимальные предшестчвующие кубические разложения для DP[i] 
 
 				const int power_indicator = 2; // 2 -- степенной показатель, обусловленный условием задачи. При замене его на число k, получаем решение для k-ых степеней
 				
-         			 cube = 1;
-				 for (int idx = 0; idx < power_indicator; ++idx) {cube *= step_cube;}
+         			 square = 1;
+				 for (int idx = 0; idx < power_indicator; ++idx) {square *= step_square;}
 
 			}
 		}
