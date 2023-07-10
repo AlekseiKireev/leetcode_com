705026b374b1663fcfe2e0e9301b77017d44dc9c
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-07-11 00:02:28 +0300

    Create 350d.cpp

---
+++ 350. Intersection of Two Arrays II/350d.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+   vector<int> intersect(vector<int>& a, vector<int>& b) { 
+     sort(a.begin(), a.end());
+     sort(b.begin(), b.end()); 
+     size_t ia = 0;
+     size_t ib = 0;
+     vector<int> ans;
+     while (ia < a.size() && ib < b.size()) {
+       if (a[ia] < b[ib]) {
+          ia++;
+       } else if (b[ib] < a[ia]) {
+          ib++;
+       } else {
+          ans.push_back(a[ia]);
+          ia++; 
+          ib++;
+     } 
+}
+     return ans;
+   }
+} ;
