71ada52228cc1d2e79df4ce6e227da976bd52aca
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-07-11 02:26:17 +0300

    Update 350d.cpp

--- 350. Intersection of Two Arrays II/350d.cpp
+++ 350. Intersection of Two Arrays II/350d.cpp
@@ -23,3 +23,31 @@ public:
      return ans;
    }
 } ;
+/*
+class Solution {
+public:
+   vector<int> intersect(vector<int>& a, vector<int>& b) { 
+     sort(a.begin(), a.end());
+     sort(b.begin(), b.end()); 
+      
+     size_t ia = 0;
+     size_t ib = 0;
+      
+     vector<int> ans;
+      
+     while (ia < a.size() && ib < b.size()) {
+
+       if(a[ia] == b[ib]){
+          ans.push_back(a[ia]);
+          ia++; 
+          ib++;
+       } else{
+         (a[ia] < b[ib]) ? ia++ : ib++;
+       }
+
+     } 
+
+     return ans;
+   }
+};
+*/
