0a9adcb35d35a7fb504bb19c6d26f3a75e4531fa
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-07-10 23:57:06 +0300

    Create 350c.cpp

---
+++ 350. Intersection of Two Arrays II/350c.cpp
@@ -0,0 +1,40 @@
+class Solution {
+
+struct Pair{
+   int num;
+   bool present;
+};
+
+public:
+
+   vector<int> intersect(const vector<int>& aa, const vector<int>& bb) { 
+     const vector<int> &a = aa.size() < bb.size() ? aa : bb;
+     const vector<int> &b = aa.size() < bb.size() ? bb : aa; 
+
+      vector<Pair> pairs; pairs.reserve(a.size());
+
+      for(int v : a){pairs.push_back({v, true});}
+
+      sort(pairs.begin(), pairs.end(), [](const Pair &left, const Pair &right) { 
+      return left.num < right.num;
+       });
+
+     vector<int> ans;
+     for (int v : b) {
+        auto it = lower_bound(
+           pairs.begin(),
+           pairs.end(),
+           v,
+           [](const Pair &p, int v) {
+           return p.num < v || (p.num == v && !p.present);
+           }
+        );
+       
+        if (it != pairs.end() && it->num == v && it->present > 0) { 
+           ans.push_back(v);
+            it->present = false;
+        }
+     }
+     return ans;
+  }
+};
