420677d68f584893f19093b816ceb4bb7fe834a7
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-07-11 21:57:10 +0300

    Create 704b.cpp

---
+++ 704. Binary Search/704b.cpp
@@ -0,0 +1,45 @@
+class Solution {
+
+const int ZERO = [](){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    return 0;
+
+}();
+
+public:
+int search(const vector<int>& a, int target) {
+
+    int left = 0;
+
+    int right = (int)a.size() - 1;
+
+    // answer maybe in [left; right]
+
+    while (left <= right) {
+
+        int mid = (left + right) / 2;
+
+        if (target > a[mid]) {
+
+            left = mid + 1;
+
+        }
+        else if (target < a[mid]) {
+
+            right = mid - 1;
+
+        }
+        else {
+
+            return mid;
+
+        }
+    }
+
+    return -1;
+
+}
+};
+
