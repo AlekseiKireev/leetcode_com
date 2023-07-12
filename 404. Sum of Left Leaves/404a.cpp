d59bcca242eddb6f971b34a15c8b04229ffff7e9
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-07-12 20:19:43 +0300

    Create 404a.cpp

---
+++ 404. Sum of Left Leaves/404a.cpp
@@ -0,0 +1,36 @@
+const int ZERO = [](){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    return 0;
+}();
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    int sumOfLeftLeaves(TreeNode* root) {
+        if (root == nullptr) {return 0;}
+
+        int sum;
+        if (
+        root->left != nullptr && 
+        root->left->left == nullptr && 
+        root->left->right == nullptr
+        ) {sum = root->left->val;} 
+        else {sum = sumOfLeftLeaves(root->left);}
+
+        return sum + sumOfLeftLeaves(root->right);
+    }
+};
