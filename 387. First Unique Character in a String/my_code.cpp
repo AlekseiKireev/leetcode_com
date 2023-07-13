e69a95d70ec6420945727d59489eb0f16ac67464
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-07-13 18:34:18 +0300

    Update my_code.cpp

--- 387. First Unique Character in a String/my_code.cpp
+++ 387. First Unique Character in a String/my_code.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     int firstUniqChar(string s) {
-  const int power_English_alphabet = 26;
+  	
+	const int power_English_alphabet = 26;
 	array<int, power_English_alphabet> char_to_count;
 	char_to_count.fill(0);
 
@@ -11,9 +12,9 @@ public:
 
 	for (int i = 0; i < s.size(); ++i) {
         // s[i] - 'a' == chr - 'a'
-		if (char_to_count[s[i] - 'a'] == 1) { return i; }
+		if (char_to_count[s[i] - 'a'] == 1) { return i; } // "find the first non-repeating character in it and return its index."
 	}
 
-	return -1;
+	return -1; // " If it does not exist, return -1."
     }
 };
