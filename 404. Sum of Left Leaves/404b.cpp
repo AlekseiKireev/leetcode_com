be0adb5daded46779203368a99000a8549444fed
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-07-13 14:01:58 +0300

    Update 404b.cpp

--- 404. Sum of Left Leaves/404b.cpp
+++ 404. Sum of Left Leaves/404b.cpp
@@ -1,8 +1,8 @@
-const int ZERO = [](){
+const int ZERO = []() {
 
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
-    
+
     return 0;
 }();
 
@@ -20,24 +20,34 @@ const int ZERO = [](){
 
 class Solution {
 
-  int sumOfLeftLeaves(TreeNode* node, bool isLeft) {
-    if (node == nullptr) {
-      return 0;
+    int sumOfLeftLeaves(TreeNode* node, bool isLeft) {
+
+        if (node == nullptr) { return 0; }
+
+        if (node->left == nullptr && node->right == nullptr) {
+            if (isLeft) {
+                return node->val;
+            }
+            else {
+                return 0;
+            }
+        }
+
+        return
+            sumOfLeftLeaves(node->left, true) + sumOfLeftLeaves(node->right, false);
     }
-    if (node->left == nullptr && node->right == nullptr) { 
-      if (isLeft) {
-        return node->val;
-      } else {
-        return 0;
-    } }
-    return
-      sumOfLeftLeaves(node->left, true) + 
-      sumOfLeftLeaves(node->right, false);
-     }
 
 public:
 
-  int sumOfLeftLeaves(TreeNode* root) { 
-    return sumOfLeftLeaves(root, false); 
-  }
+    int sumOfLeftLeaves(TreeNode* root) {
+        return sumOfLeftLeaves(root, false);
+    }
 };
+
+/* // https://leetcode.com/problems/sum-of-left-leaves/solutions/88951/3-line-recursive-c-solution-no-need-to-explain/
+int sumOfLeftLeaves(TreeNode* root, bool isleft = false) {
+    if (!root) return 0;
+    if (!root->left && !root->right) return isleft ? root->val : 0;
+    return sumOfLeftLeaves(root->left, true) + sumOfLeftLeaves(root->right, false);
+}
+*/
