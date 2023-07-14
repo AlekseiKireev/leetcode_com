7c9c13bb83c4df6492b1b48ee5a74d472c77f823
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-07-14 20:49:40 +0300

    Create 438d.cpp

---
+++ 438. Find All Anagrams in a String/438d.cpp
@@ -0,0 +1,45 @@
+class Solution {
+
+    long long* getKoef() {
+        static long long koef[1 + 'z'] = {};
+        if (koef[0] == 0) {
+            mt19937 gen(2202);
+            for (char c = 'a'; c <= 'z'; c++) {
+                koef[c] = uniform_int_distribution<long long>(1e14, 2e14)(gen);
+            }
+        }
+        return koef;
+    }
+
+public:
+
+    vector<int> findAnagrams(string_view s, string_view p) {
+        if (s.size() < p.size()) {
+            return {};
+        }
+        vector<int> ans;
+        long long* koef = getKoef();
+        long long sum = 0; // char of p -, chars of s +, multiply by koef 
+        for (char c : p) {
+            sum -= koef[c];
+        }
+        for (int i = 0; i < (int)p.size(); i++) {
+            char c = s[i];
+            sum += koef[c];
+        }
+        if (sum == 0) {
+            ans.push_back(0);
+        }
+        for (int i = (int)p.size(); i < (int)s.size(); i++) {
+            char toRemove = s[i - (int)p.size()];
+            char toAdd = s[i];
+            sum -= koef[toRemove];
+            sum += koef[toAdd];
+            if (sum == 0) {
+                ans.push_back(i - (int)p.size() + 1);
+            }
+            }
+            return ans;
+        }
+    
+};
