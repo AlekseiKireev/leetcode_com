6c1201b25b519b53a018a679a5d4606c4ce172b1
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-07-14 20:10:17 +0300

    Create 438с.cpp

---
+++ 438. Find All Anagrams in a String/438с.cpp
@@ -0,0 +1,52 @@
+class Solution {
+
+public:
+   vector<int> findAnagrams(string_view s, string_view DesiredAnagr) { 
+     
+      vector<int> ans;
+        
+      if(s.size() < DesiredAnagr.size()){ return ans;}
+
+      int count[1 + 'z'] = {}; // char of DesiredAnagr -, chars of s +
+      int NonZero = 0;
+
+      for (char c : DesiredAnagr) { 
+
+         if( count[c] == 0){++NonZero;}
+         count[c]--;
+      }
+
+      for (int j = 0; j < DesiredAnagr.size(); j++) { 
+
+         const char c = s[j];
+
+         if( count[c] == 0){++NonZero;}
+         count[c]++;
+         if(count[c] == 0){--NonZero;} // после сложжения в count[c]++ может быть count[c] == 0
+
+      }
+
+      if(NonZero == 0){ans.push_back(0);}
+
+
+      for (int i = (int)DesiredAnagr.size(); i < (int)s.size()  ; i++) { 
+
+         const char ToRemove = s[i - (int)DesiredAnagr.size()];
+         const char ToAdd = s[i];
+
+         if(count[ToRemove] == 0){++NonZero;}
+         --count[ToRemove];        
+         if(count[ToRemove] == 0){--NonZero;}
+
+
+         if(count[ToAdd] == 0){++NonZero;}
+         ++count[ToAdd];
+         if(count[ToAdd] == 0){--NonZero;}
+
+         if (NonZero == 0) {ans.push_back(i- (int)DesiredAnagr.size() + 1);}
+
+      }
+
+      return ans;
+   }
+} ;
