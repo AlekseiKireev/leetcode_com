bbdd5d003d001a264914dbb6eed6f0f0432f0c0b
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-07-16 00:56:49 +0300

    Update my_code.cpp

--- 704. Binary Search/my_code.cpp
+++ 704. Binary Search/my_code.cpp
@@ -12,12 +12,20 @@ public:
 
 	int search(const vector<int>& a, int target) {
 
+		if (a.back() < target || target < a.front()) { return -1; }
+		
 		// left и right -- противоположные инварианты: a[left] < target, a[right] >= target --> ответ будет right или -1
 		int left = -1;
 		int right = (int)a.size();
 
-		if (a.back() < target) { return -1; }
-
+		/*
+		//2 / 47 testcases passed
+		//nums = [5] 
+		//target = 5
+  		int left = 0;
+		int right = a.size() - 1;
+		*/
+		
 		// примечательно также то, что данная реализация будет работать для пустого массива
 		while (1 != right - left) { // <-->  1 < right - left <--> цикл до тех пор, пока left + 1 != right -- 
 					    // то есть пока не дошли до соседних гранично-инвариантных индексов, пока расстояние между left и right не равно 1
