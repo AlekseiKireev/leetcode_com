e1f77e818e2a8968bb0335e37938b06d0cb1d401
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-07-15 20:15:55 +0300

    Update my_code_two_pointer_UPD.cpp

--- 438. Find All Anagrams in a String/my_code_two_pointer_UPD.cpp
+++ 438. Find All Anagrams in a String/my_code_two_pointer_UPD.cpp
@@ -4,20 +4,20 @@ public:
     //  p == Angr == Anagram
     vector<int> findAnagrams(string_view S, string_view Angr) {
         
-        // DataForBuildAngr[k] --> необходимое положительное количество символов 'a' + k для построения анаграммы наподобие Angr в рамках windows == [ S[l], S[l + 1], ..., S[r] ]
+        // DataWindowForBuildAngr[k] --> необходимое положительное количество символов 'a' + k для построения анаграммы наподобие Angr в рамках window == [ S[l], S[l + 1], ..., S[r] ]
         // 26 -- можщность алфавита, обусловленного улосвием задачи: "s and p consist of lowercase English letters."
-        array<int, 26> DataForBuildAngr; // содержит положительное количество символов, которые необходимы для построения анаграммы наподобие Angr
-        DataForBuildAngr.fill(0);
+        array<int, 26> DataWindowForBuildAngr; // содержит положительное количество символов, которые необходимы для построения анаграммы наподобие Angr
+        DataWindowForBuildAngr.fill(0);
 
-        for(char ch : Angr){++DataForBuildAngr[ch - 'a'];}
+        for(char ch : Angr){++DataWindowForBuildAngr[ch - 'a'];}
 
         int CountCharInWindowForBuildAngr = 0; // количество символов в window, которые лежат в Angr
         
         vector<int> Idxs;
         for(int l = 0, r = 0; r < S.size();++r){
 
-            // ">=" так как декрементируем на месте. Именно благодаря декременту дли символов, не лежащих в Angr, имеет место неравенство: DataForBuildAngr[k] < 0
-            if(--DataForBuildAngr[S[r] - 'a'] >= 0){ // если это истино, то символ DataForBuildAngr[S[r] - 'a'] хотя-бы раз встречался в Angr
+            // ">=" так как декрементируем на месте. Именно благодаря декременту дли символов, не лежащих в Angr, имеет место неравенство: DataWindowForBuildAngr[k] < 0
+            if(--DataWindowForBuildAngr[S[r] - 'a'] >= 0){ // если это истино, то символ DataWindowForBuildAngr[S[r] - 'a'] хотя-бы раз встречался в Angr
                 ++CountCharInWindowForBuildAngr;
             }
 
@@ -27,9 +27,9 @@ public:
                                
                 if(r - l + 1 == Angr.size()){Idxs.push_back(l);} // <--> [ Angr[0], Angr[1], ..., Angr[Angr.size() - 1] ] == [ S[l], S[l + 1], ..., S[r] ]
 
-                // сдвигаем левый указатель. Если ++DataForBuildAngr[S[l++] - 'a'] > 0 истино, то левый указатель указывал на символ, лежащий в Angr, т.е. S[l], 
+                // сдвигаем левый указатель. Если ++DataWindowForBuildAngr[S[l++] - 'a'] > 0 истино, то левый указатель указывал на символ, лежащий в Angr, т.е. S[l], 
                 // именно поэтому декрементируем CountCharInWindowForBuildAngr
-                if(++DataForBuildAngr[S[l++] - 'a'] > 0){--CountCharInWindowForBuildAngr;}
+                if(++DataWindowForBuildAngr[S[l++] - 'a'] > 0){--CountCharInWindowForBuildAngr;}
 
             }
 
