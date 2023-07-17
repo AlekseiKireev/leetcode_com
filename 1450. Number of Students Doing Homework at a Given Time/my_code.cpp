43de1d2f05bf854e1079fcad379f727d28b6145a
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-07-17 05:05:33 +0300

    Create my_code.cpp

---
+++ 1450. Number of Students Doing Homework at a Given Time/my_code.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int busyStudent(vector<int>& startTime, vector<int>& endTime, int queryTime) {
+        
+        int ans = 0;
+
+        for(int i = 0; i < startTime.size(); ++i){
+            
+            /* // тест демонстрирует что данные не отсортированы. Было бы это так -- вместо continue можно использовать break
+            [9,8,7,6,5,4,3,2,1]
+            [10,10,10,10,10,10,10,10,10]
+            5
+            */
+            if(queryTime < startTime[i]){continue;}           
+            if(startTime[i] <= queryTime && queryTime <= endTime[i]) {++ans;}
+                       
+        }
+
+        return ans;
+    }
+};
