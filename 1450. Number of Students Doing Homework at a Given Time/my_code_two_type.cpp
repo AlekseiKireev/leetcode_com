3c0d2f828e9333d685e2d4445e5702119a88f3f5
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-07-17 04:51:33 +0300

    Create my_code_two_type.cpp

---
+++ 1450. Number of Students Doing Homework at a Given Time/my_code_two_type.cpp
@@ -0,0 +1,43 @@
+class Solution {
+
+struct events{
+int val;
+int type;
+};
+
+public:
+    int busyStudent(vector<int>& startTime, vector<int>& endTime, int queryTime) {
+        
+        vector<events> Events;
+        Events.reserve(endTime.size());
+
+        for(int i = 0; i < endTime.size(); ++i){
+            
+            Events.push_back({startTime[i], 1});
+            Events.push_back({endTime[i], -1});
+        }
+
+        /*
+        [5, 7], [5, 9]
+        [8, 11], [11, 16]
+        */
+        sort(Events.begin(), Events.end(), [](const events l, const events r){
+            /*
+            l.val < r.val -- сортировка по возрастанию
+            r.type < l.type -- сортировка по убыванию
+            */
+            return l.val < r.val || (l.val == r.val && r.type < l.type); 
+        }
+        );
+
+        // "Return the number of students doing their homework at time queryTime"
+        int ans = 0;
+
+        for(auto [val, type] : Events){
+            if(queryTime < val || (queryTime == val && type == -1)){break;}
+            ans += type;
+        }
+
+        return ans;
+    }
+};
