09661ca424c2ecb642b3e4566131184318c86ef2
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-07-18 06:56:53 +0300

    Create my_code_DFS.cpp

---
+++ 200. Number of Islands/my_code_DFS.cpp
@@ -0,0 +1,50 @@
+class Solution {
+
+                /*
+
+                * == точка интереса, надо посетить и узнать, есть ли там суша
+
+                .*.
+                *1*
+                .*.
+
+                Поэтому передаем в DFS координаты
+
+                */
+                
+void DFS(vector<vector<char>>& grid, int i, int j){
+
+    // if(grid[i][j] != '1'){return;} // можно написать такой условный оператор, вмесо "if(grid[i + dx][j + dy] != '1'){continue;}"
+    grid[i][j] = '2'; // '2' == visited
+
+    for(auto [dx, dy] : vector<pair<int, int>>{{0,1}, {0,-1}, {1,0}, {-1,0} }){
+        
+        if((i + dx) * (j + dy) < 0 || (i + dx) >= grid.size() || (j + dy) >= grid.back().size()){continue;}
+        if(grid[i + dx][j + dy] != '1'){continue;}
+
+        DFS(grid, i + dx, j + dy);
+
+    }
+
+}
+
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        
+        int NumberIslands = 0;
+
+        if(grid.empty()){return NumberIslands;}
+
+        for(int i = 0; i  < grid.size(); ++i){ // обход по стркоам
+            for(int j = 0; j < grid.back().size(); ++j){ // обход по столбцам
+                
+                if(grid[i][j] == '1'){
+                    ++NumberIslands;
+                    DFS(grid, i, j);
+                }
+            }
+        }
+
+        return NumberIslands;
+    }
+};
