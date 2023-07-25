a234377e613c8a4e7e961e046e546535dc44ef5f
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-07-26 01:06:41 +0300

    Update 200b.cpp

--- 200. Number of Islands/200b.cpp
+++ 200. Number of Islands/200b.cpp
@@ -9,7 +9,7 @@ public:
         vector<int> q;
         q.reserve(grid.size() * grid[0].size() * 2); // "* 2" обусловлен тем, что в q не как исконно хранятся пары кооринат, а храним пары подряд, т.е. пара имеем вид: {q[2n], q[2n + 1]} : n \in Z_{+}
         
-        int qBegin = 0; // начало очереди 
+        int qBegin = 0; // изначально - начало очереди, в последствии - позиция в векторе, которую обрабатываем, т.е. указывает на координату в grid, которую надо исследовать
         int ans = 0;
         
         for (int i = 0; i < (int)grid.size(); i++) {
@@ -32,8 +32,8 @@ public:
                         for (int di = -1; di <= 1; di++) {
                             for (int dj = -1; dj <= 1; dj++) {
                                 if (di * di + dj * dj == 1) {
-                                    int ni = i + di;
-                                    int nj = j + dj;
+                                    int ni = i + di; // == new_i
+                                    int nj = j + dj; // == new_j
                                     if (
                                         0 <= ni && ni < (int)grid.size() &&
                                         0 <= nj && nj < (int)grid[ni].size() &&
