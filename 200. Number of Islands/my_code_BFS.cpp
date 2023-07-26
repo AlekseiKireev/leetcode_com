4a94dd68173a1b9d02930724c0dea06656279c8f
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-07-26 14:13:44 +0300

    Update my_code_BFS.cpp

--- 200. Number of Islands/my_code_BFS.cpp
+++ 200. Number of Islands/my_code_BFS.cpp
@@ -1,1 +1,68 @@
+class Solution {
+
+struct Pair{
+
+int x;
+int y;
+
+};
+
+void BFS(vector<vector<char>>& grid, int i, int j) {
+
+    queue<Pair> BurningVertexes; // Queue -- содержит вершины из текущего кольца огня и, может быть, последующего 
+
+    BurningVertexes.push({ i, j });
+    grid[i][j] = '0';
+
+    while (BurningVertexes.empty() == false) {
+
+        const int x = BurningVertexes.front().x;
+        const int y = BurningVertexes.front().y;
+
+        BurningVertexes.pop();
+
+        for (const auto [dx, dy] : vector<pair<int, int>>{ {0, 1},{1, 0},{0, -1},{-1, 0} }) { // or: array<int, 5> Step = { 0,1,0,-1,0 };
+
+
+            if (
+                0 <= x + dx && x + dx < grid.size()
+                &&
+                0 <= y + dy && y + dy < grid.front().size()
+                &&
+                grid[x + dx][y + dy] == '1'
+               ) 
+                {
+
+                    grid[x + dx][y + dy] = '0';
+                    BurningVertexes.push({ x + dx, y + dy });
+
+                }
+
+        }
+
+    }
+
+}
+
+public:
+
+    int numIslands(vector<vector<char>>& grid) {
+        
+        int NumberIslands = 0;
+
+        if(grid.empty()){return NumberIslands;}
+
+        for(int i = 0; i  < grid.size(); ++i){ // обход по стркоам
+            for(int j = 0; j < grid.back().size(); ++j){ // обход по столбцам
+                
+                if(grid[i][j] == '1'){
+                    ++NumberIslands;
+                    BFS(grid, i, j);
+                }
+            }
+        }
+
+        return NumberIslands;
+    }
+};
 
