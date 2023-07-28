23b7c4f3c2402e5db8215c4afd7e067cced84783
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-07-28 23:22:15 +0300

    Update 167g-good.cpp

--- 167. Two Sum II - Input Array Is Sorted/167g-good.cpp
+++ 167. Two Sum II - Input Array Is Sorted/167g-good.cpp
@@ -1,1 +1,61 @@
-c
+#include <vector> 
+#include <cassert> 
+#include <climits> 
+#include <stdexcept>
+#include <iostream>
+
+using namespace std;
+
+
+class Solution{
+
+bool sumGreater(int a, int b, int c) { // a + b > c
+    if (b > 0 && a > INT_MAX - b) {
+        return true;
+    }
+    if (b < 0 && a < INT_MIN - b) {
+        return false;
+    }
+    return a + b > c;
+}
+
+bool sumEquals(int a, int b, int c) { // a + b == c
+    if (b > 0 && a > INT_MAX - b) {
+        return false;
+    }
+    if (b < 0 && a < INT_MIN - b) {
+        return false;
+    }
+    return a + b == c;
+}
+
+public:
+
+    vector<int> twoSum(const vector<int>& a, int target) {
+
+        int j = (int)a.size() - 1;
+        for (int i = 0; i < (int)a.size(); i++) {
+
+            while (j >= 0 && sumGreater(a[i], a[j], target)) { j--; }
+
+            assert(i < j);
+
+            if (sumEquals(a[i], a[j], target)) {
+                return { i + 1, j + 1 };
+            }
+
+        }
+
+        throw invalid_argument("no sum target");
+    }
+
+};
+
+
+int main() {
+
+    cout << Solution().twoSum({ 1,2,INT_MAX }, 3)[0] << ' ' << Solution().twoSum({ 1,2,INT_MAX }, 3)[1] << '\n'; // 1 2
+    cout << Solution().twoSum({ INT_MIN, -1,-2 }, -3)[0] << ' ' << Solution().twoSum({ INT_MIN, -1,-2 }, -3)[1] << '\n'; // 2 3
+
+    return 0;
+}
