d14936cd6849c100bf7a05f86e98073b070902ae
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-07-29 18:26:45 +0300

    Update 167c.cpp

--- 167. Two Sum II - Input Array Is Sorted/167c.cpp
+++ 167. Two Sum II - Input Array Is Sorted/167c.cpp
@@ -6,7 +6,7 @@ public:
             int j = (int)(upper_bound(a.begin(), a.end(), target - a[i]) - a.begin()) - 1;
 
             // if (j != i && j >= 0 && a[j] == target - a[i]) {return { i + 1, j + 1 };} // equiv
-            assert(j > i);
+            assert(j > i); // ПОЧЕМУ j > i == TRUE???
             if (a[j] == target - a[i]) {return { i + 1, j + 1 };}      // РАЗВЕ ТУТ НЕ НУЖНА ПРОВЕРКА НА ТО, ЧТО J != I ???????
 
         }
