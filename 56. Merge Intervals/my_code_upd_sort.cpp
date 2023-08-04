66eff8afe30c558308c66bf19551fa620b3575df
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-08-04 22:22:09 +0300

    Create my_code_upd_sort.cpp

---
+++ 56. Merge Intervals/my_code_upd_sort.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        
+        sort(intervals.begin(), intervals.end());
+
+        vector<vector<int>> Covers;
+        Covers.reserve(intervals.size());
+
+        for(auto interval : intervals){
+
+            if(Covers.empty() || ! /*CheckIntersection = */ (interval[0] <= Covers.back()[1])){
+                Covers.push_back(move(interval));
+            }else{ // CheckIntersection == true
+                Covers.back()[1] = max(Covers.back()[1], interval[1]);
+            }
+
+        }
+
+        Covers.shrink_to_fit();
+        return Covers;
+
+    }
+};
