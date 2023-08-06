16bdc3290fb3df4e550efc07d62ac611e6b05e79
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-08-06 09:25:54 +0300

    Update my_code_unordered_set.cpp

--- 380. Insert Delete GetRandom O(1)/my_code_unordered_set.cpp
+++ 380. Insert Delete GetRandom O(1)/my_code_unordered_set.cpp
@@ -1,1 +1,28 @@
-d
+class RandomizedSet {
+
+private:
+
+unordered_set<int> Values;
+
+public:
+    
+    bool insert(int val) {
+
+        if(Values.count(val)){return false;}
+        Values.insert(val);
+        return true;
+
+    }
+    
+    bool remove(int val) {
+        if(Values.count(val) == 0){return false;}
+        Values.erase(val);
+        return true;
+    }
+    
+    int getRandom() const {
+
+
+        return *next(Values.cbegin(), ( rand() % (Values.size()) ) );
+    }
+};
