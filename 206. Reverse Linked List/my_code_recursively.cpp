087b8d0e56115665801fa9bc3fa52dd520d38458
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-08-07 20:15:16 +0300

    Create my_code_recursively.cpp

---
+++ 206. Reverse Linked List/my_code_recursively.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+ public:
+  ListNode* reverseList(ListNode* head) {
+
+    // for "Example 3:"
+    if(head == nullptr){cout << "head == nullptr\n";return nullptr;}
+
+    // выход из рекурсии
+    if (head->next == nullptr) {return head;}
+
+    ListNode* Ans = reverseList(head->next); // Ans указывает на head->next
+    cout << head->val << ' ';
+
+    head->next->next = head;
+    head->next = nullptr; // зануляем связь?
+    return Ans;
+  }
+};
