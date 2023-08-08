36e3c5a3c52d2601cea2182ddd05da22450e093d
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-08-08 19:42:11 +0300

    Update my_code.cpp

--- 763. Partition Labels/my_code.cpp
+++ 763. Partition Labels/my_code.cpp
@@ -8,12 +8,15 @@ public:
         vector<int> CharToEndIdx(26); // "s consists of lowercase English letters."
         for(int i = 0; i < s.size(); ++i){CharToEndIdx[s[i] - 'a'] = i;}
 
-        int PrevIdxEndPart = -1; /* "-1" необходим для того для удобства измерения расстояния между индексами. 
+        int IdxEndPart = -1; /* "-1" необходим для того для удобства измерения расстояния между индексами. 
                                     Если бы "PrevIdxEndPart = 0", то имело бы место стркоа: "SizeParts.push_back(IdxEndPart -   PrevIdxEndPart + 1);"*/
         int IdxEndPart = 0;
 
         for(int i = 0; i < s.size(); ++i){ // сдвиг левого указтеля
-           
+
+            // если i < IdxEndPart и при этом IdxEndPart < CharToEndIdx[s[i] - 'a'], значит в исследуемом чанке, в рамках индексов {IdxEndPart, IdxEndPart}, есть 
+            // символ для которого не выполняется условие " each letter appears in at most one part." -- ибо есть еще такой символ по инексу CharToEndIdx[s[i] - 'a']
+            // поэтому IdxEndPart надо обновить до CharToEndIdx[s[i] - 'a']
             IdxEndPart = max(IdxEndPart, CharToEndIdx[s[i] - 'a']); // сдвиг правого указтеля
 
             assert(i <= IdxEndPart); 
