9135272835b7db7cf1cc3076d595eb54bb99ad9c
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-08-12 18:51:31 +0300

    Update my_code_brute_force.cpp

--- 19. Remove Nth Node From End of List/my_code_brute_force.cpp
+++ 19. Remove Nth Node From End of List/my_code_brute_force.cpp
@@ -1,1 +1,49 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 
+class Solution {
+public:
+
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        
+        ListNode* Buffer = head;
+        int Len = 0;
+        while(Buffer != nullptr){
+            ++Len;
+            Buffer = Buffer->next;
+        }
+
+        // дошли до конца списка --> у удаляемого элеменат Garbage нет prev(Garbage), т.е. удаляем первый элемент листа
+        if(Len == n){
+
+            ListNode* Garbage = head;
+            head = head->next;
+            delete Garbage;
+            return head;
+
+        }
+
+        assert(n < Len);
+
+        Buffer = head;
+        for(int i = 0; i < Len - n - 1; ++i){ // условие выхода из цикла можно прочуст-ь на примере "Ex 1 for alg 1."
+            Buffer = Buffer->next;
+        } // Buffer == prev(Garbage)
+
+        ListNode* Garbage = Buffer->next;
+
+        Buffer -> next = Buffer->next -> next;
+        delete Garbage;
+        return head;
+
+    }
+
+};
