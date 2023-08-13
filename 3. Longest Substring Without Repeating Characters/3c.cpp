ed788cd00ec4eb65f61de4379cf16b566276933e
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-08-13 03:14:19 +0300

    Update 3c.cpp

--- 3. Longest Substring Without Repeating Characters/3c.cpp
+++ 3. Longest Substring Without Repeating Characters/3c.cpp
@@ -1,1 +1,24 @@
+class Solution {
+public:
 
+    int lengthOfLongestSubstring(string_view s) {
+        int ans = 0;
+        bool used[128] = {};
+        int after = 0;
+      
+        for (int first = 0; first < (int)s.size(); first++) {
+          
+            while (after < (int)s.size() && !used[s[after]]) {
+                used[s[after]] = true;
+                after++;
+            }
+          
+            ans = max(ans, after - first);
+            used[s[first]] = false;
+          
+        }
+      
+        return ans;
+    }
+    
+};
