8f8ed32f59dc2b2cedd851821b6c2bde7c4d4b3b
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-08-13 03:06:47 +0300

    Create 3b.cpp

---
+++ 3. Longest Substring Without Repeating Characters/3b.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+
+    int lengthOfLongestSubstring(string s) {
+        int ans = 0;
+        for (int len = 1; len < 128; len++) {
+          
+            bool goodLen = false;
+          
+            for (int first = 0; first + len <= (int)s.size(); first++) {
+              
+                bool used[128] = {};
+                bool good = true;
+              
+                for (int i = first; i < first + len; i++) {
+                    char c = s[i];
+                  
+                    if (used[c]) {
+                        good = false;
+                        break;
+                    }
+                  
+                    used[c] = true;
+                }
+              
+                if (good) {
+                    ans = len;
+                    goodLen = true;
+                    break;
+                }
+              
+            }
+          
+            if (!goodLen) {break;}
+          
+        }
+        return ans;
+      
+    }
+};
