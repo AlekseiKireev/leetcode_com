f5275a2ce04bc5a2227633fb416528df63df5477
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-08-16 00:35:35 +0300

    Create my_code_preorder_rec.cpp

---
+++ 103. Binary Tree Zigzag Level Order Traversal/my_code_preorder_rec.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+
+private:
+
+vector<vector<int>> ZigzagLevel; // map : height --> nodes at this height
+
+void Visited(TreeNode* node, int Height){
+
+    if(node == nullptr){return;}
+
+    if(Height == ZigzagLevel.size()){ZigzagLevel.push_back(vector<int>());}
+
+    ZigzagLevel[Height].push_back(node->val);
+
+    Visited(node->left, Height + 1);
+    Visited(node->right, Height + 1);
+
+}
+
+public:
+
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        
+        Visited(root, 0);
+
+        for(int i = 1; i < ZigzagLevel.size(); i += 2){
+
+            reverse(ZigzagLevel[i].begin(), ZigzagLevel[i].end());
+
+        }
+
+        return ZigzagLevel;
+    }
+};
