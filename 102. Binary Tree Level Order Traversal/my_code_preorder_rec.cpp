a39439e970194104ea78919346a0e74fda0a943c
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-08-15 23:53:11 +0300

    Update my_code_preorder_rec.cpp

--- 102. Binary Tree Level Order Traversal/my_code_preorder_rec.cpp
+++ 102. Binary Tree Level Order Traversal/my_code_preorder_rec.cpp
@@ -19,9 +19,14 @@ void Visited(TreeNode* node, int Height){
 
     if(node == nullptr){return;}
 
-    if(Height == ZigzagLevel.size()){ZigzagLevel.push_back(vector<int>());}
-
+    // можно использовать условный оператор вместо цикла по той причине, что для каждого рекур-о вызова сразу же проверяем Height --> не возникнет ситуации
+    // Height > ZigzagLevel.size() [что будет продемонстр-о в inorder / postorder], единственное, чтто может быть: Height == ZigzagLevel.size()
+    
+    if(Height == ZigzagLevel.size()){ZigzagLevel.push_back(vector<int>());} /* <--> */ // while(Height >= ZigzagLevel.size()){ZigzagLevel.push_back(vector<int>());} 
+    assert(Height < ZigzagLevel.size());
     ZigzagLevel[Height].push_back(node->val);
+
+    cout <<  Height << " : " << node-> val <<'\n'; // см. вывод ниже для root = [3,9,20,18,21,4,15,7]
     
     Visited(node->left, Height + 1);
     Visited(node->right, Height + 1);
@@ -33,3 +38,13 @@ public:
         return ZigzagLevel;
     }
 };
+/*
+0 : 3
+1 : 9
+2 : 18
+3 : 7
+2 : 21
+1 : 20
+2 : 4
+2 : 15
+*/
