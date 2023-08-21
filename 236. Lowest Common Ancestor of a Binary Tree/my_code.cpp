4f156be18ce94e58f232ad21c8188930ae7fbb41
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-08-21 19:55:17 +0300

    Update my_code.cpp

--- 236. Lowest Common Ancestor of a Binary Tree/my_code.cpp
+++ 236. Lowest Common Ancestor of a Binary Tree/my_code.cpp
@@ -7,6 +7,7 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+
 class Solution {
 
 private:
@@ -17,7 +18,7 @@ bool qFound = false;
 const TreeNode* P;
 const TreeNode* Q;
 
-TreeNode* LCA;
+TreeNode* LCA = nullptr;
 
 private:
 
@@ -26,17 +27,18 @@ void Traversal(TreeNode* node){
     if(node == nullptr){return;}
     if(pFound && qFound){return;}
 
+    bool FoundBefore = pFound || qFound;
+
 
     Traversal(node->left);
     Traversal(node->right);
-
-    /*
+   
 
     if(node == P){pFound = true;}
     if(node == Q){qFound = true;}
 
-    if(pFound && qFound && ){LCA = node;}
-    */
+    if(LCA == nullptr && pFound && qFound && FoundBefore == false){LCA = node;}
+    
 }
 
 public:
