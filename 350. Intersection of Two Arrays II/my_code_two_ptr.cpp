069cad6a6df4991522ac46e9601609e40d6910e8
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-08-25 00:05:33 +0300

    Update my_code_two_ptr.cpp

--- 350. Intersection of Two Arrays II/my_code_two_ptr.cpp
+++ 350. Intersection of Two Arrays II/my_code_two_ptr.cpp
@@ -1,1 +1,36 @@
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
 
+
+        vector<int> intersection; intersection.reserve(min(nums1.size(), nums2.size()));
+
+        int Ptr_1 = 0;
+        int Ptr_2 = 0;
+
+        
+        while(Ptr_1 < nums1.size() && Ptr_2 < nums2.size()){
+
+            if(nums1[Ptr_1] == nums2[Ptr_2]){
+
+                intersection.push_back(nums1[Ptr_1]);
+                ++Ptr_1;
+                ++Ptr_2;
+                
+            }else{
+                
+               //(nums1[Ptr_1] < nums2[Ptr_2]) ? ++Ptr_1 : ++Ptr_2; // equiv
+               Ptr_1 += (nums1[Ptr_1] < nums2[Ptr_2]);
+               Ptr_2 += (nums1[Ptr_1] > nums2[Ptr_2]);
+
+            }
+
+        }
+
+        intersection.shrink_to_fit();
+        return intersection;
+    }
+};
