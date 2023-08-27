1a4c87e03e4103fb05db9aad98701b406c102971
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-08-27 23:22:12 +0300

    Create 53a.cpp

---
+++ 53. Maximum Subarray/53a.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+  int maxSubArray(const vector<int>& a) {
+    int maxEndsHere = a[0];
+    int totalMax = a[0];
+    for (size_t i = 1; i < a.size(); i++) {
+     maxEndsHere = max(maxEndsHere + a[i], a[i]);
+     totalMax = max(totalMax, maxEndsHere);
+    }
+    return totalMax;
+  }
+};
+
