c535ed552703bab44092b38f1f709265856d52c1
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-08-27 17:18:53 +0300

    Update my_code_upd.cpp

--- 236. Lowest Common Ancestor of a Binary Tree/my_code_upd.cpp
+++ 236. Lowest Common Ancestor of a Binary Tree/my_code_upd.cpp
@@ -29,7 +29,7 @@ void travel(TreeNode* node){
     // Проверка на то, может ли текуищй узел быть потенциальным родителем p и q. 
     // По условию необходимо найти нижайшего родителя p и q. 
     // node может быть родителем / LCA, если расположена не ниже [выше либо равен] p или q.
-    bool CheckNodeOnParent = (! Found_P) && (! Found_Q); // == CNOP
+    bool CheckNodeOnParentP_or_Q = (! Found_P) && (! Found_Q); // == CNOP
 
     if(node == Q){ Found_Q = true; }
     if(node == P){ Found_P = true; }
@@ -37,7 +37,7 @@ void travel(TreeNode* node){
     travel(node->left);
     travel(node->right);
 
-    if(LCA == nullptr && CheckNodeOnParent && Found_P && Found_Q){LCA = node;}
+    if(LCA == nullptr && CheckNodeOnParentP_or_Q && Found_P && Found_Q){LCA = node;}
 
 }
 
