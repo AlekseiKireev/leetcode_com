c194c34a3b144adacdae596d89cef765dab56f5d
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-08-29 22:07:51 +0300

    Update my_code_unordered_set.cpp

--- 2215. Find the Difference of Two Arrays/my_code_unordered_set.cpp
+++ 2215. Find the Difference of Two Arrays/my_code_unordered_set.cpp
@@ -1,1 +1,29 @@
+class Solution {
+public:
+    vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
+        
+        unordered_set<int> set1 = {make_move_iterator(nums1.begin()), make_move_iterator(nums1.end())};
+        unordered_set<int> set2 = {make_move_iterator(nums2.begin()), make_move_iterator(nums2.end())};
 
+        vector<vector<int>> answer(2);
+        answer.front().reserve(set1.size());
+        answer.back().reserve(set2.size());
+
+        for(auto num : set1){
+            if(set2.count(num) == 0){
+                answer.front().push_back(num);
+            }
+        }
+
+        for(auto num : set2){
+            if(set1.count(num) == 0){
+                answer.back().push_back(num);
+            }
+        }        
+
+        answer.front().shrink_to_fit();
+        answer.back().shrink_to_fit();
+        return answer;
+
+    }
+};
