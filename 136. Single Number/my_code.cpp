b3e081ddba238322b3e22242be4719c217f6dee4
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-08-31 00:55:16 +0300

    Update my_code.cpp

--- 136. Single Number/my_code.cpp
+++ 136. Single Number/my_code.cpp
@@ -1,4 +1,13 @@
-  int singleNumber(vector<int> &nums)
-    {
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        
         return accumulate(nums.begin(), nums.end(), 0, bit_xor<int>());
+
     }
+};
+/*
+https://en.cppreference.com/w/cpp/algorithm/accumulate -->
+https://en.cppreference.com/w/cpp/utility/functional -->
+https://en.cppreference.com/w/cpp/utility/functional/bit_xor
+*/
