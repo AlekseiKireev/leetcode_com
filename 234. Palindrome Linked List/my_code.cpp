7553902697542a13acea94cfff9c58bb8bb964de
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-08-31 23:33:39 +0300

    Update my_code.cpp

--- 234. Palindrome Linked List/my_code.cpp
+++ 234. Palindrome Linked List/my_code.cpp
@@ -8,11 +8,12 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
 class Solution {
 
 private:
 
-int ListSize(ListNode* head){
+  int ListSize(ListNode* head){
 
     int Size = 0;
 
@@ -23,14 +24,93 @@ int ListSize(ListNode* head){
 
     return Size;
 
-}
+  }
 
-void ListReverse(){
-    
+/*
+ 1 -> 2 -> 3 -> nullptr
+ ^      
+ |
+head
+
+ 1 -> 2 -> 3 -> nullptr
+ ^    ^      
+ |    |
+PNH  head
+
+Ввиду следующего графика видим необходимость переброски указателя head->next на HLR,
+но при этом надо не потерять "хвост" в виде "2 -> 3 -> nullptr" -- на него тоже нужен указатель!
+
+nullptr <- 1    2 -> 3 -> nullptr
+
+nullptr <- 1    2 -> 3 -> nullptr
+    ^           ^
+    |           |
+   HLR         head
+
+nullptr <- 1    2 -> 3 -> nullptr
+           ^    ^
+           |    |
+          HLR  head
+   
+nullptr <- 1 <- 2    3 -> nullptr
+
+ nullptr <- 1 <- 2 <- 3
+                      ^      
+                      |
+                     HLR
+
+3 -> 2 -> 1 -> nullptr
+^      
+|
+HLR
+
+head в конечном итоге == nullptr
+*/
+
+  ListNode* ListReverse(ListNode* head){
+      
+    ListNode* HeadListReverse = nullptr; // == HLR
+
+    while(head != nullptr){
+
+        ListNode* PrevNodeHead = head; // == PNH
+        head = head->next;
+
+        PrevNodeHead->next = HeadListReverse;
+        HeadListReverse = PrevNodeHead;
+
+    }
+
+    return HeadListReverse;
+
+  }
+
+ListNode* CentrePalindrome(ListNode* head, int CoordinateCentre){
+
+    while(CoordinateCentre > 0){
+      head = head->next;
+      --CoordinateCentre;
+    }
+
+    return head;
 }
 
 public:
+
     bool isPalindrome(ListNode* head) {
-        
+      
+      int Size = ListSize(head);
+      ListNode* Centre = CentrePalindrome(head, Size / 2);
+      ListNode*  LastNode = ListReverse(Centre);
+
+      while(LastNode != nullptr){
+
+        if(LastNode->val != head->val){return false;}
+        head = head->next;
+        LastNode = LastNode->next;
+      }
+
+      return true;
     }
+
 };
