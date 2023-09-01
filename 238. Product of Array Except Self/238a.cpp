daf02b319f32979073d7dbd978860600307cd418
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-09-01 14:26:30 +0300

    Update 238a.cpp

--- 238. Product of Array Except Self/238a.cpp
+++ 238. Product of Array Except Self/238a.cpp
@@ -11,7 +11,7 @@ public:
    vector<int> productExceptSelf(const vector<int>& a) { 
        vector<int> ans(a.size());
        ans[0] = 1;
-       for (int i = 1; i < (int)a.size(); i++) { 
+       for (int i = 1; i < (int)a.size(); i++) { // make product prefix
            ans[i] = ans[i - 1] * a[i - 1];
        }
        int suffix = 1;
