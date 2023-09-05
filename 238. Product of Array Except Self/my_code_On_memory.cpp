2a604cd40193cffe1cd89435ac531aa06c0559e7
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-09-06 02:33:26 +0300

    Update my_code_On_memory.cpp

--- 238. Product of Array Except Self/my_code_On_memory.cpp
+++ 238. Product of Array Except Self/my_code_On_memory.cpp
@@ -8,10 +8,12 @@ public:
         const int SIZE = nums.size();
 
         /* answer[i] = (prefix[i]) * (suffix[i]) --> При SIZE = 6
+        answer[i] = (prefix[i]) * (suffix[i]) --> При SIZE = 6:
         answer[0] = (1) * (nums[1] * nums[2] * nums[3] * nums[4] * nums[5] * 1)
-        ...
+        answer[1] = (1 * nums[0]) * (nums[2] * nums[3] * nums[4] * nums[5] * 1)
+        answer[2] = (1 * nums[0] * nums[1]) * (nums[3] * nums[4] * nums[5] * 1)
         answer[3] = (1 * nums[0] * nums[1] * nums[2]) * (nums[4] * nums[5] * 1)
-        ...
+        answer[4] = (1 * nums[0] * nums[1] * nums[2] * nums[3]) * (nums[5] * 1)
         answer[5] = (1 * nums[0] * nums[1] * nums[2] * nums[3] * nums[4]) * (1)
 
         prefix.front() = 1
