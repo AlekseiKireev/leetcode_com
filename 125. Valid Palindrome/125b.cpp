ee2c30d4fdb16c028152911a9785d49390906ca3
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-09-06 22:25:07 +0300

    Update 125b.cpp

--- 125. Valid Palindrome/125b.cpp
+++ 125. Valid Palindrome/125b.cpp
@@ -1,41 +1,43 @@
 const int FASTIO = []() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    return 0;
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	return 0;
 }();
 
 class Solution {
 public:
-	bool isAlphaNum(char c) { 
-		return ('A' <= c && c <= 'Z') || ('a' <= c && c <= 'z') || ('0' <= c && c <= '9'); 
+	bool isAlphaNum(char c) {
+		return ('A' <= c && c <= 'Z') || ('a' <= c && c <= 'z') || ('0' <= c && c <= '9');
 	}
 
-char toLower(char c) { 
-	if ('A' <= c && c <= 'Z') { 
-		return char(c - 'A' + 'a'); 
-	} else { 
-		return c; 
-	} 
-}
-
-bool isPalindrome(const string& s) {
-	int i = 0; 
-    int j = (int)s.size() - 1;
-
-	while (true) {
-		while (i < j && !isAlphaNum(s[i])) {
-			i++;
+	char toLower(char c) {
+		if ('A' <= c && c <= 'Z') {
+			return char(c - 'A' + 'a');
 		}
-		while (i < j && !isAlphaNum(s[j])) {
-			j--;
+		else {
+			return c;
 		}
-        
-		if (i < j) {
-            
-			if (toLower(s[i]) != toLower(s[j])) { return false;  }
-            i++; 
-            j--;
-			} else {
+	}
+
+	bool isPalindrome(const string& s) {
+		int i = 0;
+		int j = (int)s.size() - 1;
+
+		while (true) {
+			while (i < j && !isAlphaNum(s[i])) {
+				i++;
+			}
+			while (i < j && !isAlphaNum(s[j])) {
+				j--;
+			}
+
+			if (i < j) {
+
+				if (toLower(s[i]) != toLower(s[j])) { return false; }
+				i++;
+				j--;
+			}
+			else {
 				return true;
 			}
 		}
