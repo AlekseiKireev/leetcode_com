de043fd6b3d1e6869bb8f1172d9834e7f875b899
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-09-06 22:24:34 +0300

    Create 125b.cpp

---
+++ 125. Valid Palindrome/125b.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+	bool isAlphaNum(char c) { 
+		return ('A' <= c && c <= 'Z') || ('a' <= c && c <= 'z') || ('0' <= c && c <= '9'); 
+	}
+
+char toLower(char c) { 
+	if ('A' <= c && c <= 'Z') { 
+		return char(c - 'A' + 'a'); 
+	} else { 
+		return c; 
+	} 
+}
+
+bool isPalindrome(const string& s) {
+	int i = 0; 
+    int j = (int)s.size() - 1;
+
+	while (true) {
+		while (i < j && !isAlphaNum(s[i])) {
+			i++;
+		}
+		while (i < j && !isAlphaNum(s[j])) {
+			j--;
+		}
+        
+		if (i < j) {
+            
+			if (toLower(s[i]) != toLower(s[j])) { return false;  }
+            i++; 
+            j--;
+			} else {
+				return true;
+			}
+		}
+	}
+
+};
