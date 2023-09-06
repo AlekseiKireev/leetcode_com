a339df945d15c9d67d78044f8eb702c4d4977302
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-09-06 11:05:01 +0300

    Create my_code_rec_void.cpp

---
+++ 104. Maximum Depth of Binary Tree/my_code_rec_void.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+
+private:
+
+int MaximumDepth = 0; // подсчет высоты происходит сверху вниз, в то время как в my_code_rec_int.cpp -- снизу вверх
+
+void travel(TreeNode* Root, int height = 0){
+
+  if(Root == nullptr){return;}
+
+  height += 1; // перефраз задачи: просят найти максимальную глубину рекурсии
+  //cout << "height: " << height << '\n';
+  MaximumDepth = max(MaximumDepth, height);
+
+  travel(Root->left, height);
+  /* Wrong Answer 13 / 39 testcases passed Editorial Input root = [1,2] Use Testcase Output 1 Expected 2
+    height += 1; // перефраз задачи: просят найти максимальную глубину рекурсии
+  // cout << "height: " << height << '\n';
+  MaximumDepth = max(MaximumDepth, height);
+  */
+  travel(Root->right, height);
+
+}
+
+public:
+    int maxDepth(TreeNode* root) {
+        travel(root);
+        return MaximumDepth;
+    }
+};
