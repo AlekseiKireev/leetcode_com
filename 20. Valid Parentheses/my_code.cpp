5363f1f3eddc815895bba5a3886356e7f271bdfc
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-09-06 17:38:06 +0300

    Update my_code.cpp

--- 20. Valid Parentheses/my_code.cpp
+++ 20. Valid Parentheses/my_code.cpp
@@ -7,21 +7,21 @@ public:
 
         unordered_map<char, char> OpenToClosed = {{'(', ')'}, {'{', '}'}, {'[', ']'}};
         
-        stack<char> brackets;
+        stack<char> OpenBrackets;
 
         for(int i = 0; i < s.size(); ++i){
             
-            // "!brackets.empty() &&" необходимо на случай, например, таких входных данных: "()()[]" -- то есть при i = 1 уже будет пустым brackets. 
-            // Также при i = 0 позваляет перейти к "brackets.push(s[i]);"
+            // "!OpenBrackets.empty() &&" необходимо на случай, например, таких входных данных: "()()[]" -- то есть при i = 1 уже будет пустым OpenBrackets. 
+            // Также при i = 0 позваляет перейти к "OpenBrackets.push(s[i]);"
             
-            if(!brackets.empty() && s[i] == OpenToClosed[brackets.top()] ){ 
-                brackets.pop(); // удаляем, так как на вершине стека лежит открывающая скобка, в то время как в s[i] лежит соответст-я закрывающая скобка
+            if(!OpenBrackets.empty() && s[i] == OpenToClosed[OpenBrackets.top()] ){ 
+                OpenBrackets.pop(); // удаляем, так как на вершине стека лежит открывающая скобка, в то время как в s[i] лежит соответст-я закрывающая скобка
             }
-            else{ // если "s[i] != OpenToClosed[brackets.top()]", то значит есть, например, конструкция вида: ([]) -- то есть вподряд идет несколько открывающих скобок
-                brackets.push(s[i]);
+            else{ // если "s[i] != OpenToClosed[OpenBrackets.top()]", то значит есть, например, конструкция вида: ([]) -- то есть вподряд идет несколько открывающих скобок
+                OpenBrackets.push(s[i]);
             }
         }
 
-        return brackets.empty();
+        return OpenBrackets.empty();
     }
 };
