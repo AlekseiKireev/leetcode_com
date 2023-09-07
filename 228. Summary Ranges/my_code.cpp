1e249d2c556d991973fa57beb86afa48086013cf
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-09-07 23:46:39 +0300

    Update my_code.cpp

--- 228. Summary Ranges/my_code.cpp
+++ 228. Summary Ranges/my_code.cpp
@@ -7,6 +7,8 @@ public:
         for (int i = 0; i < nums.size(); ++i) {
 
             const int start_ranges = nums[i];
+
+            // while (i + 1 < nums.size() && nums[i + 1]  - nums[i] == 1) { ++i; } // error:
             //  nums[i] < nums[i+1] --> в 1 + nums[i] переполнения не будет, в то
             // время как в nums[i + 1]  - nums[i]  будет 
             while (i + 1 < nums.size() && nums[i + 1] == 1 + nums[i]) { ++i; }
