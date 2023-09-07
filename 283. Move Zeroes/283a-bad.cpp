b5b377e0f01c55a6c1d474c3f540b7d718ad71f1
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-09-07 20:56:32 +0300

    Update 283a-bad.cpp

--- 283. Move Zeroes/283a-bad.cpp
+++ 283. Move Zeroes/283a-bad.cpp
@@ -1,1 +1,14 @@
-
+class Solution {
+public:
+    void moveZeroes(vector<int>& a) {
+        for (int i = 0; i < (int)a.size(); i++) {
+            if (a[i] != 0) {
+                int j = i - 1;
+                while (j >= 0 && a[j] == 0) {
+                    swap(a[j + 1], a[j]);
+                    j--;
+                }
+            }
+        }
+    }
+};
