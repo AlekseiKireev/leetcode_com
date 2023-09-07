f8aeec5f84d809b6b0f4b8878a4d5a12d92164bb
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-09-07 23:23:06 +0300

    Update my_code_with_CountZero.cpp

--- 283. Move Zeroes/my_code_with_CountZero.cpp
+++ 283. Move Zeroes/my_code_with_CountZero.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
-void moveZeroes(vector<int>& nums) {
+	void moveZeroes(vector<int>& nums) {
 
-	int CountZero = 0;
+		int CountZero = 0;
 
-	for (int i = 0; i < nums.size();++i) {
-		if (nums[i] != 0) {
-			nums[i - CountZero] = nums[i];
-			
-		}else{
-            ++CountZero;
-        }
+		for (int i = 0; i < nums.size(); ++i) {
+			if (nums[i] != 0) {
+				nums[i - CountZero] = nums[i];
+
+			}
+			else {
+				++CountZero;
+			}
+		}
+
+		for (int i = nums.size() - CountZero; i < nums.size(); ++i) {
+			nums[i] = 0;
+		}
 	}
-
-	for (int i = nums.size() - CountZero; i < nums.size();++i) {
-		nums[i] = 0;
-	}
-}
 };
