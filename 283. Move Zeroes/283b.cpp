77c39e97c686fd18c1aa55b664c40069742797ba
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-09-07 23:06:08 +0300

    Update 283b.cpp

--- 283. Move Zeroes/283b.cpp
+++ 283. Move Zeroes/283b.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& a) {
-        int i = 0;
+        int i = 0; // откуда пишем
         while (i < (int)a.size() && a[i] != 0) {
             i++;
         }
-        int j = i;
+        int j = i; // куда пишем
         for (; i < (int)a.size(); i++) {
             if (a[i] != 0) {
                 swap(a[i], a[j]);
