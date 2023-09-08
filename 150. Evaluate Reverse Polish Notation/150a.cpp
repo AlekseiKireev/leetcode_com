7e5395ef4bdc96bc80a9746b76a7f12c3b63ad22
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-09-08 13:26:46 +0300

    Update 150a.cpp

--- 150. Evaluate Reverse Polish Notation/150a.cpp
+++ 150. Evaluate Reverse Polish Notation/150a.cpp
@@ -1,33 +1,48 @@
 class Solution {
+
 public:
+
   int evalRPN(const vector<string>& tokens) {
+
     stack<int, vector<int>> stack;
+    
     for (const string& token : tokens) {
+        
       if (token == "+") { 
        assert(stack.size() >= 2); 
        int second = stack.top(); 
        stack.pop();
        stack.top() += second;
-      } else if (token == "-") { 
+
+      } 
+      else if (token == "-") { 
        assert(stack.size() >= 2); 
        int second = stack.top(); 
        stack.pop();
        stack.top() -= second;
-      } else if (token == "*") { 
+
+      } 
+      else if (token == "*") { 
        assert(stack.size() >= 2); 
        int second = stack.top(); 
        stack.pop();
        stack.top() *= second;
-      } else if (token == "/") { 
+
+      } 
+      else if (token == "/") { 
        assert(stack.size() >= 2); 
        int second = stack.top(); 
        stack.pop();
        stack.top() /= second; 
+
       } else {
+
        stack. push(stoi(token));
       }
     }
+
     assert(stack.size() == 1); 
     return stack.top();
   }
+
 };
