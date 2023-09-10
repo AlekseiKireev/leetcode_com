69fb43c561d8b8e9e7e9e75e6b5df975c06ff991
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-09-10 14:13:38 +0300

    Update my_code.cpp

--- 12. Integer to Roman/my_code.cpp
+++ 12. Integer to Roman/my_code.cpp
@@ -15,9 +15,9 @@ public:
         // const array<RomanNum, 13> DescendingRomanNum = { // why error?
         const vector<RomanNum> DescendingRomanNum = {
 
-        {1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"}, {100, "C"},
-        {90, "XC"},  {50, "L"},   {40, "XL"}, {10, "X"},   {9, "IX"},
-        {5, "V"},    {4, "IV"},   {1, "I"}
+        {1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"}, 
+        {100, "C"}, {90, "XC"},  {50, "L"}, {40, "XL"}, 
+        {10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}
 
         }; 
 
