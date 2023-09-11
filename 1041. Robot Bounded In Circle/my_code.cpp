1acc8a53a4fea753dde73bd0f84266f2f7d7923f
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-09-11 22:25:11 +0300

    Update my_code.cpp

--- 1041. Robot Bounded In Circle/my_code.cpp
+++ 1041. Robot Bounded In Circle/my_code.cpp
@@ -75,5 +75,5 @@ The west direction is the negative direction of the x-axis.
     }
 };
 /* // https://ru.wikipedia.org/wiki/%D0%A1%D1%80%D0%B0%D0%B2%D0%BD%D0%B5%D0%BD%D0%B8%D0%B5_%D0%BF%D0%BE_%D0%BC%D0%BE%D0%B4%D1%83%D0%BB%D1%8E#%D0%9A%D0%BB%D0%B0%D1%81%D1%81%D1%8B_%D0%B2%D1%8B%D1%87%D0%B5%D1%82%D0%BE%D0%B2
-L + R = 0 degrees <--> (SideWorld + 1) % 4 + (TMP) % 4 = 0 --> TMP == SideWorld + 3
+L + R = 0 degrees <--> (SideWorld + 1) % 4 + (TMP) % 4 = 0 --> TMP == SideWorld + 3 : SideWorld == 0 started
 */
