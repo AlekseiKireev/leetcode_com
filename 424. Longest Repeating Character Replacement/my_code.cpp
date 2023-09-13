462c6c2fd3e9e565997d9b7b1d118c8be07ab21a
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-09-13 19:55:08 +0300

    Update my_code.cpp

--- 424. Longest Repeating Character Replacement/my_code.cpp
+++ 424. Longest Repeating Character Replacement/my_code.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int characterReplacement(string_view S, int k) {
 
-        int MaxCountRepeatSymb = 0;
+        int maxFrequency = 0;
         int LengthLongestSubstring = -1;
 
         array<int, 26> CharToCountInSuffix; // Suffix лежит в границах [LeftPtr, RightPtr]
@@ -11,14 +11,14 @@ public:
         for (int RightPtr = 0, LeftPtr = 0; RightPtr < S.size(); RightPtr++) {
 
             CharToCountInSuffix[S[RightPtr] - 'A']++;
-            MaxCountRepeatSymb = max(MaxCountRepeatSymb, CharToCountInSuffix[S[RightPtr] - 'A']); // MaxCountRepeatSymb in Suffix
+            maxFrequency = max(maxFrequency, CharToCountInSuffix[S[RightPtr] - 'A']); // maxFrequency in Suffix
 
             int WindowLength = RightPtr - LeftPtr + 1;
-            //if (WindowLength - MaxCountRepeatSymb > k) { // good!
-            while (LeftPtr < RightPtr && WindowLength - MaxCountRepeatSymb > k) { // разве при изменении LeftPtr не надо обновлять MaxCountRepeatSymb?
+            //if (WindowLength - maxFrequency > k) { // good!
+            while (LeftPtr < RightPtr && WindowLength - maxFrequency > k) { // разве при изменении LeftPtr не надо обновлять maxFrequency?
                 --CharToCountInSuffix[S[LeftPtr] - 'A'];
                 LeftPtr++;
-                // MaxCountRepeatSymb = max(MaxCountRepeatSymb, CharToCountInSuffix[S[LeftPtr] - 'A']); // работает с этой строкой, но, работает и без нее!
+                // maxFrequency = max(maxFrequency, CharToCountInSuffix[S[LeftPtr] - 'A']); // работает с этой строкой, но, работает и без нее!
                 WindowLength = RightPtr - LeftPtr + 1;
             }
             
