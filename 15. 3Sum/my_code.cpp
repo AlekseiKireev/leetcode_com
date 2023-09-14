a0404d3efa53535cf6eb4cafc3cb18cf7e294398
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-09-14 22:34:09 +0300

    Update my_code.cpp

--- 15. 3Sum/my_code.cpp
+++ 15. 3Sum/my_code.cpp
@@ -29,8 +29,14 @@ public:
                 if(Target + nums[k] == 0/* &&(Triplets.empty() || Triplets.back() != vector<int>{nums[i], nums[j], nums[k]})*/){ // либо  &&(Triplets.empty() || Triplets.back() != vector<int>{nums[i], nums[j], nums[k]}), либо  while(j + 1 != k && nums[j] == nums[j + 1]){++j;}
                     Triplets.push_back({nums[i], nums[j], nums[k]});
                 }
-                
+
+                /* если для (i,j,k) ВЫПОЛНЯЕТСЯ требуемое, то ввиду того что i -- фиксированно, j уже найдено --> может быть только одно k = -i-j, то нет смысла 
+                   проверять nums[j] == nums[j + 1], так как получим тот же самый результат*/
+                /* если для (i,j,k) НЕ ВЫПОЛНЯЕТСЯ, то ввиду того что i -- фиксированно, для заданного j уже проверенно, что нет подходящих k , то нет смысла 
+                   проверять nums[j] == nums[j + 1], так как получим тот же самый результат*/
                 while(j + 1 != k && nums[j] == nums[j + 1]){++j;} // если закомментировать будет ошибка: Wrong Answer 79 / 312 testcases passed nums = [0,0,0,0] Expected[[0,0,0]]
+                
+                
                 // while(j != k + 1 && nums[k-1] == nums[k]){--k;} // если раскомментировать будет ошибка: Wrong Answer 261 / 312 testcases passed Input nums = [-4,-2,1,-5,-4,-4,4,-2,0,4,0,-2,3,1,-5,0] Expected[[-5,1,4],[-4,0,4],[-4,1,3],[-2,-2,4],[-2,1,1],[0,0,0]]
             }
 
