251ee2b232274a482013f895eaee9b6d47a448ba
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-09-14 16:17:51 +0300

    Update 15a.cpp

--- 15. 3Sum/15a.cpp
+++ 15. 3Sum/15a.cpp
@@ -3,23 +3,24 @@ public:
     vector<vector<int>> threeSum(vector<int>& a) {
         sort(a.begin(), a.end());
         vector<vector<int>> ans;
-        for (int i = 0; i + 2 < (int)a.size(); i++) {
-            if (i > 0 && a[i] == a[i - 1]) {
-                continue;
-            }
-            if (a[i] + a[i + 1] + a[i + 2] > 0) {
-                continue;
-            }
-            if (a[i] + a[a.size() - 1] + a[a.size() - 2] < 0) {
-                continue;
-            }
+        for (int i = 0; i + 2 < (int)a.size(); i++) { // move i [== first ptr] forward
+
+            //___________________________________________________
+            // optimization
+            if (i > 0 && a[i] == a[i - 1]) {continue;}
+            if (a[i] + a[i + 1] + a[i + 2] > 0) {continue;}
+            if (a[i] + a[a.size() - 1] + a[a.size() - 2] < 0) {continue;}
+            //___________________________________________________
+            
             int j = i + 1;
             int k = (int)a.size() - 1;
-            for (int j = i + 1; j < k; j++) {
-                while (j < k - 1 && a[i] + a[j] + a[k] > 0) {
+            for (int j = i + 1; j < k; j++) { // move [j == second ptr] forward
+                
+                while (j < k - 1 && a[i] + a[j] + a[k] > 0) { // move [k == third ptr] backward
                     k--;
                 }
-                assert(i < j&& j < k);
+                
+                assert(i < j && j < k);
                 if (a[i] + a[j] + a[k] == 0) {
                     if (ans.empty() || a[i] != ans.back()[0] || a[j] != ans.back()[1] || a[k] != ans.back()[2]) {
                         ans.push_back({ a[i], a[j], a[k] });
