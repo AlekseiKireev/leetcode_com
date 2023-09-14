17a5fe4a47122e4a3fd3ee30e9fc4283085ecdcf
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-09-14 19:08:58 +0300

    Update 15a_MVP.cpp

--- 15. 3Sum/15a_MVP.cpp
+++ 15. 3Sum/15a_MVP.cpp
@@ -7,7 +7,7 @@ public:
                
         for (int i = 0; i + 2 < (int)a.size(); i++) { // move i [== first ptr] forward
            
-            if (i > 0 && a[i] == a[i - 1]) {continue;}
+            if (i > 0 && a[i] == a[i - 1]) {continue;} // for nums = [-4,-2,-2,-2,0,1,2,2,2,3,3,4,4,6,6] Expected[[-4,-2,6],[-4,0,4],[-4,1,3],[-4,2,2],[-2,-2,4],[-2,0,2]]
 
             int k = (int)a.size() - 1;
             for (int j = i + 1; j < k; j++) { // move j [ == second ptr] forward
