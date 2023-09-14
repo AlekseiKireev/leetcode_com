7491623dcec4b9924c2997eba87ee1fc61440a4e
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-09-14 20:36:50 +0300

    Update my_code.cpp

--- 15. 3Sum/my_code.cpp
+++ 15. 3Sum/my_code.cpp
@@ -8,7 +8,21 @@ public:
 
         if(nums.front() > 0 || nums.back() < 0 ){return Triplets;}
 
-        for(int i = 0; i < nums.size(); ++i){
+        for(int i = 0; i < nums.size() - 2; ++i){ //  nums.size() - 2 <-- "3 <= nums.length <= 3000"
+
+            if(i > 0 && nums[i - 1] == nums[i]){ // если текущий элемент nums[i] равен предыдущему, то по нему не собираем статистику, так как вся возможная статистика была учтена при обработке nums[i - 1]
+                continue;
+            }
+
+            for(int j = i + 1; j < nums.size() - 1; ++j){
+                
+                const int Target = nums[i] + nums[j];
+                if(Target > 0){break;}
+                
+                int k = nums.size() - 1;
+                for(; j < k && Target + nums[k] > 0; --k);
+                if(Target + nums[k] == 0){Triplets.push_back({nums[i], nums[j], nums[k]});}
+            }
 
         }
 
