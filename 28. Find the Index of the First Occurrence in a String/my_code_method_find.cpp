2c078b75a07de93844b7d8ad204c329e277f6bde
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-09-15 16:56:07 +0300

    Update my_code_method_find.cpp

--- 28. Find the Index of the First Occurrence in a String/my_code_method_find.cpp
+++ 28. Find the Index of the First Occurrence in a String/my_code_method_find.cpp
@@ -2,7 +2,9 @@ class Solution {
 public:
     int strStr(string haystack, string needle) {
         
-        const int IDX = haystack.find(needle);
+        if(haystack.size() < needle.size()){return -1;}
+
+        const int IDX = haystack.find(needle); // https://en.cppreference.com/w/cpp/string/basic_string/find
         return ((string::npos == IDX) ? -1: IDX);
 
     }
