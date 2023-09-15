8ef730f8c5a48949e431a50a1cbbfaf19fe7b989
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-09-15 17:31:17 +0300

    Update my_code_method_substr.cpp

--- 28. Find the Index of the First Occurrence in a String/my_code_method_substr.cpp
+++ 28. Find the Index of the First Occurrence in a String/my_code_method_substr.cpp
@@ -10,9 +10,7 @@ public:
         
         // i < haystack.size() - needle.size() + 1 // почему +1? По меньшей мере нужен для теста Input haystack = "abc" needle = "c"  Expected 2
         for(int i = 0; i < haystack.size() - needle.size() + 1; ++i){
-            if(haystack.substr(i, needle.size()) == needle){
-                return i;
-            }
+            if(haystack.substr(i, needle.size()) == needle){return i;}
         }
 
         return -1;
