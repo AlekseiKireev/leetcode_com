e9a7577a035a1a5c48bb749d4754c22e7c2bf119
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-09-16 14:03:20 +0300

    Create my_c0de_functor.cpp

---
+++ 34. Find First and Last Position of Element in Sorted Array/my_c0de_functor.cpp
@@ -0,0 +1,37 @@
+class Solution {
+
+private:
+
+    int BinarySearch(const vector<int>& nums,const int target, function<int (int,int)> Operator,const bool LowerBound){
+
+        int LeftBorder = -1;
+        int RightBorder = nums.size();
+
+        while(1 != RightBorder - LeftBorder){
+                        
+            assert(LeftBorder < RightBorder);
+            int Mid = (LeftBorder + RightBorder) / 2;
+
+            Operator(nums[Mid], target) ? LeftBorder = Mid : RightBorder = Mid;
+            cout << LowerBound << ' ' << LeftBorder << ' ' << RightBorder << '\n';
+        }
+
+        if(LowerBound){return ((nums[RightBorder] != target) ? -1 : RightBorder);}
+        return ((nums[LeftBorder] != target) ? -1 : LeftBorder);
+    }
+
+public:
+
+    vector<int> searchRange(vector<int>& nums, int target) {
+        
+        if( nums.empty() || target < nums.front() || nums.back() < target){return {-1,-1};}
+        
+        return { // https://en.cppreference.com/w/cpp/utility/functional
+            
+            BinarySearch(nums, target, less<int>(),1), //  <
+            BinarySearch(nums, target, less_equal<int>(),0) // <=
+        };
+        
+    }
+
+};
