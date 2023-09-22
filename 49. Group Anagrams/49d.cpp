c95d34bf0721d47339a415065283d485e287073a
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-09-22 21:57:09 +0300

    Update 49d.cpp

--- 49. Group Anagrams/49d.cpp
+++ 49. Group Anagrams/49d.cpp
@@ -1,40 +1,40 @@
-const int ZERO = [](){
+const int ZERO = []() {
 
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
-    
+
     return 0;
 }();
 
 class Solution {
 public:
-  vector<vector<string>> groupAnagrams(const vector<string>& strs) { 
-    unordered_map<string, vector<string>> groups;
-    vector<char> key;
-    for (const string &str : strs) {
-      int histogram[26] = {}; 
-      for (char c : str) { 
-        histogram[c - 'a']++;
-      }
-      key.clear();
-      for (char c = 'a'; c <= 'z'; c++) {
-        int count = histogram[c - 'a'];
-        if (count > 0) { 
-          key.push_back(c);
+    vector<vector<string>> groupAnagrams(const vector<string>& strs) {
+        unordered_map<string, vector<string>> groups;
+        vector<char> key;
+        for (const string& str : strs) {
+            int histogram[26] = {};
+            for (char c : str) {
+                histogram[c - 'a']++;
+            }
+            key.clear();
+            for (char c = 'a'; c <= 'z'; c++) {
+                int count = histogram[c - 'a'];
+                if (count > 0) {
+                    key.push_back(c);
+                }
+                if (count > 1) {
+                    assert(count < 256 * 256);
+                    key.push_back(count >> 8);
+                    key.push_back(count & 0xFF);
+                }
+            }
+            groups[string(key.begin(), key.end())].push_back(str);
         }
-        if (count > 1) {
-          assert(count < 256 * 256);
-          key.push_back(count >> 8);
-          key.push_back(count & 0xFF);
+        vector<vector<string>> ans;
+        ans.reserve(groups.size());
+        for (auto& kv : groups) {
+            ans.push_back(move(kv.second));
         }
-      }
-      groups[string(key.begin(), key.end())].push_back(str);
+        return ans;
     }
-    vector<vector<string>> ans; 
-    ans.reserve(groups.size()); 
-    for (auto &kv : groups) {
-      ans.push_back(move(kv.second));
-    }
-    return ans;
-  }
 };
