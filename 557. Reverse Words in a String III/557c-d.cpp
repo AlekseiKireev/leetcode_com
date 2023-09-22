091f86325212600147e73b0f469f9453d91e9952
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-09-22 23:53:48 +0300

    Update 557c-d.cpp

--- 557. Reverse Words in a String III/557c-d.cpp
+++ 557. Reverse Words in a String III/557c-d.cpp
@@ -1,1 +1,39 @@
+const int ZERO = []() { // 557c.cpp
 
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    return 0;
+}();
+
+class Solution {
+
+    void reverse(string::iterator first, string::iterator after) { // 557d.cpp
+        while (true) {
+            if (first == after) {
+                return;
+            }
+            --after;
+            if (first == after) {
+                return;
+            }
+            swap(*first, *after);
+            ++first;
+        }
+    }
+
+
+public:
+    string reverseWords(string& s) {
+        string::iterator wordStart = s.begin();
+        for (string::iterator wordEnd = s.begin(); wordEnd != s.end(); ++wordEnd) {
+            if (*wordEnd == ' ') {
+                reverse(wordStart, wordEnd);
+                wordStart = wordEnd;
+                ++wordStart;
+            }
+        }
+        reverse(wordStart, s.end());
+        return move(s);
+    }
+};
