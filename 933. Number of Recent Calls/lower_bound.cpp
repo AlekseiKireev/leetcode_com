dc9b0fb70abf25d3cafe3c48df0b3fad5c5284c9
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-09-23 19:24:07 +0300

    Create lower_bound.cpp

---
+++ 933. Number of Recent Calls/lower_bound.cpp
@@ -0,0 +1,26 @@
+class RecentCounter {
+
+private:
+
+vector<int> Pings; // "1 <= t <= 10^9"
+
+public:
+    RecentCounter() { // "At most 10^4 calls will be made to ping."
+        Pings.reserve(1e4);
+    }
+    
+    int ping(int t) {
+
+        Pings.push_back(t);
+
+        // "Each test case will call ping with strictly increasing values of t."
+        return Pings.end() - lower_bound(Pings.begin(), Pings.end(), t - 3000);
+    }
+
+};
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter* obj = new RecentCounter();
+ * int param_1 = obj->ping(t);
+ */
