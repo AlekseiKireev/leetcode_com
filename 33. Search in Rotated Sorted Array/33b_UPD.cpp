eb8de93828c2230ce688482f5114bbe31e313320
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-09-24 01:31:21 +0300

    Update 33b_UPD.cpp

--- 33. Search in Rotated Sorted Array/33b_UPD.cpp
+++ 33. Search in Rotated Sorted Array/33b_UPD.cpp
@@ -27,7 +27,7 @@ public:
             }
             
             if (a[sortedLeft] <= target && target < a[sortedRight]) { 
-                auto It = lower_bound(a.begin() + sortedLeft, a.begin() + sortedRight + 1, target); // " - 1" 
+                auto It = lower_bound(a.begin() + sortedLeft, a.begin() + sortedRight + 1, target); // " + 1" 
                 return (*It == target ? It - a.begin() : -1);
             }
             else {
