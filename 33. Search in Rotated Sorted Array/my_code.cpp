be86eb28d4e6b48a04fe555b7ed15bf5c591e729
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-09-24 14:18:51 +0300

    Update my_code.cpp

--- 33. Search in Rotated Sorted Array/my_code.cpp
+++ 33. Search in Rotated Sorted Array/my_code.cpp
@@ -1,1 +1,62 @@
+class Solution {
 
+private:
+
+int BinarySearch(const vector<int>& a, int target) {
+
+        int left = 0;
+        int right = (int)a.size() - 1;
+
+        while (1 != right - left) {
+                       
+            int mid = (left + right) / 2;
+            assert(mid != left && mid != right);
+
+            int sortedLeft, sortedRight, secondLeft, secondRight;
+            if (a[left] <= a[mid]) { // is_sorted({left, ..., mid}) == true
+                sortedLeft = left;
+                sortedRight = mid;
+                secondLeft = mid;
+                secondRight = right;
+            }
+            else { // в диапозоне [mid + 1, ..., left*, ..., right] существует is_sorted({left*, ..., right}) == true
+                sortedLeft = mid;
+                sortedRight = right;
+                secondLeft = left;
+                secondRight = mid;
+            }
+            
+            if (a[sortedLeft] <= target && target < a[sortedRight]) { // знак "<" в "target < a[sortedRight]" необходим ввиду поддержки инварианта "a[sortedLeft] <= target " чтобы позже обработать случай "(a[left] == target"
+                auto It = lower_bound(a.begin() + sortedLeft, a.begin() + sortedRight + 1, target); // " + 1" чтобы поиск была в границах [a.begin() + sortedLeft, a.begin() + sortedRight]
+                return (*It == target ? It - a.begin() : -1);
+            }
+            else {
+                left = secondLeft;
+                right = secondRight;
+            }
+
+            if(a[left] == target) {return left;}
+        }
+    
+    return  -1;
+}
+
+public:
+    int search(const vector<int>& a, int target) {
+
+        // 3 последующих оператора обработывают случаи вида: Wrong Answer 176 / 195 Input nums = [1,3] target = 3
+        // это необходимо для того, чтобы изначально в BinarySearch указатели были указаны на "int left = 0; int right = (int)a.size() - 1;"
+        if (a.front() == target ) {return 0;}
+        if (a.size() == 1) {return -1;}
+        if (a.back() == target) {return (int)a.size() - 1;}
+
+        if(a.front() < a.back()) { // is_sorted(nums) == true
+            auto It = lower_bound(a.begin(), a.end(), target); 
+            return (It == a.end() || *It != target ? -1 : It - a.begin());
+        }
+
+        const int Idx = BinarySearch(a, target);
+        return  Idx;
+
+    }
+};
