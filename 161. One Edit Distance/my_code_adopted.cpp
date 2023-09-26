d22665f2c3cda9206754aa826b440ff64d5f6b00
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-09-26 10:43:25 +0300

    Update my_code_adopted.cpp

--- 161. One Edit Distance/my_code_adopted.cpp
+++ 161. One Edit Distance/my_code_adopted.cpp
@@ -1,1 +1,65 @@
+class Solution {
 
+private:
+    int MinLeng;
+    int MaxLeng;
+
+private:
+    // insertion, deletion or substitution
+
+    bool Replace(string_view First, string_view Second) {
+
+        int CountDifference = 0;
+
+        for (int i = 0; i < MinLeng; ++i) {
+
+            CountDifference += (First[i] != Second[i]);
+            if (CountDifference > 1) { return false; }
+        }
+
+        return (CountDifference == 1);
+
+    }
+    /*
+        bool Delete(string_view  First, string_view  Second) { // == Replace
+
+            for (int i = 0; i < MinLeng; ++i) {
+
+                if (First[i] != Second[i]) { // delete Second[i]
+                    return (First.substr(i) == Second.substr(i + 1));
+                }
+            }
+
+            return true; // ex: abc abcd -- во втором слове удаляем d
+        }
+    */
+    bool Insert(string_view First, string_view Second) {
+
+        for (int i = 0; i < MinLeng; ++i) {
+
+            if (First[i] != Second[i]) { // вставляем Second[i] в слово First на i позицию, все остальные буквы слова сдвигаем на "+1" вправа
+                return (First.substr(i) == Second.substr(i + 1));
+            }
+        }
+
+        return true; // ex: abc abcd -- в первое слово вставляем d
+    }
+
+public:
+
+    bool isOneEditDistance(string_view First, string_view Second) {
+
+        MinLeng = First.size();
+        MaxLeng = Second.size();
+
+        if (MinLeng > MaxLeng) { return isOneEditDistance(Second, First); }
+        assert(MinLeng <= MaxLeng);
+
+        if (MaxLeng - MinLeng > 1) { return false; }
+
+        assert(MinLeng == MaxLeng || MinLeng == MaxLeng - 1);
+        return ((MinLeng == MaxLeng) ? Replace(First, Second) : (/*Delete(First, Second) || */Insert(First, Second)));
+
+    }
+
+};
