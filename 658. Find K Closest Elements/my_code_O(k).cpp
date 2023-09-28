1a5e996d6fc67e79e368502489d372ee3e0e0e48
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-09-28 18:31:14 +0300

    Create my_code_O(k).cpp

---
+++ 658. Find K Closest Elements/my_code_O(k).cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+
+        int l = 0, r = arr.size() - 1;
+        while (r - l + 1 != k) {
+            if (x - arr[l] <= arr[r] - x) {
+                --r;
+            } else {
+                ++l;
+            }
+        } // l и r указывают на концы искомого вектора --> при возврате надо дописать " + 1"
+
+        return vector<int>(arr.begin() + l, arr.begin() + r + 1);
+    }
+};
