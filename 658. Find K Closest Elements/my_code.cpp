6d6d8333b24b5ac3736ccd5d7af35a544b531447
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-09-28 16:16:54 +0300

    Update my_code.cpp

--- 658. Find K Closest Elements/my_code.cpp
+++ 658. Find K Closest Elements/my_code.cpp
@@ -1,32 +1,38 @@
 class Solution {
 public:
-    vector<int> findClosestElements(const vector<int>& arr, int k, const int x) {
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        
+        auto ItCheck_x = lower_bound(arr.begin(), arr.end(), x);
 
-        // [0,0,1,2,3,3,4,7,7,8] 3 5 // вся соль в повторя-я 7
-        auto It = lower_bound(arr.begin(), arr.end(), x);
-
-        if(It == arr.begin()){return {arr.begin(), arr.begin() + k};} // My ex 2
-        if(It == arr.end() || It == prev(arr.end())){return {arr.end() - k, arr.end()};} // My ex 3
-
+        if(ItCheck_x == arr.begin()){return {arr.begin(), arr.begin() + k};} // My ex 2 / 2.1
+        if(ItCheck_x == arr.end()){return {arr.end() - k, arr.end()};} // My ex 3
 
         int LeftPtr, RightPtr;
+        if(*ItCheck_x == x){
+            LeftPtr = RightPtr = ItCheck_x - arr.begin();
+        }else{ // *prev(ItCheck_x) < x < *ItCheck_x
 
-        // [1,3] 1 2
-        // 
-        if(*It == x || abs(*prev(It) - x) == abs(*It - x) ){
-            LeftPtr = RightPtr = It - arr.begin() - 1;
-        }else{
-            LeftPtr = RightPtr = It - arr.begin();
+            if( abs(*prev(ItCheck_x) - x) == abs(*ItCheck_x - x) ){ 
+
+                LeftPtr = RightPtr = ItCheck_x - arr.begin() - 1; // "-1" <-- [1,3] 1 2
+
+            }else{
+
+                if(abs(*prev(ItCheck_x) - x) < abs(*ItCheck_x - x)){ 
+                    LeftPtr = RightPtr = ItCheck_x - arr.begin() - 1; // "-1" <-- [0,0,1,2,3,3,4,7,7,8] 3 5
+                }else{
+                    LeftPtr = RightPtr = ItCheck_x - arr.begin();
+                }
+
+            }
+
         }
-
         
         cout << LeftPtr << ' ' << RightPtr << '\n';
-        if(k == 1){return {arr.begin() + LeftPtr, arr.begin() + RightPtr + 1};}
-        
-        while(k != RightPtr - LeftPtr + 1){
-        //for(;k != 0;--k){
 
-            if( RightPtr >= arr.size() - 1 || 
+        while(k != RightPtr - LeftPtr + 1){
+
+            if( RightPtr == arr.size() - 1 || 
                 LeftPtr > 0 && 
                 (
                 abs(arr[LeftPtr - 1] - x) < abs(arr[RightPtr + 1] - x) || 
@@ -38,11 +44,14 @@ public:
             }else{
                 ++RightPtr;
             }
+
+            if(LeftPtr == 0){break;}
         }
 
         cout << LeftPtr << ' ' << RightPtr;
 
 
-        return {arr.begin() + LeftPtr, arr.begin()  + RightPtr + 1};
+        return {arr.begin() + LeftPtr, arr.begin()  + LeftPtr + k};                        
+
     }
 };
