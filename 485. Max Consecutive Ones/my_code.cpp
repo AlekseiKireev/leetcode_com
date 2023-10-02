347384825d0cb0e49fb26ae8354777db837f6639
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-10-02 23:22:59 +0300

    Update my_code.cpp

--- 485. Max Consecutive Ones/my_code.cpp
+++ 485. Max Consecutive Ones/my_code.cpp
@@ -39,3 +39,27 @@ public:
     }
 };
 */
+
+/*
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        
+        int Ans = 0;
+        int CurrentSum = 0;
+        for(int num : nums){
+            if(num == 1){
+                ++CurrentSum;
+                Ans = max(Ans, CurrentSum); // ex: [1,1,1,1,1]
+            }else{ // num == 0
+                
+                CurrentSum = 0; // ex: [1,1,0,1,1,1]
+            }
+            
+        }
+
+        return Ans; 
+
+    }
+};
+*/
