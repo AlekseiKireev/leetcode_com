d480457492fd4110e9f4264fff0120df700bfea2
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-10-02 21:45:04 +0300

    Update my_code.cpp

--- 340. Longest Substring with At Most K Distinct Characters/my_code.cpp
+++ 340. Longest Substring with At Most K Distinct Characters/my_code.cpp
@@ -1,63 +1,67 @@
-class Solution{
-  
-  private:
-  
-    const int SIZE_ALPHABET = 26;
-      
-  public:
-  
-    int longestKSubstr(string_view s, int k) { // "0 <= k <= 50"
-        
-        array<int, SIZE_ALPHABET> CharToCount; CharToCount.fill(0);
-        
-        int LengthSubstr = -1;
-        
-        for(int RightBorderWindow = 0, LeftBorderWindow = 0; RightBorderWindow <=  nums.size() - 1; ++RightBorderWindow){ // move Right ptr
-          
-           if(++CharToCount[RightBorderWindow] == 1){ // новая буква (CharToCount[RightBorderWindow]) в sliding window
-                --k;
-            }
-          
-          /* // лучше не сдвигать двумя способами правй указатель, не удобно / понижает читабельность / повышает вероятность ошибки
-            while(RightBorderWindow < nums.size() - 1 && k >= 0){
-                
-                    if(++CharToCount[RightBorderWindow] == 1){ // новая буква (CharToCount[RightBorderWindow]) в sliding window
-                        --k;
-                    }
-                    ++RightBorderWindow;
-                
-            } // RightBorderWindow указывет на последний или за последний символ sliding window ????
-            */
-            
-            while(LeftBorderWindow < RightBorderWindow && k < 0){ // move Left ptr
-                              
-                if(--CharToCount[LeftBorderWindow] == 0){
+#include <iostream>
+#include <string>
+#include<string_view>
+#include <array>
+
+using namespace std;
+
+constexpr int SIZE_ALPHABET = 26;
+
+class Solution {
+
+private:
+
+    //constexpr int SIZE_ALPHABET = 26; // 'Solution::SIZE_ALPHABET' cannot be declared with 'constexpr' specifier // почему так?
+    int LengthSubstr = 0;
+public:
+
+    int lengthOfLongestSubstringKDistinct(string& s, int k) { // "0 <= k <= 50"
+
+        if(k <= 0){ return LengthSubstr; }
+
+
+        array<int, SIZE_ALPHABET> CharToCount; CharToCount.fill(0);        
+
+        for (int RightBorderWindow = 0, LeftBorderWindow = 0; RightBorderWindow <= s.size() - 1; ++RightBorderWindow) { // move Right ptr
+
+            //if (++CharToCount[s[RightBorderWindow] - 'a'] == 1) { // новая буква (CharToCount[RightBorderWindow]) в sliding window
+            //    --k;
+            //} // RightBorderWindow указывет на последний символ sliding window 
+
+             // лучше не сдвигать двумя способами правй указатель, не удобно / понижает читабельность / повышает вероятность ошибки
+              while(RightBorderWindow < s.size() - 1 && k > 0){
+
+                      if(++CharToCount[RightBorderWindow] == 1){ // новая буква (CharToCount[RightBorderWindow]) в sliding window
+                          --k;
+                      }
+                      ++RightBorderWindow;
+
+              } // RightBorderWindow указывет на последний символ sliding window
+              
+
+            while (LeftBorderWindow < RightBorderWindow && k < 0) { // move Left ptr
+
+                if (--CharToCount[s[LeftBorderWindow] - 'a'] == 0) {
                     ++k;
                 }
                 ++LeftBorderWindow;
             } // LeftBorderWindow указывет на первый символ sliding window
-            
+
             LengthSubstr = max(LengthSubstr, RightBorderWindow - LeftBorderWindow + 1);
-                
+
         }
-        
+
         return LengthSubstr;
     }
-    
+
 };
 
-/*
-НАПИСАТЬ ТЕСТЫ
-
-"aaaa" 0
-0
-"aaaa" 1
-4
-"aaaa" 2 
-4
-"aaaa" 4
-4
-"aaaa" 42 
-4
-*/
+//int main() {
+//    cout << Solution().lengthOfLongestSubstringKDistinct("aaaa", 0) << '\n';
+//    cout << Solution().lengthOfLongestSubstringKDistinct("aaaa", 1) << '\n';
+//    cout << Solution().lengthOfLongestSubstringKDistinct("aaaa", 2) << '\n';
+//    cout << Solution().lengthOfLongestSubstringKDistinct("aaaa", 4) << '\n';
+//    cout << Solution().lengthOfLongestSubstringKDistinct("aaaa", 42) << '\n';
+//
+//}
 
