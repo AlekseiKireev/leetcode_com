eebec15561936a9da7c101667489f7b7f241c479
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-10-02 21:52:01 +0300

    Update my_code.cpp

--- 253. Meeting Rooms II/my_code.cpp
+++ 253. Meeting Rooms II/my_code.cpp
@@ -1,50 +1,82 @@
+//#include <iostream>
+//#include <vector>
+//#include<algorithm>
+//
+//using namespace std; 
+
 class Solution {
-    
- private:
- 
-    struct Event{
-      
-      int Val;
-      int Type;
-      
-      bool operator<(const Event L, const Event R) const {
-          return (L.Val == R.Val ? L.Type < R.Type : L.Val < R.Val);
-      }
 
-    };     
+private:
 
- private:
- 
-  int MNCR = 0; // Minimum Number of Conference Rooms 
-     
- public:
- 
-    int minMeetingRooms(vector<vector<int>>& intervals) {
-    
-        vector<Event> events; events.reserve(2 * intervals.size()); // "2 * " <-- укладывания концов в intervals в events 
-        
-        for(int i = 0; i < intervals.size(); ++i){
-            
-            const auto event = events[intervals.size() - 1 - i];
-            events.pop_back();
-            
-            events.push_back({event.front(), 1});
-            events.push_back({event.back(), -1});
-            
-        }
-        
-        sort(events.begin(), events.end());
-        
-        int CurrentNCR = 0;
-        
-        for(const auto [val, type] : events){
-            CurrentNCR += type;
-            max(MNCR, CurrentNCR);
-        }
-        
-        
-        return MNCR;
-    
+    struct Event {
+
+        int Val;
+        int Type;
+
+    };
+
+    friend bool operator<(const Event L, const Event R) {
+        return (L.Val == R.Val ? L.Type < R.Type : L.Val < R.Val);
     }
-  
+
+private:
+
+    int MNCR = 0; // Minimum Number of Conference Rooms 
+
+public:
+
+    int minMeetingRooms(vector<vector<int>>& intervals) {
+
+        const int COUNT_INTERVALS = intervals.size();
+        vector<Event> events; events.reserve(2 * COUNT_INTERVALS); // "2 * " <-- укладывания концов в intervals в events 
+
+        // for (int i = 0; i < intervals.size(); ++i) { // error!
+        for (int i = 0; i < COUNT_INTERVALS; ++i) {
+
+            const auto event = intervals.back();
+            intervals.pop_back();
+            events.push_back({ event.front(), 1 });
+            events.push_back({ event.back(), -1 });
+
+        }
+
+        sort(events.begin(), events.end());
+
+        int CurrentNCR = 0;
+
+        for (const auto [val, type] : events) {
+            CurrentNCR += type;
+            MNCR = max(MNCR, CurrentNCR);
+        }
+
+
+        return MNCR;
+
+    }
+
 };
+
+/*
+
+вопрос: почему если конструкцию
+
+    struct Event {
+
+        int Val;
+        int Type;
+
+    };
+
+    const bool operator<(const Event L, const Event R) {
+        return (L.Val == R.Val ? L.Type < R.Type : L.Val < R.Val);
+    }
+
+держать в приватной секции, будет ошибка too many parameters for this operator function
+
+А если вынести ее из класса Solution, то ошибка исчезает?
+
+
+
+Потому что внутри класса по-умолчанию объявляются member function с неявным параметром this. Получается три параметра: this, L, R; можно объявить её friend
+https://en.cppreference.com/w/cpp/language/operator_comparison
+*/
