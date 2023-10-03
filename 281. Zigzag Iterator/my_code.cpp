bb427e4be2eb67ee910ebac2428f059dcd29c604
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-10-03 20:20:29 +0300

    Update my_code.cpp

--- 281. Zigzag Iterator/my_code.cpp
+++ 281. Zigzag Iterator/my_code.cpp
@@ -1,3 +1,4 @@
+/*
 class ZigzagIterator {
  public:
   ZigzagIterator(vector<int>& v1, vector<int>& v2) {
@@ -12,3 +13,39 @@ class ZigzagIterator {
     
   }
 };
+*/
+
+class ZigzagIterator {
+
+private:
+
+struct Pair{
+    
+    const vector<int>::iterator CurrentItr;
+    const vector<int>::iterator EndItr;
+    
+};
+
+private:
+
+    queue<Pair> GetZigzag;
+
+public:
+    ZigzagIterator(const vector<int>& v1, const vector<int>& v2) { // 0 <= v1.length, v2.length <= 1000        
+        
+        if(v1.empty() == false){GetZigzag.push({v1.cbegin(), v1.cend()});}
+        if(v2.empty() == false){GetZigzag.push({v2.cbegin(), v2.cend()});}
+        
+    }
+
+    int next() {
+        
+        
+        const auto CurItr = 
+        return ;
+    }
+
+    bool hasNext() { return !(GetZigzag.empty()); }
+    
+}; 
+       
