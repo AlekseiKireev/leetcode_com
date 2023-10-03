08b59ea134c3ca121b6e61fb7d2f2740f90889b4
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-10-04 01:15:00 +0300

    Create my_code_queue.cpp

---
+++ 933. Number of Recent Calls/my_code_queue.cpp
@@ -0,0 +1,27 @@
+class RecentCounter {
+
+private:
+
+queue<int> Pings;
+
+private:
+
+    void clear(int timestamp) {
+        while(Pings.empty() == false && Pings.front() + 3000 <  timestamp){Pings.pop();}
+    }
+
+public:
+    RecentCounter() {}
+    
+    int ping(int t) {
+        Pings.push(t);
+        clear(t);
+        return Pings.size();
+    }
+};
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter* obj = new RecentCounter();
+ * int param_1 = obj->ping(t);
+ */
