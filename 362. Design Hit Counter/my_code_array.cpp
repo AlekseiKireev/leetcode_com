7150b7a9d6f3b8c4b38d7ee4b0abe5e90d7219b1
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-10-04 15:56:44 +0300

    Create my_code_array.cpp

---
+++ 362. Design Hit Counter/my_code_array.cpp
@@ -0,0 +1,34 @@
+constexpr int NUMBER_SECONDS = 300;
+
+class HitCounter {
+
+
+public:
+
+    HitCounter() {
+        timestamps.fill(0);
+        hits.fill(0);
+    }
+
+    void hit(int timestamp) { // "All the calls are being made to the system in chronological order (i.e., timestamp is monotonically increasing)."
+
+        const int i = timestamp % 300;
+        timestamps[i] = timestamp;
+        hits[i] = 1; 
+        
+    }
+
+    int getHits(int timestamp) {
+        int countHits = 0;
+
+        for (int i = 0; i < 300; ++i) {
+            if (timestamp - timestamps[i] < 300) { countHits += hits[i]; }
+        }
+
+        return countHits;
+    }
+
+private:
+    array<int, NUMBER_SECONDS> timestamps;
+    array<int, NUMBER_SECONDS> hits;
+};
