01c5abf9691cde52fa513f17c462653002641a2f
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-10-05 22:15:15 +0300

    Create my_code_MVP.cpp

---
+++ 1004. Max Consecutive Ones III/my_code_MVP.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int CountUpdZero) {       
+        
+        int Ans = 0;
+
+        for(int RightBorderWindow = 0, LeftBorderWindow = 0; RightBorderWindow < nums.size(); ++RightBorderWindow){ // сдвиг правого указателя
+
+            if(nums[RightBorderWindow] == 0){--CountUpdZero;}
+
+            while(CountUpdZero < 0){ // сдвиг левого указателя
+                if(nums[LeftBorderWindow++] == 0){++CountUpdZero;}
+            }
+
+            Ans = max(Ans, RightBorderWindow - LeftBorderWindow  + 1);
+        }
+
+        return Ans;
+    }
+};
