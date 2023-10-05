14318f023978dd5e53e67ed17555a5ec3699f25c
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-10-05 22:28:16 +0300

    Update my_code_MVP.cpp

--- 1004. Max Consecutive Ones III/my_code_MVP.cpp
+++ 1004. Max Consecutive Ones III/my_code_MVP.cpp
@@ -12,6 +12,9 @@ public:
                 if(nums[LeftBorderWindow++] == 0){++CountUpdZero;}
             }
 
+            // if(CountUpdZero != 0){ assert(LeftBorderWindow < RightBorderWindow); } // error: nums = [1,1,1,0,0,0,1,1,1,1,0] k = 2
+            if(CountUpdZero != 0){ assert(LeftBorderWindow <= RightBorderWindow); } // LeftBorderWindow == RightBorderWindow по меньшей мере в самом начале, когда указывают на первый элемент
+            
             // В отрезке [LeftBorderWindow, RightBorderWindow] содержится максимум CountUpdZero нулей
             Ans = max(Ans, RightBorderWindow - LeftBorderWindow  + 1);
         }
