e0e279be8f9bebfaf48947d0c79b13c058bcffa6
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-10-06 22:58:42 +0300

    Update my_code_func_Add.cpp

--- 1008. Construct Binary Search Tree from Preorder Traversal/my_code_func_Add.cpp
+++ 1008. Construct Binary Search Tree from Preorder Traversal/my_code_func_Add.cpp
@@ -1,1 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 
+class Solution {
+
+private:
+
+    TreeNode* Add(TreeNode* BST, int Value){
+
+        if(BST == nullptr){return (new TreeNode(Value));}
+
+        if(Value < BST->val){BST->left = Add(BST->left, Value);}
+        if(BST->val < Value){BST->right = Add(BST->right, Value);}
+
+        return BST;
+    }
+
+public:
+
+    TreeNode* bstFromPreorder(vector<int>& preorder) {
+        
+        TreeNode* BST = nullptr;
+
+        for(const int Value : preorder){
+            BST = Add(BST, Value);
+        }
+
+        return BST;
+    }
+
+};
