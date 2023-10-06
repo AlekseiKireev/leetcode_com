50ffa4ebe499939691d6fbef60b0c51c4a613a9a
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-10-07 00:43:15 +0300

    Update 53a_my_UPD.cpp

--- 53. Maximum Subarray/53a_my_UPD.cpp
+++ 53. Maximum Subarray/53a_my_UPD.cpp
@@ -1,1 +1,17 @@
+class Solution {
 
+public:
+  int maxSubArray(const vector<int>& a) {
+    
+    int CurrentMaxSuffixSum = a[0];
+    int MaxSuffixSum = a[0];
+    
+    for (size_t i = 1; i < a.size(); i++) {
+     CurrentMaxSuffixSum = max(CurrentMaxSuffixSum + a[i], a[i]);
+     MaxSuffixSum = max(MaxSuffixSum, CurrentMaxSuffixSum);
+    }
+    
+    return MaxSuffixSum;
+    
+  }
+};
