eed16308c60652610068bf6348dfec5089230083
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-10-06 17:53:29 +0300

    Create my_code_Preorder.cpp

---
+++ 449. Serialize and Deserialize BST/my_code_Preorder.cpp
@@ -0,0 +1,57 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Codec {
+public:
+
+    string serialize(TreeNode* root) {
+        return Preorder(root);
+    }
+
+    TreeNode* deserialize(string data) {
+
+        stringstream ss(data);        
+        TreeNode* root = nullptr;
+        
+        for (string item; getline (ss, item, '-');) {
+            root = Add(root, stoi(item));
+        }
+            
+        return root;
+    }
+    
+private:
+
+    string Preorder(TreeNode* root) {
+        if (!root) {return {};}
+        return to_string(root->val) + "-" + Preorder(root->left) + Preorder(root->right);
+    }
+    
+    TreeNode* Add(TreeNode* root, int Value) {
+        
+        if (root == nullptr) {return new TreeNode(Value);;}
+    
+		if (Value < root->val){
+            root->left=Add(root->left,Value);
+        }
+    
+		if (root->val < Value){
+			root->right=Add(root->right,Value);
+        }
+		return root;
+    }
+};
+
+// Your Codec object will be instantiated and called as such:
+// Codec* ser = new Codec();
+// Codec* deser = new Codec();
+// string tree = ser->serialize(root);
+// TreeNode* ans = deser->deserialize(tree);
+// return ans; 
