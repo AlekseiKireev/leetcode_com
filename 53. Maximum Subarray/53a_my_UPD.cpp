36cb3e70bbeea3bcab0d6b84d5adcbe99c447b7e
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-10-07 00:53:18 +0300

    Update 53a_my_UPD.cpp

--- 53. Maximum Subarray/53a_my_UPD.cpp
+++ 53. Maximum Subarray/53a_my_UPD.cpp
@@ -3,8 +3,9 @@ class Solution {
 public:
     int maxSubArray(const vector<int>& nums) {
 
-        int CurrentMaxAffixSum = nums[0];
-        int TotalMaxAffixSum = nums[0];
+        // "nums.front()" существует ввиду "1 <= nums.length <= 10^5"
+        int CurrentMaxAffixSum = nums.front();
+        int TotalMaxAffixSum = nums.front();
 
         for (size_t i = 1; i < nums.size(); i++) {
             CurrentMaxAffixSum = max(CurrentMaxAffixSum + nums[i], nums[i]);
