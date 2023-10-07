d3ba7c39bc4bcb038dd5565a348ff389a4971be8
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-10-08 00:13:43 +0300

    Update 22b.cpp

--- 22. Generate Parentheses/22b.cpp
+++ 22. Generate Parentheses/22b.cpp
@@ -25,6 +25,8 @@ void generateParentheses(vector<char>& cur, int toOpen, int toClose, vector<stri
     }
 }
 
+// Зачем double?
+// https://neerc.ifmo.ru/wiki/index.php?title=%D0%A7%D0%B8%D1%81%D0%BB%D0%B0_%D0%9A%D0%B0%D1%82%D0%B0%D0%BB%D0%B0%D0%BD%D0%B0#.D0.90.D0.BD.D0.B0.D0.BB.D0.B8.D1.82.D0.B8.D1.87.D0.B5.D1.81.D0.BA.D0.B0.D1.8F_.D1.84.D0.BE.D1.80.D0.BC.D1.83.D0.BB.D0.B0
 double factorial(int n) {
     if (n == 0) {return 1;}
     else {return n * factorial(n - 1);}
