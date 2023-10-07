a18975844256549fbeb8c37a70a53f18f6891223
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-10-08 01:07:48 +0300

    Update my_code_with_pop_back.cpp

--- 22. Generate Parentheses/my_code_with_pop_back.cpp
+++ 22. Generate Parentheses/my_code_with_pop_back.cpp
@@ -14,24 +14,24 @@ private:
     }
 
     // https://neerc.ifmo.ru/wiki/index.php?title=%D0%9F%D1%80%D0%B0%D0%B2%D0%B8%D0%BB%D1%8C%D0%BD%D1%8B%D0%B5_%D1%81%D0%BA%D0%BE%D0%B1%D0%BE%D1%87%D0%BD%D1%8B%D0%B5_%D0%BF%D0%BE%D1%81%D0%BB%D0%B5%D0%B4%D0%BE%D0%B2%D0%B0%D1%82%D0%B5%D0%BB%D1%8C%D0%BD%D0%BE%D1%81%D1%82%D0%B8#.D0.A0.D0.B5.D0.BA.D1.83.D1.80.D1.81.D0.B8.D0.B2.D0.BD.D1.8B.D0.B9_.D0.B0.D0.BB.D0.B3.D0.BE.D1.80.D0.B8.D1.82.D0.BC_.D0.BF.D0.BE.D0.BB.D1.83.D1.87.D0.B5.D0.BD.D0.B8.D1.8F_.D0.BB.D0.B5.D0.BA.D1.81.D0.B8.D0.BA.D0.BE.D0.B3.D1.80.D0.B0.D1.84.D0.B8.D1.87.D0.B5.D1.81.D0.BA.D0.BE.D0.B3.D0.BE_.D0.BF.D0.BE.D1.80.D1.8F.D0.B4.D0.BA.D0.B0 
-    void MakeBalancedBracket(int ToOpen, int ToClose){
+    void MakeBalancedBracket(int CountOpenBracket, int CountCloseBracket){
 
         // https://neerc.ifmo.ru/wiki/index.php?title=%D0%9F%D1%80%D0%B0%D0%B2%D0%B8%D0%BB%D1%8C%D0%BD%D1%8B%D0%B5_%D1%81%D0%BA%D0%BE%D0%B1%D0%BE%D1%87%D0%BD%D1%8B%D0%B5_%D0%BF%D0%BE%D1%81%D0%BB%D0%B5%D0%B4%D0%BE%D0%B2%D0%B0%D1%82%D0%B5%D0%BB%D1%8C%D0%BD%D0%BE%D1%81%D1%82%D0%B8#.D0.90.D0.BB.D0.B3.D0.BE.D1.80.D0.B8.D1.82.D0.BC_.D0.BF.D1.80.D0.BE.D0.B2.D0.B5.D1.80.D0.BA.D0.B8_.D0.BF.D1.80.D0.B0.D0.B2.D0.B8.D0.BB.D1.8C.D0.BD.D0.BE.D1.81.D1.82.D0.B8_.D1.81.D0.BA.D0.BE.D0.B1.D0.BE.D1.87.D0.BD.D0.BE.D0.B9_.D0.BF.D0.BE.D1.81.D0.BB.D0.B5.D0.B4.D0.BE.D0.B2.D0.B0.D1.82.D0.B5.D0.BB.D1.8C.D0.BD.D0.BE.D1.81.D1.82.D0.B8      
-        assert(ToClose <= ToOpen); // не встречалось закрывающих скобок, для которых не было соответствующих открывающих
-        if(ToOpen + ToClose == 2*N){ // все открывающие скобки закрыты, при этом нет лишних закрытых скобок
+        assert(CountCloseBracket <= CountOpenBracket); // не встречалось закрывающих скобок, для которых не было соответствующих открывающих
+        if(CountOpenBracket + CountCloseBracket == 2*N){ // все открывающие скобки закрыты, при этом нет лишних закрытых скобок
             BBS.push_back(STACK);
             return;
         }
 
-        if(ToOpen < N){
+        if(CountOpenBracket < N){
             STACK.push_back('(');
-            MakeBalancedBracket(ToOpen + 1, ToClose);
+            MakeBalancedBracket(CountOpenBracket + 1, CountCloseBracket);
             STACK.pop_back();
         }
 
-        if(ToClose < ToOpen){
+        if(CountCloseBracket < CountOpenBracket){
             STACK.push_back(')');
-            MakeBalancedBracket(ToOpen, ToClose + 1);
+            MakeBalancedBracket(CountOpenBracket, CountCloseBracket + 1);
             STACK.pop_back();
         }
     }
