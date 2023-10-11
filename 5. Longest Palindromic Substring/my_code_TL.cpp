6970444f72ca6d0c34bfef5bfa9e59bc397bab77
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-10-11 23:02:49 +0300

    Update my_code_TL.cpp

--- 5. Longest Palindromic Substring/my_code_TL.cpp
+++ 5. Longest Palindromic Substring/my_code_TL.cpp
@@ -19,6 +19,7 @@ public:
         for (int i = 0; i < s.size(); i++) {
                 for (int j = i + 1; j <= s.size(); j++) {
                     
+                    if(j - i <= LPS.size()){continue;}
                     if(isPalindrome({s.begin() + i, s.begin() + j}) && LPS.size() < j - i){
                        LPS = {s.begin() + i, s.begin() + j};
                     }
@@ -28,30 +29,3 @@ public:
         return LPS;
     }
 };
-/* // примечательно, что такой код на JS работает
-const isPalidrom = (s) => {
-    let l = 0
-    let r = s.length - 1
-    while (l < r) {
-        if (s[l] !== s[r]) return false
-        l++
-        r--
-    }
-    return true
-}
-
-function longestPalindrome(s )  {
-    let longest = s[0]
-    for (let i = 0; i < s.length; i++) {
-        let right = s.length - 1
-        while (i < right) {
-            const curStr = s.slice(i, right + 1)
-            if (isPalidrom(curStr)) {
-                if (longest.length < curStr.length) longest = curStr
-            }
-            right -= 1
-        }
-    }
-    return longest
-};
-*/
