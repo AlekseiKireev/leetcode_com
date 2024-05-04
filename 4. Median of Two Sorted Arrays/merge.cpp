4191daac9422b51d3e8bc9181d60aa02247eaf04
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2024-05-04 19:04:26 +0300

    Create merge.cpp

---
+++ 4. Median of Two Sorted Arrays/merge.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+
+        std::vector<int> merged(nums1.size() + nums2.size());
+
+        // Сливаем отсортированные векторы
+        std::merge(nums1.begin(), nums1.end(), nums2.begin(), nums2.end(), merged.begin());
+
+        int total = merged.size();
+
+        if (total % 2 == 1) {
+            // If the total number of elements is odd, return the middle element as the median.
+            return (merged[total / 2]);
+        } else {
+            // If the total number of elements is even, calculate the average of the two middle elements as the median.
+            int middle1 = merged[total / 2 - 1];
+            int middle2 = merged[total / 2];
+            return (middle1 + middle2) / 2.0;
+        }
+    }
+};
