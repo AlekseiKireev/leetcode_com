7142a0fcf7499f6ef487bc7177a982995daf76cb
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2024-05-06 12:29:16 +0300

    Update my_code_without_unique.cpp

--- 26. Remove Duplicates from Sorted Array/my_code_without_unique.cpp
+++ 26. Remove Duplicates from Sorted Array/my_code_without_unique.cpp
@@ -3,7 +3,7 @@
         long long CountDuplicates = 0;
         for(int i = 1; i < nums.size();++i){
 
-            if(nums[i - 1] != nums[i]){
+            if(nums[i - 1] != nums[i]){ // смещаем влево
                 // 1 2 3 3 3 4 5 6 6 7 --> 1 2 3 _ _ 4 5 6 6 7 --> 1 2 3 4 5 6 7 _ _ _
                 nums[i-CountDuplicates] = nums[i]; // 1 3 2 2 4 5 --> 1 3 2 4 5 _
             }
