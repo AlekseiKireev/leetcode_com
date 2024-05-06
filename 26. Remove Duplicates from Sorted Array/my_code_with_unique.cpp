e71ea79e035c3d378941e84f99b1a2e4004834a6
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2024-05-06 10:13:09 +0300

    Update my_code_with_unique.cpp

--- 26. Remove Duplicates from Sorted Array/my_code_with_unique.cpp
+++ 26. Remove Duplicates from Sorted Array/my_code_with_unique.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        const auto it = unique(begin(nums), end(nums));
+        const auto it = unique(begin(nums), end(nums)); // https://en.cppreference.com/w/cpp/algorithm/unique
         nums.erase(it, end(nums));
         return nums.size(); 
     }
