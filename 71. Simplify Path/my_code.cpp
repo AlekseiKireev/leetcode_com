f86129a5ba54deb4482ca5c44991a09382e99fed
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2024-05-06 20:52:00 +0300

    Update my_code.cpp

--- 71. Simplify Path/my_code.cpp
+++ 71. Simplify Path/my_code.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    string simplifyPath(string& path) {
+    string simplifyPath(string& path) { // error with string_view
         
         // RE on "6 / 258 testcases passed" without "path.empty() == false &&" on ex: "/"
         while(path.empty() == false && (path.back() == '/' /*|| path.back() == '.'*/)){ // Если вы столкнетесь с символом "/", то игнорируйте его.
