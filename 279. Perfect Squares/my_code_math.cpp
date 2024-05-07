210c371b157fb5553cda1a761db9a409c4432a84
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2024-05-07 17:44:36 +0300

    Update my_code_math.cpp

--- 279. Perfect Squares/my_code_math.cpp
+++ 279. Perfect Squares/my_code_math.cpp
@@ -29,7 +29,7 @@ public:
             }
         }  
 
-
+        // https://ru.wikipedia.org/wiki/%D0%A2%D0%B5%D0%BE%D1%80%D0%B5%D0%BC%D0%B0_%D0%9B%D0%B5%D0%B6%D0%B0%D0%BD%D0%B4%D1%80%D0%B0_%D0%BE_%D1%82%D1%80%D1%91%D1%85_%D0%BA%D0%B2%D0%B0%D0%B4%D1%80%D0%B0%D1%82%D0%B0%D1%85
         // The result is 4 if and only if n can be written in the 
         // form of 4^k*(8*m + 7). Please refer to 
         // Legendre's three-square theorem.
