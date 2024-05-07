eceb8e1828bf4c78743d888e72ab9161a698e4d0
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2024-05-07 09:58:03 +0300

    Create my_code_upd.cpp

---
+++ 76. Minimum Window Substring/my_code_upd.cpp
@@ -0,0 +1,55 @@
+class Solution {
+
+private:
+
+    // https://en.cppreference.com/w/cpp/language/ascii
+    const int POWER_ENCODING = 128; // s and t consist of uppercase and lowercase English letters.
+
+public:
+    string minWindow(string& S, string& T) {
+
+        // m == s.length n == t.length 1 <= m, n <= 10^5
+        if(/*s.empty() || t.empty() ||*/ S.size() < T.size()){ // Input: s = "a", t = "aa" Output: ""
+            return {};
+        }
+
+        vector<int> CharT_to_Count(POWER_ENCODING);
+        for(const char ch : T){
+            ++CharT_to_Count[ch]; // если CharT_to_Count[ch] > 0, значит ch из t
+        }
+
+        int T_Size = T.size();
+
+        // локальный максимум необходим ввиду того, чтобы гарантировать выполненеи RightPtr - LeftPtr < Substring_Size при T_Size == 0
+        int Substring_Size = 10e5 + 1; // m == s.length n == t.length 1 <= m, n <= 10^5
+        int Substring_LeftPtr = -1;
+
+        for(int LeftPtr = 0, RightPtr = 0; RightPtr < S.size(); ++RightPtr){ // сдвиг правого указателя
+
+            if(CharT_to_Count[S[RightPtr]] > 0){ // S[RightPtr] также существутет в T
+                --T_Size;
+            }
+            --CharT_to_Count[S[RightPtr]];
+
+            for( ;T_Size == 0; ++LeftPtr){ // сдвиг левого указателя
+
+                if(RightPtr - LeftPtr < Substring_Size){ // обновляем ответ
+                    Substring_Size = RightPtr - LeftPtr;
+                    Substring_LeftPtr = LeftPtr;
+                }
+
+                if(CharT_to_Count[S[LeftPtr]] == 0){ // условие выхода из цикла
+                    ++T_Size;
+                }
+
+                ++CharT_to_Count[S[LeftPtr]];
+            } // левый указатель
+
+        }
+
+
+
+        return Substring_LeftPtr == -1 ? "" : S.substr(Substring_LeftPtr, Substring_Size + 1); 
+        // +1 необходим ввиду того, что LeftPtr / RightPtr указывают на грницы искомго Substring
+    }
+};
