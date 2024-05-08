3c4f5dff2a56fcdbc94fc6007dbfaabb71fdcf0e
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2024-05-08 21:48:45 +0300

    Create 121c.cpp

---
+++ 121. Best Time to Buy and Sell Stock/121c.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+  int maxProfit(const vector<int>& a) {
+    int ans = 0;
+    int nextMax = 0;
+    for (int i = (int)a.size() - 1; i >= 0; i--) { 
+      int v = a[i];
+      ans = max(ans, nextMax - v);
+      nextMax = max(nextMax, v);
+    }
+    return ans;
+  }
+};
