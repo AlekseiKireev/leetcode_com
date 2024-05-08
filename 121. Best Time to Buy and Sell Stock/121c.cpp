f8986fa15640ffa4dbf43cebed24aecc8e18d02d
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2024-05-08 21:50:09 +0300

    Update 121c.cpp

--- 121. Best Time to Buy and Sell Stock/121c.cpp
+++ 121. Best Time to Buy and Sell Stock/121c.cpp
@@ -11,3 +11,18 @@ public:
     return ans;
   }
 };
+/*
+class Solution {
+public:
+  int maxProfit(const vector<int>& a) {
+    int ans = 0;
+    int nextMax = 0;
+    for (size_t i = a.size() ; i-- > 0; i) { 
+      int v = a[i];
+      ans = max(ans, nextMax - v);
+      nextMax = max(nextMax, v);
+    }
+    return ans;
+  }
+};
+*/
