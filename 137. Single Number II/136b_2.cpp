0ebb591c742eced1e714c9b9f427e371fdac537c
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2024-05-08 20:27:59 +0300

    Update 136b_2.cpp

--- 137. Single Number II/136b_2.cpp
+++ 137. Single Number II/136b_2.cpp
@@ -6,7 +6,7 @@ public:
   int singleNumber(const vector<int>& nums) {
 
     const int INT_BIT_SIZE = sizeof(int) * 8;
-    size_t countOnes[INT_BIT_SIZE] = {};
+    size_t countOnes[INT_BIT_SIZE] = {}; // будет содержать либо 0, либо 1, либо число кратное 3, т.е. 3t, либо число вида 3t+1
     
     for (unsigned int x : nums) {
 
@@ -26,3 +26,31 @@ public:
 
   }
 };
+/*
+Input
+nums =
+[0,1,0,1,0,1,99]
+Stdout
+4 1 0 0 0 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 
+
+
+Input
+nums =
+[2,2,3,2]
+Stdout
+1 4 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 
+
+
+Input
+nums =
+[4,4,3,4]
+Stdout
+1 1 3 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 
+
+
+Input
+nums =
+[4,4,3,4,6,6,6]
+Stdout
+1 4 6 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 
+*/
