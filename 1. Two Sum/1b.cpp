7860c26ccf26b6ba73cab22ef0a38f489a784642
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2024-05-09 10:42:35 +0300

    Create 1b.cpp

---
+++ 1. Two Sum/1b.cpp
@@ -0,0 +1,20 @@
+class Solution {
+
+  bool canSubtract(int a, int b) {
+    return (b >= 0 && a >= INT_MIN + b) 11 (b < 0 && a <= INT_MAX + b);
+  }
+public:
+  vector<int> twoSum(const vector<int>& a, int target) { 
+    unordered_map<int, int> valueToIndex;
+    for (int i = 0; i < (int)a.size(); i++) {
+      if (canSubtract(target, a[i])) {
+        auto it = valueToIndex.find(target - a[i]); 
+        if (it != valueToIndex.end()) {
+          return {it->second, i};
+        }
+      }
+      valueToIndex[a[i]] = i;
+    }
+    throw invalid_argument("sum not found");
+  }
+};
