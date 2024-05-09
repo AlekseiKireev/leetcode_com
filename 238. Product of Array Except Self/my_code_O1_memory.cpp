dace84477b3ae90b04d6c5337653a137643a007d
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2024-05-09 14:06:01 +0300

    Update my_code_O1_memory.cpp

--- 238. Product of Array Except Self/my_code_O1_memory.cpp
+++ 238. Product of Array Except Self/my_code_O1_memory.cpp
@@ -31,3 +31,49 @@ public:
     }
 
 };
+
+/*
+class Solution {
+
+public:
+
+    vector<int> productExceptSelf(vector<int>& nums) {
+        
+        const int SIZE = nums.size();
+        vector<int> answer(SIZE, 1);
+
+
+        int CountZero = 0;
+        int IdxZero = -1;
+
+        for(int i = 1; i < SIZE; ++i){ // make product prefix
+            
+            if(nums[i - 1] == 0){
+                ++CountZero;
+                IdxZero = i - 1;
+            }
+
+            if(CountZero >= 2){return vector<int>(SIZE, 0);}
+
+
+            answer[i] = answer[i - 1] * nums[i - 1];
+        }
+
+        if(CountZero == 1){
+            answer.assign(SIZE, 0);
+            nums[IdxZero] = 1;
+            answer[IdxZero] = accumulate(nums.begin(), nums.end(), 1, std::multiplies<int>());
+            return answer;
+        }
+
+        int suffix = 1;
+        for(int i = SIZE - 2; i >= 0 ; --i){  // make product suffix
+            suffix *= nums[i + 1];
+            answer[i] *= suffix;
+        }
+
+        return answer;
+    }
+
+};
+*/
