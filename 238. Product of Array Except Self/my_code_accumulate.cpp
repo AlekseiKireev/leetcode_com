fcd4c0d8f7b6d5e80c3dbf561fca2f364449b5b0
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2024-05-09 14:25:32 +0300

    Create my_code_accumulate.cpp

---
+++ 238. Product of Array Except Self/my_code_accumulate.cpp
@@ -0,0 +1,40 @@
+class Solution {
+
+public:
+
+    vector<int> productExceptSelf(vector<int>& nums) {
+        
+        const int SIZE = nums.size();
+        vector<int> answer(SIZE);
+
+
+        int CountZero = 0;
+        int IdxZero = -1;
+
+        for(int i = 0; i < SIZE; ++i){
+            
+            if(nums[i] == 0){
+                ++CountZero;
+                if(CountZero >= 2){return answer;}
+                IdxZero = i;
+            }            
+
+        }
+
+        if(CountZero == 1){
+            nums[IdxZero] = 1;
+            answer[IdxZero] = accumulate(nums.begin(), nums.end(), 1, std::multiplies<int>()); // https://en.cppreference.com/w/cpp/algorithm/accumulate
+            return answer;
+        }
+
+
+        const long long product_nums = accumulate(nums.begin(), nums.end(), 1, std::multiplies<int>()); // https://en.cppreference.com/w/cpp/algorithm/accumulate
+
+        for(int i = 0; i < SIZE; ++i){
+            answer[i] = product_nums / nums[i];
+        }
+
+        return answer;
+    }
+
+};
