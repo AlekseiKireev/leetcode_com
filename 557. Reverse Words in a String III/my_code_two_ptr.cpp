8329cc928e4588fd048185fd680e371d457fca96
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2024-05-09 08:54:47 +0300

    Create my_code_two_ptr.cpp

---
+++ 557. Reverse Words in a String III/my_code_two_ptr.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string reverseWords(string& s) {
+        
+        auto LeftPtr = s.begin();
+
+        for(auto RightPtr = LeftPtr + 1; RightPtr != s.end(); ++RightPtr){
+
+            if(*RightPtr == ' '){
+                reverse(LeftPtr, RightPtr);
+                LeftPtr = RightPtr + 1;
+            }
+        }
+
+        reverse(LeftPtr, s.end()); /* на случай если в слове нет пробела. Также чтобы это работало надо удалять пробелы с конца s
+                                      The input must not contain any leading or trailing spaces. -- вывод на тесте "Let's take LeetCode contest         "*/
+        return s;
+
+    }
+};
