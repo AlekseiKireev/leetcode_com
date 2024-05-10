b55933afba562b8c2eeb56e073956a32c708eafb
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2024-05-10 11:56:06 +0300

    Create my_code.cpp

---
+++ 347. Top K Frequent Elements/my_code.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        
+        if(nums.size() == k){ // k is in the range [1, The number of unique nums[i]]            
+            //ranges::sort(nums); //  You may return the answer in any order.
+            return nums;
+        }   
+
+        unordered_map<int, int> NumToCount; NumToCount.reserve(nums.size() - 1);
+
+        for(int num : nums){
+            ++NumToCount[num];
+        } 
+
+         vector<pair<int, int>> CountToNum; CountToNum.reserve(nums.size() - 1);
+
+         for(auto [num, cnt] : NumToCount){
+
+            CountToNum.push_back({cnt, num});
+
+         }
+        CountToNum.shrink_to_fit();
+
+        nth_element(
+            CountToNum.begin(),
+            CountToNum.begin() + k,
+            CountToNum.end(), 
+            [](const auto F, const auto S){
+                return F.first > S.first;
+            }
+        );
+
+        vector<int> TopK; TopK.reserve(k);
+
+        for(int i = 0; i < k; ++i){
+            TopK.push_back(CountToNum[i].second);
+        }
+
+        return TopK;
+    }
+};
