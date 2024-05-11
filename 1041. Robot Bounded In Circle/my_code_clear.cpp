93b85edfcdd71fea05cfbc6fbac34115687bad27
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2024-05-11 17:28:17 +0300

    Update my_code_clear.cpp

--- 1041. Robot Bounded In Circle/my_code_clear.cpp
+++ 1041. Robot Bounded In Circle/my_code_clear.cpp
@@ -1,1 +1,45 @@
+class Solution {
 
+struct step{
+
+int x;
+int y;
+
+};
+
+public:
+
+    bool isRobotBounded(string_view instructions) {
+        
+
+        int X = 0;
+        int Y = 0;
+
+        int SideWorld = 0;
+
+
+        array<step, 4> Steps = {
+            {
+                {0, 1}, {1, 0}, {0, -1}, {-1, 0}
+            }
+
+        };
+        for(const char move : instructions){
+
+            if(move == 'G'){ 
+
+                X += Steps[SideWorld].x;
+                Y += Steps[SideWorld].y;
+
+            }else if(move == 'L') { 
+                SideWorld = (SideWorld + 3) % 4;
+            }else if(move == 'R') { 
+                SideWorld = (SideWorld + 1) % 4;
+            }
+
+        }
+
+        return (X == 0 && Y == 0) || SideWorld != 0; 
+
+    }
+};
