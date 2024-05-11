5bdfd4854e33de5ccd8b1357f23f064272ffd34b
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2024-05-11 23:43:25 +0300

    Update 560a.cpp

--- 560. Subarray Sum Equals K/560a.cpp
+++ 560. Subarray Sum Equals K/560a.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
   int subarraySum(const vector<int>& nums, int k) {
 
-    unordered_map<int, int> prefixSumToCount;
+    unordered_map<int, int> prefixSumToCount; // ввиду формул в README не префикс, а аффикс
     int prefixSum = 0;
     prefixSumToCount[prefixSum] = 1; // для обработки случая prefixSum = k 
 
