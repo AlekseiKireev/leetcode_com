c5879ecdf47177d3333efd246ec5e5a58b75b42c
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2024-05-11 16:52:20 +0300

    Update my_code.cpp

--- 443. String Compression/my_code.cpp
+++ 443. String Compression/my_code.cpp
@@ -9,16 +9,18 @@ public:
 
             const char prev_char = chars[i]; 
             chars[NewLength] = prev_char; // "The compressed string s should not be returned separately, but instead, be stored in the input character array chars. "
-
-            // CountRepeat -- количество повторяющихся символов prev_char, которые расположены в диапозоне [i, j] : i <= j
-            int CountRepeat = 1; // 1, так как учитываем prev_char
             ++NewLength; // инкрементируем, так как учитываем prev_char
 
+            
+            // CountRepeat -- количество повторяющихся символов prev_char, которые расположены в диапозоне [i, j] : i <= j
+            int CountRepeat = 1; // 1, так как учитываем prev_char
+            
             // обновление CountRepeat
             for(; i+1 < chars.size() && prev_char == chars[i+1]; ++i){ // "i+1" ввиду того, что chars[i] обработали выше                 
                 ++CountRepeat;                            
             }
 
+            
             // используем тот факт, что |substr| < |substr.size()|, где |*| -- функция, возвращающая длину строки *
             if(CountRepeat > 1){ // "Note that group lengths that are 10 or longer will be split into multiple characters in chars."
 
