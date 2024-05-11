7863919bbb8adeef601951a665bd05d6afdfe1ac
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2024-05-11 16:34:06 +0300

    Update my_code.cpp

--- 443. String Compression/my_code.cpp
+++ 443. String Compression/my_code.cpp
@@ -8,26 +8,23 @@ public:
         for(int i = 0; i < chars.size() ; ++i){
 
             char prev_char = chars[i]; 
-            chars[NewLength] = prev_char;
+            chars[NewLength] = prev_char; // "The compressed string s should not be returned separately, but instead, be stored in the input character array chars. "
 
             // CountRepeat -- количество повторяющихся символов prev_char, которые расположены в диапозоне [i, j] : i <= j
             int CountRepeat = 1; // 1, так как учитываем prev_char
             ++NewLength; // инкрементируем, так как учитываем prev_char
 
             // обновление CountRepeat
-            while(i+1 < chars.size() && prev_char == chars[i+1]){
-                
-                ++CountRepeat;
-                ++i;
-                
+            for(; i+1 < chars.size() && prev_char == chars[i+1]; ++i){                
+                ++CountRepeat;                            
             }
 
             // используем тот факт, что |substr| < |substr.size()|, где |*| -- функция, возвращающая длину строки *
-            if(CountRepeat > 1){
+            if(CountRepeat > 1){ // "Note that group lengths that are 10 or longer will be split into multiple characters in chars."
 
                 for(const char ch : to_string(CountRepeat)){
                     
-                    chars[NewLength] = ch;
+                    chars[NewLength] = ch; // "The compressed string s should not be returned separately, but instead, be stored in the input character array chars. "
                     ++NewLength;
                     // equiv: chars[NewLength++] = ch;
                 }
