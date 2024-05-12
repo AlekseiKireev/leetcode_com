8f6e11d415876b10e0b25d78b1ade418e650e5c7
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2024-05-12 18:36:02 +0300

    Update my_code_O(1)_memory.cpp

--- 581. Shortest Unsorted Continuous Subarray/my_code_O(1)_memory.cpp
+++ 581. Shortest Unsorted Continuous Subarray/my_code_O(1)_memory.cpp
@@ -38,3 +38,31 @@ public:
     }
 
 };
+
+/*
+class Solution {
+public:
+    int findUnsortedSubarray(vector<int>& nums) {
+        
+        int RightPtr = -1;
+        int LeftPtr = -1;
+
+        int ExtremeMaxFront = nums.front();
+        int ExtremeMinBack  = nums.back();
+        for(int i = 0; i < nums.size(); ++i){
+
+            ExtremeMaxFront = max(ExtremeMaxFront, nums[i]);
+            if(nums[i] != ExtremeMaxFront){RightPtr = i;}
+            
+            ExtremeMinBack = min(ExtremeMinBack, nums[nums.size() - 1 - i]);
+            if(nums[nums.size() - 1 - i] != ExtremeMinBack){LeftPtr = nums.size() - 1 - i;}
+
+
+        }
+
+        if(RightPtr == -1){return 0;}
+        return RightPtr - LeftPtr + 1;
+
+    }
+};
+*/
