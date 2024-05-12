c82adb06132cfb62bcaf350f579ccf2cf30a76d5
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2024-05-12 23:11:13 +0300

    Update my_code_Inorder.cpp

--- 98. Validate Binary Search Tree/my_code_Inorder.cpp
+++ 98. Validate Binary Search Tree/my_code_Inorder.cpp
@@ -27,3 +27,43 @@ public:
 
     }
 };
+
+/*
+class Solution {
+private:
+    // Предыдущее значение узла, начальное значение устанавливается как минимальное значение для long long
+    long long prevNodeValue = numeric_limits<long long>::min();
+
+    // Рекурсивная функция для проверки определения BST
+    bool isBST(TreeNode* root) {
+        // Если узел является пустым, возвращаем true, так как пустое поддерево считается корректным BST
+        if (root == nullptr) {
+            return true;
+        }
+
+        // Проверяем левое поддерево
+        bool isLeftSubtreeValid = isBST(root->left);
+        // Если левое поддерево не является BST, возвращаем false
+        if (!isLeftSubtreeValid) {
+            return false;
+        }
+
+        // Проверяем, что значение текущего узла больше, чем предыдущее значение узла
+        if (prevNodeValue >= root->val) {
+            return false;
+        } else {
+            prevNodeValue = root->val; // Обновляем предыдущее значение узла
+        }
+
+        // Проверяем правое поддерево
+        return isBST(root->right);
+    }
+
+public:
+    // Основная функция для проверки корректности BST
+    bool isValidBST(TreeNode* root) {
+        return isBST(root);
+    }
+};
+
+*/
