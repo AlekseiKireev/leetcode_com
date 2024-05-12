e84af0f9e99f7b5550b226e9b3f2435a5090e88a
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2024-05-12 21:43:13 +0300

    Update my_code_upd.cpp

--- 101. Symmetric Tree/my_code_upd.cpp
+++ 101. Symmetric Tree/my_code_upd.cpp
@@ -17,11 +17,13 @@ private:
     // На основе my_code.cpp можно сделать вывод, что подходит любой вид рекурсивного обхода
     bool Travel(TreeNode* LeftNode, TreeNode* RightNode){
 
+        //--------------------------УСЛОВИЕ-ВЫХОДА-ИЗ-РЕКУРСИИ---------------------------------------------------------
         if(LeftNode == nullptr && RightNode == nullptr){return true;} // хотя-бы одна нода указывает НЕ на nullptr
         if(LeftNode == nullptr || RightNode == nullptr){return false;}
 
         if(LeftNode->val != RightNode->val){return false;}
-
+        //--------------------------УСЛОВИЕ-ВЫХОДА-ИЗ-РЕКУРСИИ---------------------------------------------------------
+        
         return (Travel(LeftNode->left, RightNode->right) && Travel(LeftNode->right, RightNode->left));
 
     }
