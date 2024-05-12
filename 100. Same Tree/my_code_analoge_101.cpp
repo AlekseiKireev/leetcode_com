66723bd8a8a03788fd1be4928e323910ca609125
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2024-05-12 22:05:25 +0300

    Create my_code_analoge_101.cpp

---
+++ 100. Same Tree/my_code_analoge_101.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+
+private:
+
+    // На основе my_code.cpp можно сделать вывод, что подходит любой вид рекурсивного обхода
+    bool Travel(TreeNode* LeftNode, TreeNode* RightNode){
+
+        //--------------------------УСЛОВИЕ-ВЫХОДА-ИЗ-РЕКУРСИИ---------------------------------------------------------
+        if(LeftNode == nullptr && RightNode == nullptr){return true;} // хотя-бы одна нода указывает НЕ на nullptr
+        if(LeftNode == nullptr || RightNode == nullptr){return false;}
+
+        if(LeftNode->val != RightNode->val){return false;}
+        //--------------------------УСЛОВИЕ-ВЫХОДА-ИЗ-РЕКУРСИИ---------------------------------------------------------
+        
+        return (Travel(LeftNode->left, RightNode->left) && Travel(LeftNode->right, RightNode->right));
+
+    }
+
+public:
+
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        return (Travel(p, q) && Travel(q, p));
+    }
+
+};
