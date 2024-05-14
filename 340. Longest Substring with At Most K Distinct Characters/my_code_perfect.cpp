5ef19b7ecaf2b0ca0f149a691d00f31af44adde1
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2024-05-14 12:09:46 +0300

    Update and rename my_code.cpp to my_code_perfect.cpp

---
+++ 340. Longest Substring with At Most K Distinct Characters/my_code_perfect.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include<cassert>
+#include <array>
+
+using namespace std; 
+
+constexpr int SIZE_ALPHABET = 128;
+
+class Solution {
+
+private:
+
+
+    int LengthSubstr = 0;
+public:
+
+    int lengthOfLongestSubstringKDistinct(string_view s, const int k) {
+    
+    
+        int PowerSlidingWindow = 0;
+        
+        array<int,SIZE_ALPHABET> CharToCount; CharToCount.fill(0);
+        
+        
+        for(int LeftPtr = 0 , RightPtr = 0; RightPtr < s.size(); ++RightPtr){
+            
+            if(++CharToCount[s[RightPtr]] == 1){ // new unique char in Sliding Window
+                ++PowerSlidingWindow;
+            }
+            
+            /* мощность искомой подстроки превышает допустимую, необходимо сдвинуть левый указатель */
+            for(;LeftPtr < RightPtr && PowerSlidingWindow > k; ++LeftPtr){ 
+                
+                if(--CharToCount[s[LeftPtr]] == 0){
+                    --PowerSlidingWindow;
+                }
+            }
+            
+            // по сути этот оператор вообще не нужен!
+            if(PowerSlidingWindow <= k){ // "<=" обусловлен s = "aaaaaaaa", k = 3 --> 8
+                LengthSubstr = max(LengthSubstr, RightPtr - LeftPtr + 1);
+            }
+            
+        }
+        
+        return LengthSubstr;
+    }
+
+};
+
+int main() {
+    cout << Solution().lengthOfLongestSubstringKDistinct("abc", 0) << '\n';
+    cout << Solution().lengthOfLongestSubstringKDistinct("aaa", 0) << '\n';
+    cout << Solution().lengthOfLongestSubstringKDistinct("aAaA", 1) << '\n';
+    cout << Solution().lengthOfLongestSubstringKDistinct("aA~aA", 2) << '\n';
+    cout << Solution().lengthOfLongestSubstringKDistinct("aAaA", 2) << '\n';
+    cout << Solution().lengthOfLongestSubstringKDistinct("abbbbbbc", 2) << '\n'; // 7
+    cout << Solution().lengthOfLongestSubstringKDistinct("abcddefg", 3) << '\n'; // 4
+    cout << Solution().lengthOfLongestSubstringKDistinct("aaaaaaaa", 3) << '\n'; // 8
+    cout << Solution().lengthOfLongestSubstringKDistinct("abcefg", 1) << '\n'; // 1    
+}
+/*
+0
+0
+1
+2
+4
+*/
