ecc58a5e454c4817b198832709c2fc7ba60bbf72
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2024-05-15 01:39:27 +0300

    Create related_56.cpp

---
+++ 986. Interval List Intersections/related_56.cpp
@@ -0,0 +1,31 @@
+class Solution {
+
+private:
+
+    bool CheckIntersectionSortedIntervals(vector<int>& First, vector<int>& Second){
+
+        return max(First.front(), Second.front()) <= min(First.back(), Second.back());
+    }
+
+public:
+    vector<vector<int>> intervalIntersection(vector<vector<int>>& firstList, vector<vector<int>>& secondList) {
+        
+        vector<vector<int>> Ans;
+
+        for(int FirstPtr = 0, SecondPtr = 0; FirstPtr < firstList.size() && SecondPtr < secondList.size(); ){
+
+            if( CheckIntersectionSortedIntervals(firstList[FirstPtr], secondList[SecondPtr]) ){
+
+                Ans.push_back({max(firstList[FirstPtr].front(), secondList[SecondPtr].front()), 
+                                min(firstList[FirstPtr].back(), secondList[SecondPtr].back())});
+
+            }
+
+            // у кого правая сторона меньше, ту и сдвигаем, та как большая сторона может иметь пересечения с следущюим элементом
+            firstList[FirstPtr].back() < secondList[SecondPtr].back() ? ++FirstPtr : ++SecondPtr;
+        }
+
+        return Ans;
+
+    }
+};
