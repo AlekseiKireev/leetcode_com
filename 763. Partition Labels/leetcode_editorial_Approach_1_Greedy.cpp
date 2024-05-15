a33315e8d9e6456ff4d4032a8e7c4ac29cd4a5f6
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2024-05-15 15:06:21 +0300

    Update leetcode_editorial_Approach_1_Greedy.cpp

--- 763. Partition Labels/leetcode_editorial_Approach_1_Greedy.cpp
+++ 763. Partition Labels/leetcode_editorial_Approach_1_Greedy.cpp
@@ -1,3 +1,32 @@
+class Solution {
+public:
+    std::vector<int> partitionLabels(std::string_view S) {
+
+        // last[IdxChar] -> index of S where char occurs last
+        std::array<int, 26> last; last.fill(0);
+        for (int i = 0; i < S.length(); ++i){
+            last[S[i] - 'a'] = i;
+        }
+            
+
+        int EndCurrentChunk  = 0; // конец текущего раздела
+        int StartCurrentChunk = 0; // начало текущего раздела, начинается с 0
+
+        std::vector<int> ans;
+        for (int i = 0; i < S.length(); ++i) {
+            EndCurrentChunk = std::max(EndCurrentChunk, last[S[i] - 'a']);
+
+            if (i == EndCurrentChunk) {
+                ans.push_back(i - StartCurrentChunk + 1); // длина текущего раздела
+                StartCurrentChunk = i + 1; // указываем на следующий символ, начало нового раздела
+            }
+        }
+        return ans;
+    }
+};
+
+
+/*
 #include <vector>
 #include <string>
 
@@ -21,3 +50,4 @@ public:
         return ans;
     }
 };
+*/
