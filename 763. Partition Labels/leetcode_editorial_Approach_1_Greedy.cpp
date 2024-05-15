ed5df90bedb02025c1c9fc2d435332fc0bc36d42
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2024-05-15 15:13:52 +0300

    Update leetcode_editorial_Approach_1_Greedy.cpp

--- 763. Partition Labels/leetcode_editorial_Approach_1_Greedy.cpp
+++ 763. Partition Labels/leetcode_editorial_Approach_1_Greedy.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     std::vector<int> partitionLabels(std::string_view S) {
 
-        // last[IdxChar] -> index of S where char occurs last
-        std::array<int, 26> last; last.fill(0);
+        // CharToLastID[IdxChar] -> index of S where char occurs CharToLastID
+        std::array<int, 26> CharToLastID; CharToLastID.fill(0);
         for (int i = 0; i < S.length(); ++i){
-            last[S[i] - 'a'] = i; // "s consists of lowercase English letters."
+            CharToLastID[S[i] - 'a'] = i; // "s consists of lowercase English letters."
         }
             
 
@@ -14,7 +14,7 @@ public:
 
         std::vector<int> ans;
         for (int i = 0; i < S.length(); ++i) {
-            EndCurrentChunk = std::max(EndCurrentChunk, last[S[i] - 'a']);
+            EndCurrentChunk = std::max(EndCurrentChunk, CharToLastID[S[i] - 'a']);
 
             if (i == EndCurrentChunk) { // S[i] в разделе встречается последний раз, j < i также встречались до него ввиду использования max в EndCurrentChunk
                 ans.push_back(i - StartCurrentChunk + 1); // длина текущего раздела
@@ -33,15 +33,15 @@ public:
 class Solution {
 public:
     std::vector<int> partitionLabels(std::string S) {
-        std::vector<int> last(26, 0);
+        std::vector<int> CharToLastID(26, 0);
         for (int i = 0; i < S.length(); ++i) {
-            last[S[i] - 'a'] = i;
+            CharToLastID[S[i] - 'a'] = i;
         }
 
         int j = 0, anchor = 0;
         std::vector<int> ans;
         for (int i = 0; i < S.length(); ++i) {
-            j = std::max(j, last[S[i] - 'a']);
+            j = std::max(j, CharToLastID[S[i] - 'a']);
             if (i == j) {
                 ans.push_back(i - anchor + 1);
                 anchor = i + 1;
