41707c2ac6fa6c0d423c1a23bf4c0e73f690eee0
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2024-05-16 14:29:55 +0300

    Create slow_and_fast_upd.cpp

---
+++ 234. Palindrome Linked List/slow_and_fast_upd.cpp
@@ -0,0 +1,57 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+
+ListNode* FindRightMid(ListNode* head){
+    ListNode* Slow = head;
+    ListNode* Fast = head;
+
+    while(Fast != nullptr && Fast->next != nullptr){
+        Slow = Slow->next;
+        Fast = Fast->next->next;
+    }
+
+    return Slow;
+}
+
+
+// https://github.com/SkosMartren/leetcode_com/tree/main/206.%20Reverse%20Linked%20List
+ListNode* Reverse(ListNode* head){
+
+    ListNode* ReverseNodes = nullptr;
+
+    while(head != nullptr){
+        ListNode* NextNode = head->next;
+        head->next = ReverseNodes;
+        ReverseNodes = head;
+
+        head = NextNode;
+
+    }
+
+    return ReverseNodes;
+}
+
+public:
+    bool isPalindrome(ListNode* head) {
+        
+        ListNode* RightPtr = Reverse(FindRightMid(head));
+
+        while(RightPtr != nullptr){
+            
+            if(head->val != RightPtr->val){return false;}
+            RightPtr = RightPtr->next;
+            head = head->next;
+        }
+
+        return true;
+    }
+};
