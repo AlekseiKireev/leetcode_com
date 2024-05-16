ef1297ab9a60f474cfc1d149f972bd7fa99dcdb8
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2024-05-16 20:28:10 +0300

    Create my_code.cpp

---
+++ 124. Binary Tree Maximum Path Sum/my_code.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    int max_sum = -1001; // -1000 <= Node.val <= 1000
+
+    int max_gain(TreeNode* root) {
+
+        if (root == nullptr) {
+            return 0; // возврат нуля так как ноды, которые ни куда не указывает имеют нулевой вес, это лоигчно
+        }
+
+        // "0" означает, что node не рассматриваем, так как нет смысла в пути, стоимость которого меньше 0
+        // "0" на случай рассматрение листа, тогда в price_path_sum будет значение этого листа?
+        int l = max(max_gain(root->left), 0); 
+        int r = max(max_gain(root->right), 0);
+
+        int price_path_sum = root->val + l + r; // оптимальная сумма поддерева с текущем корнем root
+        max_sum = max(max_sum, price_path_sum); // обновляем искомое
+
+        return root->val + max(l, r); // "max(l, r)" необходим для того, чтобы из правого и левого поддерева с корнем root выбрать максимальное и отбросить ненужную ветку
+    }
+
+    int maxPathSum(TreeNode* root) {
+        max_gain(root);
+        return max_sum;
+    }
+};
