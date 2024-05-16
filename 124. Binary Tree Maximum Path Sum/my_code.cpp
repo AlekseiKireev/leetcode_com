df7254213c077db2f374b00e6912ec1be652dcbf
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2024-05-16 20:56:08 +0300

    Update my_code.cpp

--- 124. Binary Tree Maximum Path Sum/my_code.cpp
+++ 124. Binary Tree Maximum Path Sum/my_code.cpp
@@ -24,13 +24,13 @@ public:
         // "0" означает, что node не рассматриваем, так как нет смысла в пути, стоимость которого меньше 0
         int l = max(max_gain(root->left), 0); 
         int r = max(max_gain(root->right), 0);
-
-        // CurrentMaxPath == price_path_sum
-        int price_path_sum = root->val + l + r; // оптимальная сумма пути в поддеревьях с текущем корнем root (путь имеет вид: l1<--l2<--...<--root-->...-->r2-->r1)
+        assert(0 <= l && 0 <= r);
+        
+        int CurrentMaxSumPricePath = root->val + l + r; // оптимальная сумма пути в поддеревьях с текущем корнем root (путь имеет вид: l1<--l2<--...<--root-->...-->r2-->r1)
         
         
         
-        max_sum = max(max_sum, price_path_sum); // обновляем искомое
+        max_sum = max(max_sum, CurrentMaxSumPricePath); // обновляем искомое
 
         assert( 0 <= max(l, r));
         return root->val + max(l, r); // "max(l, r)" необходим для того, чтобы из правого и левого поддерева с корнем root выбрать максимальный пуь и отбросить ненужную ветку
