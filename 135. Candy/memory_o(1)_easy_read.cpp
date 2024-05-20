a82fbde0cdb8eeb3ea12ab302a75d70cbc006948
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2024-05-20 22:54:21 +0300

    Create memory_o(1)_easy_read.cpp

---
+++ 135. Candy/memory_o(1)_easy_read.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        
+        const int n = ratings.size();
+
+        int candies = n;
+
+        for(int i = 0; i < n - 1;){ // если оператор тут будет сдигаться -- это ошибка, так как он может сдивагтсья внутри других циклов, не обрабатываться, а потом тут еще раз сдвигаться!        
+            
+            if(ratings[i] == ratings[i + 1]){++i; continue;}
+
+            int Hight = 0;
+            for(;i < n - 1 && ratings[i] < ratings[i + 1]; ++i){
+                
+                ++Hight;
+                candies += Hight;
+                
+            }
+
+            int Low = 0;
+            for(;i < n - 1 && ratings[i] > ratings[i + 1]; ++i){
+                
+                ++Low;
+                candies += Low;
+                
+            }
+
+            candies -= min(Hight, Low);
+
+        }
+
+        return candies;
+    }
+};
