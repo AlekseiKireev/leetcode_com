43f539e4187173a855a282355673c2feb53c9d31
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2024-05-20 16:40:38 +0300

    Create memory_o(1).cpp

---
+++ 135. Candy/memory_o(1).cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+
+        int n = ratings.size();
+        int candy = n, i=1;
+        
+        while(i<n){
+
+            if(ratings[i] == ratings[i-1]){
+                i++;
+                continue;
+            }
+            
+            //For increasing slope
+            int peak = 0;
+            while(ratings[i] > ratings[i-1]){
+                peak++;
+                candy += peak;
+
+                i++; if(i == n) {return candy;}
+            }
+            
+            //For decreasing slope
+            int valley = 0;
+            for(;i<n && ratings[i] < ratings[i-1]; i++){
+
+                valley++;
+                candy += valley;
+
+            }
+            candy -= min(peak, valley); //Keep only the higher peak
+        }
+        return candy;
+    }
+};
