a5184f6b88375f252bdd6d54af5f93018af2e145
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2024-05-22 17:59:03 +0300

    Create my_code_two_ptr_upd.cpp

---
+++ 19. Remove Nth Node From End of List/my_code_two_ptr_upd.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        
+        ListNode* RightPtr = head;
+        for(;0 < n; --n){
+            RightPtr = RightPtr->next;
+        }
+
+        if(RightPtr == nullptr){ // head = [1] n = 1
+            head = head->next; 
+            return head;
+        }
+
+        ListNode* LeftPtr = head; // будет указывать на ближайший слева элемент, который надо удалить
+        for(;RightPtr->next != nullptr; RightPtr = RightPtr->next){ // RightPtr->next != nullptr следует из примера Input: head = [1,2,3,4,5], n = 2 Output: [1,2,3,5] -- можно легко это прочувствовать
+            LeftPtr = LeftPtr->next;
+        }
+
+        LeftPtr->next = LeftPtr->next->next;
+
+        return head;
+
+    }
+};
