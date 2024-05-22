359f258aeade062c0f1037234f1fca9fce7d9f19
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2024-05-22 20:27:22 +0300

    Update my_code.cpp

--- 771. Jewels and Stones/my_code.cpp
+++ 771. Jewels and Stones/my_code.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     int numJewelsInStones(string jewels, string stones) {
-        
+
+        // если не помнишь про 'z' - 'A' + 1 можно использовать array от мощности кодировки, в ascii это 128
         // 'z' - 'A' + 1 <-- https://en.cppreference.com/w/cpp/language/ascii <-- https://www.utf8-chartable.de/
         vector<bool> JewelToCount('z' - 'A' + 1, false); // https://en.cppreference.com/w/cpp/container/vector_bool
         for(const char jewel : jewels){ // All the characters of jewels are unique.
