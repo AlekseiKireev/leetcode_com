12662dd6907a980a0ca0e146142dee1c6ec61cb7
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2024-05-22 22:08:53 +0300

    Update my_code_functor.cpp

--- 150. Evaluate Reverse Polish Notation/my_code_functor.cpp
+++ 150. Evaluate Reverse Polish Notation/my_code_functor.cpp
@@ -12,14 +12,21 @@ public:
     int evalRPN(vector<string>& tokens) {
         
         stack<int, vector<int>> Numbs;
+        
         for(const string& token : tokens){
+            
             if(OperatorToFuctor.count(token)){
+                
                 const int  RightOperand = Numbs.top();  Numbs.pop();
                 const int  LeftOperand = Numbs.top();  Numbs.pop();
                 Numbs.push(OperatorToFuctor.at(token)(LeftOperand, RightOperand));
+                
             }else{
+                
                 Numbs.push(stoi(token));
+                
             }
+            
         }
 
         return Numbs.top();
