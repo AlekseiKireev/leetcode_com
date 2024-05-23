9cb242dcd488bfb91769fa7f35f9d6ccc7876992
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2024-05-24 00:50:00 +0300

    Update my_code_upd.cpp

--- 849. Maximize Distance to Closest Person/my_code_upd.cpp
+++ 849. Maximize Distance to Closest Person/my_code_upd.cpp
@@ -32,3 +32,45 @@ public:
         return max({PrefixSize,SuffixSize ,EndingSize}); // https://en.cppreference.com/w/cpp/algorithm/max – см. 3-4
     }
 };
+
+/*
+class Solution {
+
+private:
+
+    int PrefixSize = 0; // количество начальных нулей
+    int SuffixSize = 0; // максимальное количетство нулей, отличных от начальных и конечных
+    int EndingSize = 0; // количество конечных нулей. Также выступает в качестве буффера для EndingSize преследуя цель максимизации
+
+public:
+    int maxDistToClosest(vector<int>& seats) {
+        
+        
+        for(int seat : seats){ // обрабатываем случай ведущих нулей, например: [0,0,0,0,0,1,0,0,0]
+            if(seat){break;}
+            ++PrefixSize;
+        }
+
+        while(seats.back() == 0){
+            seats.pop_back();
+            ++EndingSize;
+        }
+
+        int LocalMaxSuffixSize = 0;
+        for(int i = PrefixSize + 1; i<seats.size(); ++i){
+
+            if(!seats[i]){ // место свободно, заносим информацию в буффер
+                ++LocalMaxSuffixSize;
+            }
+            else{
+                SuffixSize = max(SuffixSize, LocalMaxSuffixSize);
+                LocalMaxSuffixSize = 0; // обнуляем буффер 
+            }
+
+        }
+
+        SuffixSize = ((SuffixSize%2) ? SuffixSize/2 + 1: SuffixSize/2); // расстояние между Алексом и ближайшим к нему человеком, при условии, что Алекс сидит между двумя людьми
+        return max({PrefixSize,SuffixSize ,EndingSize}); // https://en.cppreference.com/w/cpp/algorithm/max – см. 3-4
+    }
+};
+*/
