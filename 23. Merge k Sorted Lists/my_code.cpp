fc74638bff35c322731874cab5d477fec7a5650a
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2024-05-24 23:15:19 +0300

    Update my_code.cpp

--- 23. Merge k Sorted Lists/my_code.cpp
+++ 23. Merge k Sorted Lists/my_code.cpp
@@ -52,3 +52,46 @@ public:
         return lists.back();
     }
 };
+// more slow
+/*
+class Solution {
+
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        
+        if(list1 == nullptr){
+            return list2;
+        }
+
+        if(list2 == nullptr){
+            return list1;
+        }
+
+        if(list1->val < list2->val){
+
+            list1->next = mergeTwoLists(list1->next , list2);
+            return list1;
+        }
+
+        list2->next = mergeTwoLists(list1 , list2->next);
+        return list2;
+
+    }
+
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+
+        if(lists.empty()){
+            return nullptr;
+        }
+
+        while(lists.size() != 1){
+
+            ListNode* list1 = lists.back();  lists.pop_back(); 
+            ListNode* list2 = lists.back();  lists.pop_back(); 
+            lists.push_back(mergeTwoLists(list1, list2));
+        }
+
+        return lists.front();
+    }
+};
+*/
