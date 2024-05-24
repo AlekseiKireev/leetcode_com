61dd473ce86e619538260b530040110df37bca22
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2024-05-25 00:33:12 +0300

    Update my_code_hash.cpp

--- 350. Intersection of Two Arrays II/my_code_hash.cpp
+++ 350. Intersection of Two Arrays II/my_code_hash.cpp
@@ -27,3 +27,15 @@ public:
         return intersection;
     }
 };
+/* short:
+vector<int> intersect(vector<int>& a, vector<int>& b) {
+    unordered_map<int, int> ctr;
+    for (int i : a)
+        ctr[i]++;
+    vector<int> out;
+    for (int i : b)
+        if (ctr[i]-- > 0)
+            out.push_back(i);
+    return out;
+}
+*/
