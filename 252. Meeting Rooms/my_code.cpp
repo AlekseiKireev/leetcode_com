b8f6b16ee239ef720c889fc934b032db6b049213
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2024-05-25 22:46:36 +0300

    Update my_code.cpp

--- 252. Meeting Rooms/my_code.cpp
+++ 252. Meeting Rooms/my_code.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool canAttendMeetings(vector<vector<int>>& intervals) {
         
-        sort(intervals.begin(), intervals.end());
+        sort(intervals.begin(), intervals.end()); // это гарантирует, что intervals[i-1].front() <= intervals[i].front()
         
         for(int i = 1; i < intervals.size(); ++i){
                 if(intervals[i].front() < intervals[i-1].back()){return false;}
@@ -12,3 +12,14 @@ public:
         
     }
 };
+/*
+после сортировки интревалы имеют вид: 
+a1|---------------------------------------------------|b1
+        a2|-----------------------------------|b2
+                a3|---------------------|b3
+                a4|----------|b4                
+
+Достаточно проверить, что a_{i-1} < b_{i}
+
+Ответ: True                
+*/
