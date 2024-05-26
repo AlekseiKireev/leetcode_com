39fd2f697ee4c357e18a8c946e81856a61c2a336
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2024-05-26 17:45:21 +0300

    Update my_code_easy_reader.cpp

--- 1004. Max Consecutive Ones III/my_code_easy_reader.cpp
+++ 1004. Max Consecutive Ones III/my_code_easy_reader.cpp
@@ -45,3 +45,34 @@ public:
 
     }
 };
+/*
+class Solution {
+
+public:
+
+    int longestOnes(vector<int>& N, int k) {
+        
+        int Ans = 0;
+
+        int CountFlipZero = 0;
+
+        for(int L = 0, R = 0; R < N.size(); ++R){
+
+            if(N[R] == 0){
+                ++CountFlipZero;
+            }
+
+            for(;CountFlipZero == k + 1; ++L){
+                if(N[L] == 0){
+                    --CountFlipZero;
+                }                
+            }
+
+            Ans = max(Ans, R - L + 1);
+        }
+
+        return Ans;
+    }
+
+};
+*/
