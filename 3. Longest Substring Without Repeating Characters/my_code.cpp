e692d19208cf75a988f6115e088bb756eee1307d
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2024-05-26 13:35:24 +0300

    Update my_code.cpp

--- 3. Longest Substring Without Repeating Characters/my_code.cpp
+++ 3. Longest Substring Without Repeating Characters/my_code.cpp
@@ -13,13 +13,15 @@ public:
 
         // левый указатель опускает флаг о сущест-и символа, правй -- поднимаем.
         for(int Left = 0; Left < Str.size(); ++Left){ // сдвиг левого указателя
-            
+
+            // при сдвиге правого информация о левом автоматически обновляется, надо разве что не забыть опутсить флаг символа
             // проверка на сдвиг правого указателя: необходимо выполнение услвоия "substring without repeating characters."
             for(;Right < Str.size() && CharToExistingInSlidingWindow[Str[Right]] == false;++Right){  // сдвиг правого указателя
                 CharToExistingInSlidingWindow[Str[Right]] = true;  // обновляем информацию о окне после сдвига правого указателя               
             
             } // Str[Right] уже встречался в {Str[Left], ..., Str[Right-1]} --> длина текущей подходящйе подсткроки: Right - 1 - Left + 1
-
+            // Str[Left], ..., Str[Right-1]} содержит уникальные символы
+            
             Length = max(Length, Right - Left); // "find the length of the longest substring"
 
             assert(Left <= Right); // инваринат, который появляется при работе с думя указателями
