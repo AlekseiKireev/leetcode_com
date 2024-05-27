3772979cc3ea05d7f6022636dfcf98695ad8f20c
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2024-05-27 13:11:59 +0300

    Update my_code_degree_N.cpp

--- 279. Perfect Squares/my_code_degree_N.cpp
+++ 279. Perfect Squares/my_code_degree_N.cpp
@@ -18,10 +18,13 @@ public:
 
         // конретно для этой задачи можно написать std::vector<int> DP(N + 1, 4); -- глобальынй максимум необходим так как в дальнейшем будем использовать min
         // ввиду условия: "return the least number of perfect square numbers that sum to n."
+        // "N + 1" необходим для того, чтобы DP.back() == DP[N] -- что является искомым
         std::vector<int> DP(N + 1, 101); // DP[i] == наименьшее число слагаемых в квадратичном разложении числа i : i \in |N --> искомое == DP.back()
         DP[0] = 0;
 
         for (size_t sum = 1; sum <= N; sum++) {
+
+            // В "power(first, DEGREE) <= sum" знак равенства необходим на случай полного квадрата
             for (int first = 1; power(first, DEGREE) <= sum; first++) { // first * first <= sum <-->  0 <= sum - first * first -- что используется далее в nSq[sum - first * first]
                 // min -- по условию задачи
                 DP[sum] = std::min(DP[sum], DP[sum - power(first, DEGREE)] + 1); // "+ 1" обусловлен тем, что из "sum - first * first" можно получить "sum" 
