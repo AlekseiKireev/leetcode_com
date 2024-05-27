4f1f345c1a88da68218777ce33a1ace5eee897a6
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2024-05-27 21:19:16 +0300

    Update my_code_greedy.cpp

--- 121. Best Time to Buy and Sell Stock/my_code_greedy.cpp
+++ 121. Best Time to Buy and Sell Stock/my_code_greedy.cpp
@@ -6,7 +6,7 @@ public:
 
         // if(prices.empty()){return MaximumProfit;} // 1 <= prices.length <= 10^5
 
-        int MinPrice = prices.front();// == prices[b]
+        int MinPrice = prices.front();// == prices[b] -- или можно взять локальный максимум
 
         for(int i = 0; i < prices.size(); ++i){ // prices[i] == prices[s] 
 
