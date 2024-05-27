b50a78ce091e2f263725d854f22441559c4cd1eb
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2024-05-27 21:12:54 +0300

    Create my_code_perfect.cpp

---
+++ 121. Best Time to Buy and Sell Stock/my_code_perfect.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        
+        int MaximumProfit = 0; // == max{prices[s] - prices[b] : 0 <= b < s <= prices.size() - 1}
+
+        // if(prices.empty()){return MaximumProfit;} // 1 <= prices.length <= 10^5
+
+        int MinPrice = prices.front();// == prices[b]
+
+        for(int i = 0; i < prices.size(); ++i){ // prices[i] == prices[s] 
+
+            MinPrice = min(MinPrice, prices[i]);
+            MaximumProfit = max(MaximumProfit, prices[i] - MinPrice);
+
+        }
+
+        return MaximumProfit;
+    }
+};
