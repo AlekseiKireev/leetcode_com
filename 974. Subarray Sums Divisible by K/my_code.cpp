c17f6185fd80b0e4122e178b8a2bcfa8dc7daab6
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2024-05-27 15:23:31 +0300

    Update my_code.cpp

--- 974. Subarray Sums Divisible by K/my_code.cpp
+++ 974. Subarray Sums Divisible by K/my_code.cpp
@@ -5,7 +5,7 @@ public:
         long long CurrentModPrefixSum = 0;        
         vector<int> PrefixMod(k); // PrefixMod[t] --> count prefix with t mod k
         
-        
+        // например: [10], k = 10
         ++PrefixMod[CurrentModPrefixSum]; // если остаток равен 0, то данный аффикс подходит, это надо учесть, наеример: [5,5,5], k =5, Expected = 6 = 1+2+3
 
         int Ans = 0;
