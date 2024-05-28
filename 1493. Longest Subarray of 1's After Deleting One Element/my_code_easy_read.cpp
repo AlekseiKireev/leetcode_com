9383c18fc1002628217553e1ab530420124e9185
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2024-05-28 14:14:49 +0300

    Update my_code_easy_read.cpp

--- 1493. Longest Subarray of 1's After Deleting One Element/my_code_easy_read.cpp
+++ 1493. Longest Subarray of 1's After Deleting One Element/my_code_easy_read.cpp
@@ -1,1 +1,27 @@
+class Solution {
+public:
+    int longestSubarray(vector<int>& N, int k = 1) {
 
+  int Ans = 0;
+
+        int CountFlipZero = 0;
+
+        for(int L = 0, R = 0; R < N.size(); ++R){
+
+            if(N[R] == 0){
+                ++CountFlipZero;
+            }
+
+            for(;CountFlipZero == k + 1; ++L){
+                if(N[L] == 0){
+                    --CountFlipZero;
+                }                
+            }
+
+            Ans = max(Ans, R - L + 1);
+        }
+
+        return Ans - k; // разница только в этой строке в сравнение с 1004
+
+    }
+};
