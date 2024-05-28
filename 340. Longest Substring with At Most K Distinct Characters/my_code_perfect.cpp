94026fe931b7cc6a875d06ba7823a88eb4b73cb5
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2024-05-28 14:29:49 +0300

    Update my_code_perfect.cpp

--- 340. Longest Substring with At Most K Distinct Characters/my_code_perfect.cpp
+++ 340. Longest Substring with At Most K Distinct Characters/my_code_perfect.cpp
@@ -75,19 +75,19 @@ public:
         
         int LongestSubstring = 0;
 
-        int PowerWindow = 0;
+        int PowerUniqueCharWindow = 0;
 
         vector<int> CharToCountInWIndow(128);
 
         for(int LeftPtr = 0, RightPtr = 0; RightPtr < s.size(); ++RightPtr){
 
             if(++CharToCountInWIndow[s[RightPtr]]  == 1){
-                ++PowerWindow;
+                ++PowerUniqueCharWindow;
             }
 
-            for(;PowerWindow == k + 1; ++LeftPtr){
+            for(;PowerUniqueCharWindow == k + 1; ++LeftPtr){
                 if(--CharToCountInWIndow[s[LeftPtr]]  == 0){
-                    --PowerWindow;
+                    --PowerUniqueCharWindow;
                 }
             }
 
