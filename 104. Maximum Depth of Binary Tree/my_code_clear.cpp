d98e8d5ff7b57739db5e43d65a5ed70eec0f9cbd
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2024-05-30 16:16:24 +0300

    Update my_code_clear.cpp

--- 104. Maximum Depth of Binary Tree/my_code_clear.cpp
+++ 104. Maximum Depth of Binary Tree/my_code_clear.cpp
@@ -11,8 +11,10 @@ void travel(TreeNode* Root, int height = 1){
   MaximumDepth = max(MaximumDepth, height );
 
   travel(Root->left, height + 1);
-
+  // MaximumDepth = max(MaximumDepth, height );   // good!
   travel(Root->right, height + 1);
+
+  // MaximumDepth = max(MaximumDepth, height );   // good!
 
 }
 
