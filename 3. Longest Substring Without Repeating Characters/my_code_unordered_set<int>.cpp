90d822918bbecb1a4cf5e2e91df037de8b1149cb
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2024-06-01 00:39:28 +0300

    Update my_code_unordered_set<int>.cpp

--- 3. Longest Substring Without Repeating Characters/my_code_unordered_set<int>.cpp
+++ 3. Longest Substring Without Repeating Characters/my_code_unordered_set<int>.cpp
@@ -2,28 +2,27 @@ class Solution {
 public:
     int lengthOfLongestSubstring(string Str) {
         
-        const int SIZE = Str.size();
         int Length = 0;
 
         unordered_set<int> CharToExistingInSlidingWindow;
 
-        for (int LeftPtr = 0, RightPtr = 0; RightPtr < SIZE; ++RightPtr) {
+        for (int LeftPtr = 0, RightPtr = 0; RightPtr < Str.size(); ++RightPtr) {
 
             if (CharToExistingInSlidingWindow.count(Str[RightPtr]) == 0) {
                 
-                CharToExistingInSlidingWindow.insert(Str[RightPtr]);
                 Length = max(Length, RightPtr - LeftPtr + 1);
                 
             } else {
                 
-                while (CharToExistingInSlidingWindow.count(Str[RightPtr]) == 1) {
+                for (; CharToExistingInSlidingWindow.count(Str[RightPtr]) == 1; ++LeftPtr) {
                     CharToExistingInSlidingWindow.erase(Str[LeftPtr]);
-                    ++LeftPtr;
-                }
+                    
+                }                
                 
-                CharToExistingInSlidingWindow.insert(Str[RightPtr]);
                 // тут обновлять ответ нет смысла, так как сдвинули левую границу --> уменьшили размер окна по меньшей мере на 1
             }
+
+            CharToExistingInSlidingWindow.insert(Str[RightPtr]);
         }
 
         return Length;
