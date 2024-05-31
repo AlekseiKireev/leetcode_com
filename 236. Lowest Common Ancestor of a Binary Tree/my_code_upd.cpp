ae87146b0691e7a51a41a9fd5f723f63535c5b43
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2024-05-31 14:43:37 +0300

    Update my_code_upd.cpp

--- 236. Lowest Common Ancestor of a Binary Tree/my_code_upd.cpp
+++ 236. Lowest Common Ancestor of a Binary Tree/my_code_upd.cpp
@@ -21,7 +21,7 @@ void Travel(TreeNode* node, TreeNode* P, TreeNode* Q){
 
     // LCA == node, которая является максимальны по высоте родителем (считая сверху) как P, так и Q
     // node, которая является ребенком P or Q не может быть родителем P and Q
-    const bool NodeIsParent_P_and_Q = (!Found_P) && (!Found_Q); // Нода W рассматривается как родитель ноды W, поэтому этот аргумент находится перед нижинми if
+    const bool NodeIsParent_P_or_Q = (!Found_P) && (!Found_Q); // Нода W рассматривается как родитель ноды W, поэтому этот аргумент находится перед нижинми if
 
     if(node == P){Found_P = true;}
     if(node == Q){Found_Q = true;}
@@ -29,10 +29,10 @@ void Travel(TreeNode* node, TreeNode* P, TreeNode* Q){
     Travel(node->left,P,Q);
     Travel(node->right,P,Q);
 
-    // postorder проходит снизу-вверх, это гарантирует наименьшее (наибольшее по глубине) NodeIsParent_P_and_Q
+    // postorder проходит снизу-вверх, это гарантирует наименьшее (наибольшее по глубине) NodeIsParent_P_or_Q
     // зашли в postorder раздел и предше-е ему ноды уже помечены в preorder разделе
     if(LCA == nullptr && /*это необходимо чтобы единожды обновить LCA*/
-    NodeIsParent_P_and_Q && /*услвоие, свойственное для LCA*/
+    NodeIsParent_P_or_Q && /*услвоие, свойственное для LCA*/
     Found_P && Found_Q){LCA = node;}
 }
 
