f82d8b0690e243b280d2bfab4194ea906463b612
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2024-05-31 15:42:17 +0300

    Update my_code_two_ptr_upd.cpp

--- 167. Two Sum II - Input Array Is Sorted/my_code_two_ptr_upd.cpp
+++ 167. Two Sum II - Input Array Is Sorted/my_code_two_ptr_upd.cpp
@@ -5,7 +5,7 @@ public:
         int LeftPtr = 0;
         int RightPtr = numbers.size() - 1;
 
-        while(numbers[LeftPtr] != target - numbers[RightPtr]){
+        while(numbers[LeftPtr] != target - numbers[RightPtr]){ // "The tests are generated such that there is exactly one solution. " -- в противнмо случае пришлось бы делать ограничение на указатели
 
             (numbers[LeftPtr] < target - numbers[RightPtr]) ? ++LeftPtr : --RightPtr;
         }
