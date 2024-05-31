ab06596d10ee1861170d699c37a047188c43dffc
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2024-06-01 00:33:47 +0300

    Create my_code_unordered_set<int>.cpp

---
+++ 3. Longest Substring Without Repeating Characters/my_code_unordered_set<int>.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string Str) {
+        
+        const int SIZE = Str.size();
+        int Length = 0;
+
+        unordered_set<int> CharToExistingInSlidingWindow;
+
+        for (int LeftPtr = 0, RightPtr = 0; RightPtr < SIZE; ++RightPtr) {
+
+            if (CharToExistingInSlidingWindow.count(Str[RightPtr]) == 0) {
+                CharToExistingInSlidingWindow.insert(Str[RightPtr]);
+                Length = max(Length, RightPtr - LeftPtr + 1);
+            } else {
+                while (CharToExistingInSlidingWindow.count(Str[RightPtr]) == 1) {
+                    CharToExistingInSlidingWindow.erase(Str[LeftPtr]);
+                    ++LeftPtr;
+                }
+                CharToExistingInSlidingWindow.insert(Str[RightPtr]);
+            }
+        }
+
+        return Length;
+    }
+};
