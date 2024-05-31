fe748df66eef3e575e99ae4da4c6d3b056765b6e
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2024-06-01 00:49:32 +0300

    Update my_code_unordered_set<int>.cpp

--- 3. Longest Substring Without Repeating Characters/my_code_unordered_set<int>.cpp
+++ 3. Longest Substring Without Repeating Characters/my_code_unordered_set<int>.cpp
@@ -39,23 +39,20 @@ public:
         unordered_set<int> CharToExistingInSlidingWindow;
 
         for (int LeftPtr = 0, RightPtr = 0; RightPtr < Str.size(); ++RightPtr) {
-
-            if (CharToExistingInSlidingWindow.count(Str[RightPtr]) == 1) {
-                                                
+                                               
                 
-                for (; CharToExistingInSlidingWindow.count(Str[RightPtr]) == 1; ++LeftPtr) {
-                    CharToExistingInSlidingWindow.erase(Str[LeftPtr]);
-                    
-                }                
+            for (; CharToExistingInSlidingWindow.count(Str[RightPtr]) == 1; ++LeftPtr) {
+                CharToExistingInSlidingWindow.erase(Str[LeftPtr]);
                 
-                // тут обновлять ответ нет смысла, так как сдвинули левую границу --> уменьшили размер окна по меньшей мере на 1
-            }
-
+            }                
+                            
             Length = max(Length, RightPtr - LeftPtr + 1);
             CharToExistingInSlidingWindow.insert(Str[RightPtr]);
+
         }
 
         return Length;
+        
     }
 };
 */
