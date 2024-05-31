14d26cb9eb0344c3e4745f3814bf806383e9658e
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2024-06-01 02:15:38 +0300

    Update my_code.cpp

--- 392. Is Subsequence/my_code.cpp
+++ 392. Is Subsequence/my_code.cpp
@@ -1,27 +1,13 @@
 class Solution {
 public:
-    bool isSubsequence(string s, string t) {
-        
-        int S_Idx = 0;
-        for(int T_Idx = 0; T_Idx < t.size(); ++T_Idx){
-            
-            if(s[S_Idx]  == t[T_Idx]){++S_Idx;}
-            if(S_Idx == s.size()){return true;}
-        }
-        
-        if(S_Idx == s.size()){return true;}
-        return false;
-    }
-};
-
-
-class Solution {
-public:
     bool isSubsequence(string_view s, string_view t) {
+        
         if (s.empty()) {
             return true;
         }        
+        
         if(s.size() > t.size() || t.empty() /*!s.empty() == true ввиду прощлого if*/ ){return false;}
+        
         int S_Idx = 0;
         for(int T_Idx = 0; T_Idx < t.size(); ++T_Idx){
             
@@ -32,5 +18,3 @@ public:
         return false;
     }
 };
-
-
