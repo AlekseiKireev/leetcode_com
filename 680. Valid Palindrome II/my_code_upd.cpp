bc9fa725a5d631d9272ae787ad72fef6d8b56d98
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2024-05-31 13:46:47 +0300

    Update my_code_upd.cpp

--- 680. Valid Palindrome II/my_code_upd.cpp
+++ 680. Valid Palindrome II/my_code_upd.cpp
@@ -28,6 +28,7 @@ public:
             if(s[LeftPtr] != s[RightPtr]){
 
                 // воспользуется свойством палиндрома: если удалить с конца одинакового размера префикс и постфикс, тогда свойства палиндрома не изменются
+                // abceeba || abeecba
                 return (ChechkvalidPalindrome(s, LeftPtr + 1, RightPtr) /*удалили символ s[LeftPtr], проверяем  {...,s[LeftPtr + 1] , ..., s[RightPtr],...} на палиндром*/
                                             || 
                         ChechkvalidPalindrome(s, LeftPtr, RightPtr - 1) /*удалили символ s[RightPtr], проверяем {...,s[LeftPtr] , ..., s[RightPtr - 1],...} на палиндром*/
@@ -36,6 +37,7 @@ public:
 
         }
 
+        // сюда попадаем если ни разу не правдиво услвоие s[LeftPtr] != s[RightPtr], например: abeeba
         return true;
     }
 };
