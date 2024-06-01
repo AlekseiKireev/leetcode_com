1baf91a2cafcf16d8d68421913e065fc8016d522
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2024-06-01 14:07:03 +0300

    Create my_code_sort_TwoPtr.cpp

---
+++ 2215. Find the Difference of Two Arrays/my_code_sort_TwoPtr.cpp
@@ -0,0 +1,57 @@
+class Solution {
+
+    std::vector<int> findElementsNotInSecond(const std::vector<int>& nums1, const std::vector<int>& nums2) {
+        std::vector<int> result;
+        int i = 0, j = 0;
+
+        while (i < nums1.size() && j < nums2.size()) {
+
+            if (nums1[i] < nums2[j]) {
+
+                if(result.empty() || result.back() != nums1[i]) {
+                    result.push_back(nums1[i]);
+                }
+
+                i++;
+
+            }  else if (nums1[i] == nums2[j]) {
+
+                const int equiv = nums1[i];
+                /*
+                    Без циклов ошибка будет в тесте:
+                    [1,1,2,2]
+                    [1,2,3,3]
+                */
+                while(i < nums1.size() && nums1[i] == equiv){i++;}
+                while(j < nums2.size() && nums2[j] == equiv){j++;}
+
+            } 
+            
+            else if (nums1[i] > nums2[j]) {
+
+                j++;
+            }
+        }
+
+        // Если остались элементы в nums1, которые не были проверены
+        for (;i < nums1.size();i++) {
+            
+                if(result.empty() || result.back() != nums1[i]) {
+                    result.push_back(nums1[i]);
+                }            
+            
+        }
+
+        return result;
+    }
+
+public:
+
+    std::vector<std::vector<int>> findDifference(std::vector<int>& nums1, std::vector<int>& nums2) {
+
+        std::sort(nums1.begin(), nums1.end());
+        std::sort(nums2.begin(), nums2.end());               
+
+        return {findElementsNotInSecond(nums1, nums2), findElementsNotInSecond(nums2, nums1)};
+    }
+};
