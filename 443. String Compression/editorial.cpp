f476d8dd06aa16069afeb41d2681965580bc5a98
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2024-06-03 20:19:59 +0300

    Create editorial.cpp

---
+++ 443. String Compression/editorial.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int compress(vector<char>& chars) {
+        
+        int IdxOnCompressStr = 0;
+
+        for(int i = 0; i < chars.size(); ){
+            
+            const char CurChar = chars[i];
+            chars[IdxOnCompressStr] = CurChar;
+            ++IdxOnCompressStr;
+
+            int CountPepeat = 0;
+            for(; i < chars.size() && CurChar == chars[i]; ++i){
+                ++CountPepeat;
+            }
+
+            if(CountPepeat > 1){
+
+                for(char ch : to_string(CountPepeat)){
+
+                    chars[IdxOnCompressStr] = ch;
+                    ++IdxOnCompressStr;    
+
+                }
+            }
+
+        }
+
+        return IdxOnCompressStr; 
+    }
+};
