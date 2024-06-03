40c2e874523de65bb438bc17a2644c98115338bd
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2024-06-03 22:58:24 +0300

    Create code.cpp

---
+++ increasing and decreasing sequence/code.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <vector>
+#include <functional>
+#include <utility>
+
+using namespace std;
+
+class Solution {
+public:
+    pair<int, int> maxLengthSubarray(const vector<int>& nums, function<bool(int, int)> comp) {
+        if (nums.empty()) return {0, 0};
+
+        int maxLength = 1;
+        int currentLength = 1;
+        int maxStartIndex = 0;
+        int currentStartIndex = 0;
+
+        for (size_t i = 1; i < nums.size(); ++i) {
+            if (comp(nums[i-1], nums[i])) {
+                currentLength++;
+                if (currentLength > maxLength) {
+                    maxLength = currentLength;
+                    maxStartIndex = currentStartIndex;
+                }
+            } else {
+                currentLength = 1;
+                currentStartIndex = i;
+            }
+        }
+        return {maxStartIndex, maxLength};
+    }
+};
+
+int main() {
+    Solution solution;
+    vector<int> nums = {1, 3, 5, 4, 7};
+    
+    // For increasing sequence
+    auto incResult = solution.maxLengthSubarray(nums, less<int>());
+    cout << "Longest increasing subarray starts at index: " << incResult.first
+         << " with length: " << incResult.second << endl;
+
+    // For decreasing sequence
+    auto decResult = solution.maxLengthSubarray(nums, greater<int>());
+    cout << "Longest decreasing subarray starts at index: " << decResult.first
+         << " with length: " << decResult.second << endl;
+
+    return 0;
+}
+// подзадача без поиска индексов: 
+/*
+class Solution {
+public:
+    int maxLengthSubarray(const vector<int>& nums, function<bool(int, int)> comp) {
+        if (nums.empty()) return 0;
+
+        int maxLength = 1;
+        int currentLength = 1;
+
+        for (size_t i = 1; i < nums.size(); ++i) {
+            if (comp(nums[i-1], nums[i])) {
+                currentLength++;
+                maxLength = max(maxLength, currentLength);
+            } else {
+                currentLength = 1;
+            }
+        }
+        return maxLength;
+    }
+};
+*/
