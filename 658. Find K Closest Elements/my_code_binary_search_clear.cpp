9d671f93f7726a24ff65bda93470d9a105d60979
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2024-06-04 02:12:50 +0300

    Update my_code_binary_search_clear.cpp

--- 658. Find K Closest Elements/my_code_binary_search_clear.cpp
+++ 658. Find K Closest Elements/my_code_binary_search_clear.cpp
@@ -1,1 +1,24 @@
+class Solution {
+public:
 
+    vector<int> findClosestElements(vector<int>& arr, int k, int target) {
+
+        function<bool(int, int)> Operator = [target](const int a, const int b) {
+            return target - a <= b - target;
+        };
+
+        int left = 0;
+        int right = (int)arr.size() - k;
+
+        while (1 != right - left + 1) {
+
+            const int mid = left + (right - left) / 2;
+
+            Operator(arr[mid], arr[mid + k]) ? right = mid : left = mid + 1;
+
+        }
+
+        return {arr.begin() + left, arr.begin() + left + k};
+    }
+
+};
