40b77abad9a2ac2dff1b80efb0421bd2aa6d66eb
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2024-06-04 16:10:33 +0300

    Create my_code_vector_and_unordered_set.cpp

---
+++ 523. Continuous Subarray Sum/my_code_vector_and_unordered_set.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+
+        vector<int> PrefixSumModK; PrefixSumModK.reserve(nums.size() + 1);
+        PrefixSumModK.push_back(0);
+
+        long long CurSumModK = 0;
+        for(int num : nums){
+            CurSumModK = (CurSumModK + num % k + k) % k;
+            PrefixSumModK.push_back(CurSumModK);
+        }
+
+        unordered_set<int> Buffer;
+        for(int i = 2; i < PrefixSumModK.size(); ++i){
+
+            Buffer.insert(PrefixSumModK[i-2]); 
+
+            if(Buffer.count(PrefixSumModK[i])) {
+                return true;
+            }
+
+        }
+
+        return false;
+    }
+};
