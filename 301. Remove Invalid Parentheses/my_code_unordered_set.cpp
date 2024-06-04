4bec289e88155a2d58556d677a22bf12e0b48b60
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2024-06-04 21:53:04 +0300

    Create my_code_unordered_set.cpp

---
+++ 301. Remove Invalid Parentheses/my_code_unordered_set.cpp
@@ -0,0 +1,44 @@
+class Solution {
+
+
+unordered_set<string>buffer;
+public:
+
+    vector<string> removeInvalidParentheses(string s) {
+        
+        removeHelper(s, 0, 0, '(', ')');
+        return {buffer.begin(), buffer.end()};
+    }
+
+private:
+    void removeHelper(string s,  int iStart, int jStart, char openParen, char closedParen) {
+
+        int numOpenParen = 0, numClosedParen = 0;
+        for (int i = iStart; i < s.length(); i++) {
+
+            if (s[i] == openParen)   {numOpenParen++;}
+            if (s[i] == closedParen) {numClosedParen++;}
+
+            if (numClosedParen - 1 == numOpenParen) { 
+
+                for (int j = jStart; j <= i; j++) {
+                    
+                    if (s[j] == closedParen ) {
+                        removeHelper(s.substr(0, j) + s.substr( j + 1, s.length() ), i, j, openParen, closedParen);
+                    }
+                }
+
+                return;
+            }
+        } // numClosedParen <= numOpenParen
+
+        string reversed = s;
+        reverse(reversed.begin(), reversed.end());
+        if (openParen == '(') {
+            removeHelper(reversed, 0, 0, ')', '(');
+        } else {
+            buffer.insert(reversed);
+        }
+
+    }
+};
