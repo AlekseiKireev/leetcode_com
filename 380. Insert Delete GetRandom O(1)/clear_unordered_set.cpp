659a2c07317a0cbc82255c643d7467aa511f73a2
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2024-06-09 20:27:19 +0300

    Create clear_unordered_set.cpp

---
+++ 380. Insert Delete GetRandom O(1)/clear_unordered_set.cpp
@@ -0,0 +1,22 @@
+class RandomizedSet {
+
+    unordered_set<int> values;
+
+public:
+    
+    bool insert(int val) {
+        if (values.count(val) == 1) { return false; }
+        values.insert(val);
+        return true;
+    }
+
+    bool remove(int val) {
+        if (values.count(val) == 0) { return false; }
+        values.erase(val);
+        return true;
+    }
+
+    int getRandom() { // "There will be at least one element in the data structure when getRandom is called."
+         return *next(values.begin(),  rand() % values.size());
+    }
+};
