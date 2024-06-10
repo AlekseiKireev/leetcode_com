bacd6f208aee8f8bb4e376462950d67fec58f9f7
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2024-06-10 09:13:22 +0300

    Update my_code_vector<bool>.cpp

--- 3. Longest Substring Without Repeating Characters/my_code_vector<bool>.cpp
+++ 3. Longest Substring Without Repeating Characters/my_code_vector<bool>.cpp
@@ -1,41 +1,4 @@
 class Solution {
-
-private:
-    const int POWER_ENCODING = 256;
-
-public:
-    int lengthOfLongestSubstring(string_view Str) {
-        
-        vector<bool> CharToExistingInSlidingWindow(POWER_ENCODING, false);
-
-        int Length = 0;
-        int Right = 0; // правый указатель поднимает флаг о сущест-и символа 
-
-        // левый указатель опускает флаг о сущест-и символа, правй -- поднимаем.
-        for(int Left = 0; Left < Str.size(); ++Left){ // сдвиг левого указателя
-
-            // при сдвиге правого информация о левом автоматически обновляется, надо разве что не забыть опутсить флаг символа
-            // проверка на сдвиг правого указателя: необходимо выполнение услвоия "substring without repeating characters."
-            for(;Right < Str.size() && CharToExistingInSlidingWindow[Str[Right]] == false;++Right){  // сдвиг правого указателя
-                CharToExistingInSlidingWindow[Str[Right]] = true;  // обновляем информацию о окне после сдвига правого указателя               
-            
-            } // Str[Right] уже встречался в {Str[Left], ..., Str[Right-1]} --> длина текущей подходящйе подсткроки: Right - 1 - Left + 1
-            // Str[Left], ..., Str[Right-1]} содержит уникальные символы
-            
-            Length = max(Length, Right - Left); // "find the length of the longest substring"
-
-            assert(Left <= Right); // инваринат, который появляется при работе с думя указателями
-
-            CharToExistingInSlidingWindow[Str[Left]] = false; // левый указатель сдвинулся, убираем информацию о нем из окна
-        }
-
-        return Length;
-
-    }
-};
-
-/*
-class Solution {
 public:
     int lengthOfLongestSubstring(string Str) {
 
@@ -43,17 +6,20 @@ public:
 
         int Length = 0;
 
-        for(int LeftPtr = 0, RightPtr = 0; RightPtr < Str.size(); ++RightPtr) {
+        // для каждого сдвига правого указателя оптимально смещается левый указатель --> 
+        // достаточно одного прохода правого указателя --> RightPtr < Str.size()
+        for(int LeftPtr = 0, RightPtr = 0; RightPtr < Str.size(); ++RightPtr) { 
 
             for(; CharToExistingInSlidingWindow[Str[RightPtr]] == true; ++LeftPtr) {
                 CharToExistingInSlidingWindow[Str[LeftPtr]] = false;
-            }
+            } 
 
             CharToExistingInSlidingWindow[Str[RightPtr]] = true;
+            // Теперь выполняется ивариант, что в SW  = {Str[LeftPtr], ..., Str[RightPtr]} лишь один уникальный символ
+            // ввиду чего, обновляем ответ: 
             Length = max(Length, RightPtr - LeftPtr + 1);
         }
 
         return Length;
     }
 };
-*/
