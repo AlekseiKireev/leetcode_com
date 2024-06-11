283eb52435f6bc1c09755038439b545db80907a0
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2024-06-12 02:45:54 +0300

    Update 53a_my_UPD.cpp

--- 53. Maximum Subarray/53a_my_UPD.cpp
+++ 53. Maximum Subarray/53a_my_UPD.cpp
@@ -72,28 +72,4 @@ public:
 };
 */
 
-/*
-class Solution {
-public:
-    int maxSubArray(vector<int>& nums) {
-        
-        int maxSum = INT_MIN; 
-        int currentSum = 0;
-        
-        for(int i = 0; i < nums.size(); ++i){
 
-            currentSum += nums[i];
-
-            if(maxSum < currentSum){
-                maxSum = currentSum;
-            }
-
-            if(currentSum < 0){
-                currentSum = 0;
-            }
-        }
-
-        return maxSum;
-    }
-};
-*/
