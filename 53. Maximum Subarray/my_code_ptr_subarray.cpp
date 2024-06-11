9a3c5805c29cfb8b54bd14ae60d19c2bf6d0d245
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2024-06-12 02:07:48 +0300

    Create my_code_ptr_subarray.cpp

---
+++ 53. Maximum Subarray/my_code_ptr_subarray.cpp
@@ -0,0 +1,35 @@
+class Solution {
+
+pair<int, int> GetPtr(vector<int>& nums) {
+    int maxSum = INT_MIN;
+    int currentSum = 0;
+    int start = 0;
+    int end = 0;
+    int tempStart = 0;
+    
+    for (int i = 0; i < nums.size(); i++) {
+        currentSum += nums[i];
+        
+        if (currentSum > maxSum) {
+            maxSum = currentSum;
+            start = tempStart;
+            end = i;
+        }
+        
+        if (currentSum < 0) {
+            currentSum = 0;
+            tempStart = i + 1;
+        }
+    }
+    
+    return {start, end};
+}
+
+public:
+    int maxSubArray(vector<int>& nums) {
+        
+        auto [L, R] = GetPtr(nums);
+        return accumulate(nums.begin() + L, nums.begin() + R + 1, 0);
+        
+    }
+};
