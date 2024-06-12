617e4bc2c657aab34890f2a193f9ac68978fe3fa
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2024-06-12 03:00:45 +0300

    Update my_code_general.cpp

--- 53. Maximum Subarray/my_code_general.cpp
+++ 53. Maximum Subarray/my_code_general.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
         
-        int maxSum = INT_MIN; 
+        int maxSum = nums.front();
         int currentSum = 0;
         
         for(int i = 0; i < nums.size(); ++i){
