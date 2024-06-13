54f9d30c65cde614229850c09225ed0c8796bd03
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2024-06-13 22:56:21 +0300

    Create my_code_functor.cpp

---
+++ 42. Trapping Rain Water/my_code_functor.cpp
@@ -0,0 +1,36 @@
+class Solution {
+
+struct PreDecrement {
+    int operator()(int& x) const {
+        return --x;
+    }
+};
+
+struct PreIncrement {
+    int operator()(int& x) const {
+        return ++x;
+    }
+};
+
+int Trap = 0;
+
+void Check(int idxMaxHeight, int Ptr, auto comp, vector<int>& height){
+
+    int IdxLocalMaxHeight = 0;
+    for(; idxMaxHeight != Ptr; comp(Ptr)){
+        IdxLocalMaxHeight = max(IdxLocalMaxHeight, height[Ptr]);
+        Trap += (IdxLocalMaxHeight - height[Ptr]);
+        cout << Trap << '\n';
+    }
+}
+
+public:
+    int trap(vector<int>& height) {
+        
+        int idxMaxHeight = ranges::max_element(height) - height.begin();
+        Check(idxMaxHeight, 0, PreIncrement(), height);
+        Check(idxMaxHeight, height.size() - 1, PreDecrement(), height);
+
+        return Trap;
+    }
+};
