61bf53a44be59ac0bd5a2b738f25563aaad89d38
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2024-07-11 19:33:41 +0300

    Update my_code_two_pointer_NEW.cpp

--- 438. Find All Anagrams in a String/my_code_two_pointer_NEW.cpp
+++ 438. Find All Anagrams in a String/my_code_two_pointer_NEW.cpp
@@ -50,24 +50,24 @@ public:
         
         vector<int> CharAngrToCount(128);
         for(char ch : Angr){
-            --CharAngrToCount[ch];
+            --CharAngrToCount[ch]; // сумма отрицательных чисел по модулю будет равняться Angr.size()
         }
 
         vector<int> Idxs;
         int CountChFromAngrInSW = 0;
         for(int LeftPtr = 0, RightPtr = 0; RightPtr < Str.size(); ++RightPtr){
 
-            if(++CharAngrToCount[Str[RightPtr]]<= 0){
+            if(++CharAngrToCount[Str[RightPtr]]<= 0){ 
                 ++CountChFromAngrInSW;
             }
 
-            for(; CountChFromAngrInSW == Angr.size(); ++LeftPtr){
+            for(; CountChFromAngrInSW == Angr.size(); ++LeftPtr){ // CharAngrToCount[ch] >= 0 : ch \in Angr
 
                 if(RightPtr - LeftPtr + 1 == Angr.size()){
                     Idxs.push_back(LeftPtr);
                 }
 
-                if(--CharAngrToCount[Str[LeftPtr]] < 0){
+                if(--CharAngrToCount[Str[LeftPtr]] < 0){ 
                     --CountChFromAngrInSW;
                 }
             }
