1a6b503488a0450b6a51abc4c5e214dd56ef65a3
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2024-07-14 12:54:59 +0300

    Update my_code_CertificatePrefixSum.cpp

--- 53. Maximum Subarray/my_code_CertificatePrefixSum.cpp
+++ 53. Maximum Subarray/my_code_CertificatePrefixSum.cpp
@@ -9,6 +9,7 @@ public:
         int PrefixSum = 0; 
         int MinPrefixSum = 0; 
 
+        // если написать 0, это будет ошибка на массиве отрицательных чисел!
         int MaxAffixSum = nums.front(); // == MaxSuffixSum = sum{nums[j]} : LeftIdxMaxSuffixSum <= j <= RightIdxMaxSuffixSum
 
         int RightIdxMinPrefixSum = -1;
@@ -41,3 +42,43 @@ public:
         return MaxAffixSum; 
     }
 };
+
+/*
+class Solution {
+
+pair<int,int> findMaxSubArrayBounds(const vector<int>& nums){
+    int tempStart = 0;
+    int currentSumAffix = 0;
+    int minSumAffix = 0;
+    int maxSum = nums[0]; // если написать 0, это будет ошибка на массиве отрицательных чисел!
+    int Start = 0, End = 0;
+
+    for(int i = 0; i < nums.size(); ++i){
+        currentSumAffix += nums[i];
+
+        if(currentSumAffix - minSumAffix > maxSum){
+            maxSum = currentSumAffix - minSumAffix;
+            Start = tempStart;
+            End = i;
+        }
+
+        if(currentSumAffix < minSumAffix){
+            minSumAffix = currentSumAffix;
+            tempStart = i + 1;
+        }
+    }
+
+    return {Start, End};
+}
+
+public:
+    int maxSubArray(vector<int>& nums) {
+        if (nums.empty()) {
+            return 0; // For empty input, return 0 (or another appropriate value)
+        }
+        
+        auto [L, R] = findMaxSubArrayBounds(nums);
+        return accumulate(nums.begin() + L, nums.begin() + R + 1, 0);
+    }
+};
+*/
