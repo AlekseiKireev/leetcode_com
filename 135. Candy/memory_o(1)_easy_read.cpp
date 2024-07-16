263bb10ca7c90e3df8cb750e03944b07ae482142
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2024-07-16 10:24:06 +0300

    Update memory_o(1)_easy_read.cpp

--- 135. Candy/memory_o(1)_easy_read.cpp
+++ 135. Candy/memory_o(1)_easy_read.cpp
@@ -44,3 +44,33 @@ public:
         return candies;
     }
 };
+/*
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        
+        int candies = ratings.size();
+
+        for(int i = 1; i < ratings.size();){
+
+            int Hight = 0;
+            int Low = 0;
+            for(;i < ratings.size() && ratings[i] == ratings[i-1];++i);
+
+            for(;i < ratings.size() && ratings[i] > ratings[i-1];++i){
+                ++Hight;
+                candies += Hight;
+            }
+
+            for(;i < ratings.size() && ratings[i] < ratings[i-1];++i){
+                ++Low;
+                candies += Low;
+            }
+
+            candies -=min(Hight, Low);
+        }
+
+        return candies;
+    }
+};
+*/
