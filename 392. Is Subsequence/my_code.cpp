25f021bc838965cba28a8405d39eced3690fc205
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2024-07-27 22:41:07 +0300

    Update my_code.cpp

--- 392. Is Subsequence/my_code.cpp
+++ 392. Is Subsequence/my_code.cpp
@@ -11,7 +11,7 @@ public:
         int S_Idx = 0;
         for(int T_Idx = 0; T_Idx < t.size(); ++T_Idx){
             
-            if(s[S_Idx]  == t[T_Idx]){++S_Idx;}
+            S_Idx += (s[S_Idx]  == t[T_Idx]);
             if(S_Idx == s.size()){return true;}
         }
         
