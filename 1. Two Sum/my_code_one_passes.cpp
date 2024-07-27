311a535a0831abbfc5f1fd20de354da516996445
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2024-07-28 00:30:53 +0300

    Update my_code_one_passes.cpp

--- 1. Two Sum/my_code_one_passes.cpp
+++ 1. Two Sum/my_code_one_passes.cpp
@@ -8,12 +8,13 @@ vector<int> twoSum(const vector<int>& nums, int target) {
     for (int idx = 0; idx < nums.size(); ++idx) {
                
         const int complement = target - nums[idx];
-
+        // NumbToIdx[nums[idx]] = idx; // будет ошибка, например, на тесте: Input nums = [3,2,4] target = 6   Output [0,0] Expected [1,2]
+        
         if (NumbToIdx.count(complement)/* && NumbToIdx[complement] != idx*/) { 
             return { NumbToIdx[complement], idx };
         }
         
-        NumbToIdx[nums[idx]] = idx;
+        NumbToIdx[nums[idx]] = idx; 
 
     }
     return {};
