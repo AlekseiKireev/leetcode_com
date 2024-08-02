ec9a72b5e47d148aec20c87c456533120447466c
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2024-08-02 12:29:34 +0300

    Update my_code.cpp

--- 543. Diameter of Binary Tree/my_code.cpp
+++ 543. Diameter of Binary Tree/my_code.cpp
@@ -25,6 +25,7 @@ int PostOrder(TreeNode* node){
     
      // "The length of a path between two nodes is represented by the number of edges between them." -->
      // не надо никакго +1 / -1
+     // Input: root = [1,2,3,4,5] Output: 3 Explanation: 3 is the length of the path [4,2,1,3] or [5,2,1,3].
     Ans = max(Ans, L + R);
 
     return max(L, R) + 1;
