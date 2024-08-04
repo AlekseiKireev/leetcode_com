012291a88932e6b71f9fb3a2ca8cc8a1025d8408
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2024-08-05 00:12:18 +0300

    Create code.cpp

---
+++ 27. Remove Element/code.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        
+        int Idx = 0;
+
+        for(int i = 0; i < nums.size();++i){
+
+            if(nums[i] != val){
+                swap(nums[Idx++], nums[i]);
+            }
+        }
+
+        return Idx;
+
+    }
+};
