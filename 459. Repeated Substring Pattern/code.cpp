39a191c9209302cf932f58aedfc3298ddee69b25
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2024-08-07 14:12:54 +0300

    Create code.cpp

---
+++ 459. Repeated Substring Pattern/code.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool repeatedSubstringPattern(string s) { // is true --> s == P*k : P is str
+        
+        int size = s.size();
+        
+        string postfix = s.substr(1, size-1);
+        string prefix = s.substr(0, size-1);
+        
+        string sFold = postfix + prefix; // head + P*k' + tail : k' == (k-1)*2
+        //string sFold = prefix + postfix; // P*k^ + tail + head + P*k^ : k^ == k-1
+        return sFold.find(s) != string::npos;
+    }
+};
