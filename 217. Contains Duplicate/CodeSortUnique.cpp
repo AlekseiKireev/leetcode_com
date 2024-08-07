88305b38a6fbd5ad230f126b434e0cfcde6d8628
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2024-08-07 16:52:25 +0300

    Update CodeSortUnique.cpp

--- 217. Contains Duplicate/CodeSortUnique.cpp
+++ 217. Contains Duplicate/CodeSortUnique.cpp
@@ -7,3 +7,16 @@
           return last != nums.end();
       }
   };
+
+// error:
+/*
+  class Solution {
+  public:
+      bool containsDuplicate(vector<int>& nums) {
+          
+          ranges::sort(nums);
+          auto last = ranges::unique(nums);
+          return last != nums.end();
+      }
+  };
+*/
