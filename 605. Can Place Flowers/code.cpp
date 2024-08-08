9e01d3c2f72206f862918840c098626586196f8d
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2024-08-08 19:06:42 +0300

    Create code.cpp

---
+++ 605. Can Place Flowers/code.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        
+        if(0 == n){
+            return true;
+        }
+
+        for(int i = 0; i < flowerbed.size(); ++i){
+
+            if(flowerbed[i] == 0){
+
+                bool LeftEmpty = (i == 0 || flowerbed[i-1] == 0);
+                bool RightEmpty = (i == flowerbed.size() - 1 || flowerbed[i+1] == 0);
+
+                if(LeftEmpty && RightEmpty){
+                    --n;
+                    flowerbed[i] = 1;
+                }
+                if(0 == n){
+                    return true;
+                }
+            }
+
+        }
+
+        return n == 0;
+    }
+};
