66947d301474b8d9ba90281f2e4320a3bd8229f8
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2024-08-21 23:33:46 +0300

    Update TwoPtr.cpp

--- 209. Minimum Size Subarray Sum/TwoPtr.cpp
+++ 209. Minimum Size Subarray Sum/TwoPtr.cpp
@@ -1,1 +1,22 @@
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+        
+        
+        int MinSize = nums.size() + 1;
+        int SumBuff = 0;
+        for(int Left = 0,  Right = 0; Right < nums.size(); ++Right){
 
+            SumBuff += nums[Right];
+
+            for(; SumBuff >= target; ++Left){
+
+                MinSize = min(MinSize, Right- Left + 1);
+                SumBuff -= nums[Left];
+            }
+        }
+        
+        return MinSize == nums.size() + 1 ? 0 : MinSize;
+
+    }
+};
