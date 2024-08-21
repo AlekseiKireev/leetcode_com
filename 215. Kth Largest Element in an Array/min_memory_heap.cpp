d8ff78d2df64fd092e19c7cdc1496d2ad3266461
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2024-08-21 15:30:53 +0300

    Update min_memory_heap.cpp

--- 215. Kth Largest Element in an Array/min_memory_heap.cpp
+++ 215. Kth Largest Element in an Array/min_memory_heap.cpp
@@ -1,1 +1,24 @@
+class Solution {
 
+public:
+
+    int findKthLargest(vector<int>& nums, int k) {
+        
+        priority_queue<int, vector<int>, greater<int>> p_q;
+
+        for(int num : nums){
+
+            p_q.push(num);
+
+            if(p_q.size() > k){
+                p_q.pop();
+            }
+            
+
+        }
+
+        return p_q.top();
+
+    }
+
+};
