e56e561eb629c371c0a623e43924caca3698c535
AlKiAl <55439084+AlKiAl@users.noreply.github.com>
2024-11-13 23:37:38 +0300

    Update my_code_O(k).cpp

--- 658. Find K Closest Elements/my_code_O(k).cpp
+++ 658. Find K Closest Elements/my_code_O(k).cpp
@@ -37,7 +37,7 @@ public:
         }
 
         // Return the k closest elements
-        return vector<int>(arr.begin() + left + 1, arr.begin() + right);
+        return vector<int>(arr.begin() + left + 1, arr.begin() + right); // +1 на случай, если вышли за гранцу
     }
 };
 
