f28fc5f2fcdd430675c073cf711d52042ea977f6
AlKiAl <55439084+AlKiAl@users.noreply.github.com>
2024-11-14 23:45:56 +0300

    Update my_code_iter_next_node.cpp

--- 206. Reverse Linked List/my_code_iter_next_node.cpp
+++ 206. Reverse Linked List/my_code_iter_next_node.cpp
@@ -8,6 +8,10 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+/*
+Time complexity: O(head.size())
+Space complexity: O(1)
+*/    
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
