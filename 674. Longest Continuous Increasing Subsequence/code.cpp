226cef4dd495ec27a26848a4070f3e6cefef2758
AlKiAl <55439084+AlKiAl@users.noreply.github.com>
2024-12-04 04:30:42 +0300

    Update code.cpp

--- 674. Longest Continuous Increasing Subsequence/code.cpp
+++ 674. Longest Continuous Increasing Subsequence/code.cpp
@@ -1,1 +1,32 @@
+class Solution {
 
+    pair<int, int> maxLengthSubarray(const vector<int>& nums, function<bool(int, int)> comp) {
+        if (nums.empty()) return {-1, -1};
+
+        int maxLength = 1;
+        int currentLength = 1;
+        int maxStartIndex = 0;
+        int currentStartIndex = 0;
+
+        for (size_t i = 1; i < nums.size(); ++i) {
+            if (comp(nums[i-1], nums[i])) {
+                currentLength++;
+                if (currentLength > maxLength) {
+                    maxLength = currentLength;
+                    maxStartIndex = currentStartIndex;
+                }
+            } else {
+                currentLength = 1;
+                currentStartIndex = i;
+            }
+        }
+        cout << maxLength <<'\n';
+        return {maxStartIndex, maxLength};
+    }
+
+public:
+    int findLengthOfLCIS(vector<int>& nums) {
+        auto [maxStartIndex, maxLength] = maxLengthSubarray(nums, less<int>());
+        return maxLength;
+    }
+};
