d2e97028fab5887d2401b7c96d5aacdb1e26edda
AlKiAl <55439084+AlKiAl@users.noreply.github.com>
2024-12-07 07:44:28 +0300

    Create clear.cpp

---
+++ 1868. Product of Two Run-Length Encoded Arrays/clear.cpp
@@ -0,0 +1,29 @@
+class Solution {
+    
+ public:
+  vector<vector<int>> findRLEArray(vector<vector<int>>& encoded1, vector<vector<int>>& encoded2) {
+      
+    vector<vector<int>> ans;
+    int Idx1 = 0;
+    int Idx2 = 0;
+
+    while (Idx1 < encoded1.size() && Idx2 < encoded2.size()) {
+      const int product = encoded1[Idx1][0] * encoded2[Idx2][0];
+      const int minFrequency = min(encoded1[Idx1][1], encoded2[Idx2][1]);
+
+      if (!ans.empty() && product == ans.back()[0]) {
+        ans.back()[1] += minFrequency;
+      } else {
+        ans.push_back({product, minFrequency});
+      }
+
+      encoded1[Idx1][1] -= minFrequency;
+      encoded2[Idx2][1] -= minFrequency;
+
+      Idx1 += (encoded1[Idx1][1] == 0);
+      Idx2 += (encoded2[Idx2][1] == 0);
+    }
+
+    return ans;
+  }
+};
