23ac46f1776ad348c06eb0d76d7b414a10517a5c
AlKiAl <55439084+AlKiAl@users.noreply.github.com>
2024-12-14 23:19:01 +0300

    Create Code.cpp

---
+++ Summary Ranges Unsorted/Code.cpp
@@ -0,0 +1,43 @@
+    vector<vector<int>> compressArray(const vector<int>& arr) {
+        
+        // Преобразуем массив в множество для O(1) доступа
+        unordered_set<int> numSet(arr.begin(), arr.end());
+        vector<vector<int>> result;
+
+        for (int num : arr) {
+
+            if(numSet.empty()){
+                break;
+            }
+
+            // Если текущий элемент уже удален из множества, пропускаем
+            if (numSet.find(num) == numSet.end()) {
+                continue;
+            }
+
+            // Ищем границы последовательности
+            int left = num;
+            int right = num;
+
+            while (numSet.find(left - 1) != numSet.end()) {
+                left--;
+            }
+
+            while (numSet.find(right + 1) != numSet.end()) {
+                right++;
+            }
+
+            // Добавляем последовательность в результат
+            vector<int> range;
+            for (int i = left; i <= right; ++i) {
+
+                numSet.erase(i); // Удаляем элементы из множества
+            }
+
+            result.push_back(
+                (left == right ? vector<int>({left}) : vector<int>({left, right}) )
+            );
+        }
+
+        return result;
+    }
