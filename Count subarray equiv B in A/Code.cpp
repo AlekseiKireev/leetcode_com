5c4246ba2ac3a3c680ade1201c4ad8652309d233
AlKiAl <55439084+AlKiAl@users.noreply.github.com>
2024-12-19 00:36:35 +0300

    Create Code.cpp

---
+++ Count subarray equiv B in A/Code.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+#include <utility>
+
+using namespace std;
+
+struct PairHash {
+    
+    size_t operator()(const pair<size_t, size_t>& pair) const {
+        return pair.first ^ pair.second;
+    }
+};
+
+int countSubarraysWithSubsequence(const vector<int>& a, const vector<int>& b) {
+    int n = a.size(), m = b.size();
+    unordered_set<pair<int, int>, PairHash> unique_subarrays;
+
+    for (int l = 0; l < n; ++l) {
+        int cnt = 0; // Количество совпавших элементов из b
+        for (int r = l; r < n; ++r) {
+            if (a[r] == b[cnt]) {
+                ++cnt;
+            }
+            if (cnt == m) {
+                // Для каждой найденной правой границы r добавляем все возможные левые границы
+                for (int j = 0; j <= l; ++j) {
+                    unique_subarrays.emplace(j, r);
+                }
+
+            }
+        }
+    }
+
+    return unique_subarrays.size();
+}
+
+int main() {
+    vector<int> a = {1, 2, 3, 1, 42, 2, 3};
+    vector<int> b = {1, 2};
+    int result = countSubarraysWithSubsequence(a, b);
+    cout << "Result: " << result << endl; // Ожидаемый результат: 11
+    return 0;
+}
