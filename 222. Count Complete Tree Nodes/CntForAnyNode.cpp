74809963e9bbbb26d957aca4b7e7265cb4962448
AlKiAl <55439084+AlKiAl@users.noreply.github.com>
2024-12-18 22:54:44 +0300

    Create CntForAnyNode.cpp

---
+++ 222. Count Complete Tree Nodes/CntForAnyNode.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+
+unordered_map<TreeNode*, int> nodeCountMap;
+
+int calculateCountChildren(TreeNode* node/*, unordered_map<TreeNode*, int>& nodeCountMap*/) {
+    if (node == nullptr) {
+        return 0;
+    }
+
+    // Рекурсивно вычисляем количество вершин в левом и правом поддеревьях
+    int leftCount = calculateCountChildren(node->left/*, nodeCountMap*/);
+    int rightCount = calculateCountChildren(node->right/*, nodeCountMap*/);
+
+    // Текущее количество вершин
+    int totalCount = leftCount + rightCount + 1;
+
+    // Сохраняем результат для текущего узла
+    nodeCountMap[node] = totalCount;
+
+    return totalCount;
+}
+
+public:
+    int countNodes(TreeNode* root) {
+        calculateCountChildren(root);
+        return nodeCountMap[root];
+    }
+};
