480d17c95591a6d872ad04db925f2fda82251a7f
AlKiAl <55439084+AlKiAl@users.noreply.github.com>
2024-12-21 20:24:11 +0300

    Update Code.cpp

--- count pairs with sum eqiv target/Code.cpp
+++ count pairs with sum eqiv target/Code.cpp
@@ -4,11 +4,11 @@
 
 using namespace std;
 
-int countPairsWithSum(const vector<int>& a, int target) {
+int countPairsWithSum(const vector<int>& nums, int target) {
     unordered_map<int, int> NumToCount; // Хранит количество вхождений чисел
     int count = 0;
 
-    for (int num : a) {
+    for (int num : nums) {
         int complement = target - num; // Вычисляем дополнение
 
         // Если дополнение уже есть в хеш-таблице, увеличиваем счетчик пар
@@ -24,9 +24,9 @@ int countPairsWithSum(const vector<int>& a, int target) {
 }
 
 int main() {
-    vector<int> a = {1, -1, 2, -2, 3, -3, 4, -4};
+    vector<int> nums = {1, -1, 2, -2, 3, -3, 4, -4};
     int target = 0;
-    int result = countPairsWithSum(a, target);
+    int result = countPairsWithSum(nums, target);
     cout << "Количество пар с суммой " << target << ": " << result << endl;
     return 0;
 }
