a4869076d8d9b760f8bc4ece11eb3e27e25a9d11
AlKiAl <55439084+AlKiAl@users.noreply.github.com>
2024-12-25 18:03:44 +0300

    Update OnePass.cpp

--- 1679. Max Number of K-Sum Pairs/OnePass.cpp
+++ 1679. Max Number of K-Sum Pairs/OnePass.cpp
@@ -1,1 +1,23 @@
+class Solution {
+public:
+    int maxOperations(std::vector<int>& nums, int k) {
+        
+        unordered_map<int, int> NumToCount;
+        int count = 0;
 
+        for (int i = 0; i < nums.size(); ++i) {
+            int Complement = k - nums[i];
+
+            if (NumToCount[Complement] > 0) {
+
+                count++;
+                NumToCount[Complement]--;
+                
+            } else {
+                NumToCount[nums[i]]++;
+            }
+        }
+
+        return count;
+    }
+};
