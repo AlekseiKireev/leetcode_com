20772c1ab8f84b6b36fd836d79cc2fba82da88ae
AlKiAl <55439084+AlKiAl@users.noreply.github.com>
2024-12-26 05:04:45 +0300

    Update SW.cpp

--- 395. Longest Substring with At Least K Repeating Characters/SW.cpp
+++ 395. Longest Substring with At Least K Repeating Characters/SW.cpp
@@ -5,26 +5,33 @@ public:
         int maxUniqueCharacters = unordered_set<char>  {str.begin(), str.end()}.size();
         int longest = 0;
 
-
-        for (int targetUnique = 1; targetUnique <= maxUniqueCharacters; ++targetUnique) {
+        /*
+        Подстрока является допустимой, если каждый символ имеет частоту не менее k. 
+        Основная идея заключается в том, чтобы найти все допустимые подстроки 
+        с разным количеством уникальных символов и отследить максимальную длину. 
+        */
+        // Длина искомой подстроки будет содержать не более maxUniqueCharacters уник. символов
+        // --> достаточно проверить частоты, содержащие не мене k уникальных символов 
+        // из диапозона [1, maxUniqueCharacters] 
+        for (int targetUniq = 1; targetUniq <= maxUniqueCharacters; ++targetUniq) {
             vector<int> charFrequency(128, 0);            
             
-            int currentUnique = 0;
-            int atLeastKCount = 0; // количество не менее K
+            int CntCurUnique = 0;
+            int CntLetHavFreqSizeK = 0; // количество не менее K
 
             for (int left = 0, right = 0; right < str.size(); ) {
 
                 // expand the sliding window
-                if (currentUnique <= targetUnique) {
+                if (CntCurUnique <= targetUniq) {
 
                     char rightChar = str[right];
                     if (charFrequency[rightChar] == 0) {
-                        ++currentUnique;
+                        ++CntCurUnique;
                     }
                     ++charFrequency[rightChar];
 
                     if (charFrequency[rightChar] == k) {
-                        ++atLeastKCount;
+                        ++CntLetHavFreqSizeK;
                     }
                     ++right;
 
@@ -33,18 +40,20 @@ public:
 
                     char leftChar = str[left];
                     if (charFrequency[leftChar] == k) {
-                        --atLeastKCount;
+                        --CntLetHavFreqSizeK;
                     }
                     --charFrequency[leftChar];
 
                     if (charFrequency[leftChar] == 0) {
-                        --currentUnique;
+                        --CntCurUnique;
                     }
                     
                     ++left;
                 }
 
-                if (/*currentUnique == targetUnique && */currentUnique == atLeastKCount) {
+                // Окно состоящее из CntCurUnique уник. симво. которое содержит последовательность
+                // как минимум из CntLetHavFreqSizeK повторя-я символов
+                if (/*CntCurUnique == targetUniq && */CntCurUnique == CntLetHavFreqSizeK) {
                     longest = max(longest, right - left);
                 }
                 
