187ea41dff3074cf4d90e718470a6e70e6c073a3
AlKiAl <55439084+AlKiAl@users.noreply.github.com>
2025-01-11 23:55:12 +0300

    Update code.cpp

--- 1329. Sort the Matrix Diagonally/code.cpp
+++ 1329. Sort the Matrix Diagonally/code.cpp
@@ -4,18 +4,18 @@ class Solution {
     const int m = mat.size();
     const int n = mat[0].size();
 
-    unordered_map<int, priority_queue<int, vector<int>, greater<int>>> count;
+    unordered_map<int, priority_queue<int, vector<int>, greater<int>>> IdxDiagMatrixToVal;
 
     for (int i = 0; i < m; ++i){
       for (int j = 0; j < n; ++j) {
-        count[i - j].push(mat[i][j]);
+        IdxDiagMatrixToVal[i - j].push(mat[i][j]);
       }
     }
 
     for (int i = 0; i < m; ++i){
       for (int j = 0; j < n; ++j) {
-        mat[i][j] = count[i - j].top(); 
-        count[i - j].pop();
+        mat[i][j] = IdxDiagMatrixToVal[i - j].top(); 
+        IdxDiagMatrixToVal[i - j].pop();
       }
     }
     return mat;
