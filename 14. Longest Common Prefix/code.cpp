a3a83c397e6873cd90dc5d6352cd7a1c4f79e41a
AlKiAl <55439084+AlKiAl@users.noreply.github.com>
2025-01-13 03:08:19 +0300

    Update code.cpp

--- 14. Longest Common Prefix/code.cpp
+++ 14. Longest Common Prefix/code.cpp
@@ -4,11 +4,11 @@ public:
         
         if (strs.empty()) {return "";}
 
-        for (int i = 0; i < strs.front().size(); i++) {
+        for (int i = 0; i < strs.front().size(); i++) { // итерируемся по первой строке
 
             char CurCh = strs.front()[i];
 
-            for (int j = 1; j < strs.size(); j++) {
+            for (int j = 1; j < strs.size(); j++) { // итерируемся по остальным строкам массива, помимо 1 строки
                 if (
                 i == strs[j].size() /*strs.front().size() -- макисмальная длина ответа*/
                 || 
