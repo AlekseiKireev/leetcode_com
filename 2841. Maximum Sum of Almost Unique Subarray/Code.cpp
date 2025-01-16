a6118112d2c5993c45b7f2e1759e4d227b53f98b
AlKiAl <55439084+AlKiAl@users.noreply.github.com>
2025-01-17 01:47:08 +0300

    Update Code.cpp

--- 2841. Maximum Sum of Almost Unique Subarray/Code.cpp
+++ 2841. Maximum Sum of Almost Unique Subarray/Code.cpp
@@ -2,30 +2,30 @@ class Solution {
 public:
     long long maxSum(vector<int>& nums, int m, int k) {
 
-        long long Uniq = 0, MaxPrefsum = 0, AffixSum = 0;
-        unordered_map<int, int> NumToCount;
+        long long CntUniqInWindow = 0, MaxAffixSum = 0, AffixSum = 0;
+        unordered_map<int, int> AffixSumToCount;
 
         for(int Right = 0, Left = 0; Right < nums.size(); ++Right){
 
             AffixSum += nums[Right];
 
-            if(++NumToCount[nums[Right]] == 1) { // подсчет уникальных элементов
-                Uniq++;
+            if(++AffixSumToCount[nums[Right]] == 1) { // подсчет уникальных элементов
+                CntUniqInWindow++;
             }
 
             if(Right >= k){ // формирование окна длины k
 
                 AffixSum -= nums[Left];
 
-                if(--NumToCount[nums[Left]] == 0) {Uniq--;}
+                if(--AffixSumToCount[nums[Left]] == 0) {CntUniqInWindow--;}
 
                 Left++;
             }
 
-            if(Uniq >= m && Right - Left + 1 == k) { // формирование ответа
-                MaxPrefsum = max(MaxPrefsum, AffixSum);
+            if(CntUniqInWindow >= m && Right - Left + 1 == k) { // формирование ответа
+                MaxAffixSum = max(MaxAffixSum, AffixSum);
             }
         }
-        return MaxPrefsum;
+        return MaxAffixSum;
     }
 };
