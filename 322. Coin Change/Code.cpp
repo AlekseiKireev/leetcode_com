676a879a46425eed17c51ca79e5cf9eeb410f56d
AlKiAl <55439084+AlKiAl@users.noreply.github.com>
2025-01-20 21:57:25 +0300

    Update Code.cpp

--- 322. Coin Change/Code.cpp
+++ 322. Coin Change/Code.cpp
@@ -2,12 +2,14 @@ class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
         
-
+        while(!coins.empty() && amount < coins.back()){
+            coins.pop_back();
+        }
         vector<int> DP(amount + 1, amount + 1); // idx --> val == SumCoins --> FewestNum
         DP[0] = 0; // база DP
 
         for(int coin : coins){
-            for(int SumCoins = coin; SumCoins < amount + 1; ++SumCoins){
+            for(int SumCoins = coin; SumCoins <= amount; ++SumCoins){ // смотрим как можно сформировать иные суммы с монетой coin. Если нельзя, то DP[SumCoins] = amount + 1
                 DP[SumCoins] = min(DP[SumCoins], DP[SumCoins - coin] + 1);
             }            
         }
