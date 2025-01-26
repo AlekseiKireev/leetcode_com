56498881a117f00b20ffcd5664a2513ebb976646
AlKiAl <55439084+AlKiAl@users.noreply.github.com>
2025-01-26 05:55:33 +0300

    Update my_code.cpp

--- 771. Jewels and Stones/my_code.cpp
+++ 771. Jewels and Stones/my_code.cpp
@@ -4,15 +4,15 @@ public:
 
         // если не помнишь про 'z' - 'A' + 1 можно использовать array от мощности кодировки, в ascii это 128
         // 'z' - 'A' + 1 <-- https://en.cppreference.com/w/cpp/language/ascii <-- https://www.utf8-chartable.de/
-        vector<bool> JewelToCount('z' - 'A' + 1, false); // https://en.cppreference.com/w/cpp/container/vector_bool
+        vector<bool> JewelToExist('z' - 'A' + 1, false); // https://en.cppreference.com/w/cpp/container/vector_bool
         for(const char jewel : jewels){ // All the characters of jewels are unique.
-            JewelToCount['z' - jewel] = true;
+            JewelToExist['z' - jewel] = true;
         }
         
         int CountStones = 0;
 
         for(const char stone : stones){
-            CountStones += JewelToCount['z' - stone];
+            CountStones += JewelToExist['z' - stone];
         }
         
         return CountStones;
