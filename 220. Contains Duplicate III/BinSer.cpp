7155020749124639813a920909e70f337eaabc3e
AlKiAl <55439084+AlKiAl@users.noreply.github.com>
2025-01-29 08:45:36 +0300

    Update BinSer.cpp

--- 220. Contains Duplicate III/BinSer.cpp
+++ 220. Contains Duplicate III/BinSer.cpp
@@ -2,27 +2,20 @@ class Solution {
  public:
   bool containsNearbyAlmostDuplicate(vector<int>& nums, int indexDiff, int valueDiff) {
 
-    set<int> window;
+    multiset<int> window;
 
     for (int i = 0; i < nums.size(); ++i) {
-
-        auto it =  window.lower_bound(nums[i] - valueDiff); // *it >= nums[i] - valueDiff
-        if (
-         it != window.end() 
-         && 
-         *it - nums[i] <= valueDiff /* Check: abs(nums[i] - nums[j]) <= valueDiff*/
-        ){
+      auto it = window.lower_bound(nums[i] - valueDiff); // *it >= nums[i] - valueDiff
+      if (it != window.end() && *it - nums[i] <= valueDiff) { /* Check: abs(nums[i] - nums[j]) <= valueDiff*/
         return true;
-        }
+      }
 
-        window.insert(nums[i]);
-
-        /* Check: abs(i - j) <= indexDiff */
-        if (window.size() > indexDiff){ // на каждой итерации гаранти-о удаляется элемент вне окна
-           window.erase(nums[i - indexDiff]);
-        }
-
-
+      window.insert(nums[i]);
+    
+      /* Check: abs(i - j) <= indexDiff */
+      if (window.size() > indexDiff) { // на каждой итерации гаранти-о удаляется элемент вне окна
+        window.erase(window.find(nums[i - indexDiff])); // Удаляем только ОДНО вхождение!
+      }
     }
 
     return false;
