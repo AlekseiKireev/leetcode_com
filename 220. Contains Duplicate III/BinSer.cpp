f2d99ab1fb515bfb027bd5717640897f54ab73f6
AlKiAl <55439084+AlKiAl@users.noreply.github.com>
2025-01-29 08:32:37 +0300

    Update BinSer.cpp

--- 220. Contains Duplicate III/BinSer.cpp
+++ 220. Contains Duplicate III/BinSer.cpp
@@ -17,9 +17,8 @@ class Solution {
 
         window.insert(nums[i]);
 
-        /* Check: abs(i - j) <= indexDiff <--> [i-indexDiff + 1, i] -- окно, в котором*/
-        /* должно выполняться условие abs(nums[i] - nums[j]) <= valueDiff */
-        if (i >= indexDiff){ // на каждой итерации гаранти-о удаляется элемент вне окна
+        /* Check: abs(i - j) <= indexDiff */
+        if (window.size() > indexDiff){ // на каждой итерации гаранти-о удаляется элемент вне окна
            window.erase(nums[i - indexDiff]);
         }
 
