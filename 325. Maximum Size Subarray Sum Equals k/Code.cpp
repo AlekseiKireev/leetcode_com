bf6010c694822ee417b7fff3b5afeff6e1cfa9f1
AlKiAl <55439084+AlKiAl@users.noreply.github.com>
2025-02-02 08:52:43 +0300

    Update Code.cpp

--- 325. Maximum Size Subarray Sum Equals k/Code.cpp
+++ 325. Maximum Size Subarray Sum Equals k/Code.cpp
@@ -3,7 +3,7 @@ class Solution {
   int maxSubArrayLen(vector<int>& nums, int k) {
     int ans = 0;
     int prefix = 0;
-    unordered_map<int, int> prefixToIndex{{0, 0}};
+    unordered_map<int, int> PSumToIdxPSum{{0, 0}};
 
     for (int i = 0; i < nums.size(); ++i) {
         
@@ -11,16 +11,16 @@ class Solution {
     
     // обновление ответа
       const int target = prefix - k;
-      if (prefixToIndex.count(target) == 1){
+      if (PSumToIdxPSum.count(target) == 1){
         // "+1" обусловлен тем, что индекс префиксной суммы на +1 больше
-        ans = max(ans, i + 1 - prefixToIndex[target]);
+        ans = max(ans, i + 1 - PSumToIdxPSum[target]);
       }
 
         // обновляем только один раз, чтобы зафиксировать левую границу -->         
         // более длинное окно, т.к. Right-Left --> max if Left --> min
-      if (prefixToIndex.count(prefix) == 0){ 
+      if (PSumToIdxPSum.count(prefix) == 0){ 
          // "+1" обусловлен тем, что индекс префиксной суммы на +1 больше
-        prefixToIndex[prefix] = i + 1;  
+        PSumToIdxPSum[prefix] = i + 1;  
       }
         
     }
