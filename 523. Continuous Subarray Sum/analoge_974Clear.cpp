839d1c9be1cca6442bb5ba965c3f60a3ac920432
AlKiAl <55439084+AlKiAl@users.noreply.github.com>
2025-02-02 12:16:46 +0300

    Update analoge_974Clear.cpp

--- 523. Continuous Subarray Sum/analoge_974Clear.cpp
+++ 523. Continuous Subarray Sum/analoge_974Clear.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
   bool checkSubarraySum(const vector<int>& a, int k) { 
 
-     unordered_set<int> seen; 
+     unordered_set<int> ExistPS; 
      long long CurrentSum = 0;
      for (int v : a) {
        int prevSum = CurrentSum; 
        
        CurrentSum = (CurrentSum + k + v % k) % k; 
-       if (seen.count(CurrentSum)) {return true;}
-       seen.insert(prevSum);
+       if (ExistPS.count(CurrentSum)) {return true;}
+       ExistPS.insert(prevSum);
        
      }
      
