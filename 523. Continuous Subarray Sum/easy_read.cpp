f2436fa379b9eff21ed268aa4383cc795e518a70
AlKiAl <55439084+AlKiAl@users.noreply.github.com>
2025-02-02 08:57:09 +0300

    Update easy_read.cpp

--- 523. Continuous Subarray Sum/easy_read.cpp
+++ 523. Continuous Subarray Sum/easy_read.cpp
@@ -2,18 +2,18 @@ class Solution {
 public:
   bool checkSubarraySum(const vector<int>& a, int k) { 
 
-     unordered_set<int> seen; // == {PS[0], ..., PS[i - 2]}
+     unordered_set<int> ExistPS; // == {PS[0], ..., PS[i - 2]}
      int CurrentSum = 0;
      for (int v : a) {
        int prevSum = CurrentSum; // == PS[i-1]
        CurrentSum += v; // == PS[i]
        CurrentSum %= k; 
        
-        // на момент проверки существования sum в seen, в seen еще не лежит prevSum и CurrentSum --> 
-        //  на момент проверки sum[i] в seen = {sum[0], ..., sum[i - 2]} : i >= 2
-       if (seen.count(CurrentSum)) {return true;}
+        // на момент проверки существования sum в ExistPS, в ExistPS еще не лежит prevSum и CurrentSum --> 
+        //  на момент проверки sum[i] в ExistPS = {sum[0], ..., sum[i - 2]} : i >= 2
+       if (ExistPS.count(CurrentSum)) {return true;}
 
-       seen. insert(prevSum);
+       ExistPS. insert(prevSum);
      }
      
      return false;
