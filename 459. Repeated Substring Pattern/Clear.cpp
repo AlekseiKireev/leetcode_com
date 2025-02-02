cb590144767165b2d9813d38286662c81ccf91cb
AlKiAl <55439084+AlKiAl@users.noreply.github.com>
2025-02-02 15:53:01 +0300

    Update Clear.cpp

--- 459. Repeated Substring Pattern/Clear.cpp
+++ 459. Repeated Substring Pattern/Clear.cpp
@@ -3,6 +3,7 @@ public:
     bool repeatedSubstringPattern(string s) {
         int n = s.size();
         string doubled = s + s;
+        // Функция substr(1, 2 * n - 2) берет подстроку длиной 2 * n - 2, начиная с индекса 1
         return doubled.substr(1, 2 * n - 2).find(s) != string::npos;
     }
 };
