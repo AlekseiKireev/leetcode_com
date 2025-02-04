4a8484c5af70caac3a2cdadb64c6469b1c63bd30
AlKiAl <55439084+AlKiAl@users.noreply.github.com>
2025-02-04 08:51:40 +0300

    Update n.cpp

--- 274. H-Index/n.cpp
+++ 274. H-Index/n.cpp
@@ -1,20 +1,26 @@
+/*
+Найти: 
+Индекс хирша определяется как максимальное значение h, 
+при котором данный исследователь опубликовал не менее h статей, 
+каждая из которых цитировалась не менее h раз.
+*/
 class Solution {
 public:
 
     int hIndex(vector<int>& citations) {
-        int n = citations.size();
+        int n = citations.size(); // количество статей
+        // "n + 1" --> Максимальное значение h-index не может превышать n (количество статей).
         vector<int> buckets(n + 1, 0); // buckets[i] отображает в количество статей, которые цитировались >= i раз
         
         for(int c : citations) {
-            if(c >= n) {
-                buckets[n]++;
-            } else {
-                buckets[c]++;
-            }
+
+            buckets[min(n,c)]++;
+
         }
         
         int count = 0;
-        for(int i = n; i >= 0; i--) {
+        // накопление количество статей, которые имеют не менее i цитирований. 
+        for(int i = n; i >= 0; i--) { 
             count += buckets[i];
             if(count >= i) {
                 return i;
