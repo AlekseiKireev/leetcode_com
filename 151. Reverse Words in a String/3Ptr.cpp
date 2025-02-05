8eb331337c1a8ccbd5a8518e79b70817615251ed
AlKiAl <55439084+AlKiAl@users.noreply.github.com>
2025-02-05 06:30:53 +0300

    Create 3Ptr.cpp

---
+++ 151. Reverse Words in a String/3Ptr.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+
+        reverse(s.begin(), s.end()); // изменяем порядок слов
+        int n = s.size();
+        int left = 0;
+        int right = 0;
+        
+        for (int i = 0; i < n; i++) {
+
+            for (;i < n && s[i] == ' ';i++); // пропускаем все пробелы
+
+            if (i == n) {break;}
+
+            // находим левую и правую границу слову
+            while (i < n && s[i] != ' ') {
+                s[right++] = s[i++];
+            } // right указывает на пробел
+
+            reverse(s.begin() + left, s.begin() + right); // разворачиваем слово
+            s[right++] = ' '; // добавляем пробел
+            left = right; // left указывает на пробел
+            
+        }
+
+        s.resize(right - 1);
+        return s;
+    }
+};
