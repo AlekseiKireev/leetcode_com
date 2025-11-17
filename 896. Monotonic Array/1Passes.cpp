c674332b705903b3f21e4928114cb2e370d9d348
AlekseiKireev <55439084+AlekseiKireev@users.noreply.github.com>
2025-11-17 19:36:50 +0300

    Update 1Passes.cpp

--- 896. Monotonic Array/1Passes.cpp
+++ 896. Monotonic Array/1Passes.cpp
@@ -7,6 +7,7 @@ class Solution {
     for (int i = 1; i < nums.size(); ++i) {
       increasing &= nums[i] >= nums[i - 1]; // Если есть убывание, то возрастающая монотонность нарушена
       decreasing &= nums[i] <= nums[i - 1]; // Если есть возрастание, то убывающая монотонность нарушена
+     // добавить выход, когда оба условия не выполняются
     }
 
     return increasing || decreasing;
