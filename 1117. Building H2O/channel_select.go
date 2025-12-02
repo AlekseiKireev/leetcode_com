9b05a184414e978b47edca6350152bd4359a92c5
AlekseiKireev <55439084+AlekseiKireev@users.noreply.github.com>
2025-12-02 19:48:29 +0300

    Create channel_select.go

---
+++ 1117. Building H2O/channel_select.go
@@ -0,0 +1,43 @@
+package main
+
+type H2O struct {
+	h1ch chan struct{} // первый H
+	h2ch chan struct{} // второй H
+	och  chan struct{} // O
+}
+
+func NewH2O() *H2O {
+	h := &H2O{
+		h1ch: make(chan struct{}, 1),
+		h2ch: make(chan struct{}, 1),
+		och:  make(chan struct{}, 1),
+	}
+
+	// запускаем цикл с ожидания первого H
+	h.h1ch <- struct{}{}
+
+	return h
+}
+
+func (h2o *H2O) Hydrogen(releaseHydrogen func()) {
+	select {
+	case <-h2o.h1ch:
+		// после первого H разрешаем второй H
+		h2o.h2ch <- struct{}{}
+	case <-h2o.h2ch:
+		// после второго H разрешаем кислород
+		h2o.och <- struct{}{}
+	}
+
+	releaseHydrogen()
+}
+
+func (h2o *H2O) Oxygen(releaseOxygen func()) {
+	// O можно выпустить только после двух H
+	<-h2o.och
+
+	// запускаем всё по кругу — снова ждём первый H
+	h2o.h1ch <- struct{}{}
+
+	releaseOxygen()
+}
