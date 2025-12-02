5bfba8a629da579e6bc3e298c678122a5cfcd115
AlekseiKireev <55439084+AlekseiKireev@users.noreply.github.com>
2025-12-02 14:55:32 +0300

    Create channel_loop.go

---
+++ 1116. Print Zero Even Odd/channel_loop.go
@@ -0,0 +1,47 @@
+type ZeroEvenOdd struct {
+    n      int
+    zeroCh chan struct{}
+    oddCh  chan int
+    evenCh chan int
+}
+
+func NewZeroEvenOdd(n int) *ZeroEvenOdd {
+    return &ZeroEvenOdd{
+        n:      n,
+        zeroCh: make(chan struct{}),
+        oddCh:  make(chan int),
+        evenCh: make(chan int),
+    }
+}
+
+// printZero outputs 0
+func (z *ZeroEvenOdd) Zero(printZero func(int)) {
+    for i := 1; i <= z.n; i++ {
+        printZero(0)
+
+        if i%2 == 1 {
+            z.oddCh <- i
+        } else {
+            z.evenCh <- i
+        }
+
+        <-z.zeroCh
+    }
+
+    close(z.oddCh)
+    close(z.evenCh)
+}
+
+func (z *ZeroEvenOdd) Even(printEven func(int)) {
+    for x := range z.evenCh {
+        printEven(x)
+        z.zeroCh <- struct{}{}
+    }
+}
+
+func (z *ZeroEvenOdd) Odd(printOdd func(int)) {
+    for x := range z.oddCh {
+        printOdd(x)
+        z.zeroCh <- struct{}{}
+    }
+}
