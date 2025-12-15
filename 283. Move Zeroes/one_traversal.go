3f2561d96cba08b2e9c3ca6439b0bce8543c2509
AlekseiKireev <55439084+AlekseiKireev@users.noreply.github.com>
2025-12-15 22:50:05 +0300

    Create one_traversal.go

---
+++ 283. Move Zeroes/one_traversal.go
@@ -0,0 +1,15 @@
+func moveZeroes(nums []int) {
+	Val := 0 
+	PtrOnSwapFirstVal := 0 // == Count(NotVal)
+    // в любом префиксе действует инвариант: Count(Val) + Count(NotVal) == i
+    // при Count(Val) != 0 между PtrOnSwapFirstVal и i возникает окно из Val
+    // длиною Count(Val), при это PtrOnSwapFirstVal всегда указывет на первый
+    // элемент этого окна
+	for i := 0; i < len(nums); i++ {
+		if nums[i] != Val {            
+			nums[i], nums[PtrOnSwapFirstVal] = nums[PtrOnSwapFirstVal], nums[i]
+            PtrOnSwapFirstVal++
+			
+		}
+	}
+}
