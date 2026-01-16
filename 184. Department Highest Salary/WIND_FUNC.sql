00eec7183f335003372b8b9618d6c8ea1a2193c1
AlekseiKireev <55439084+AlekseiKireev@users.noreply.github.com>
2026-01-16 18:05:51 +0300

    Update WIND_FUNC.sql

--- 184. Department Highest Salary/WIND_FUNC.sql
+++ 184. Department Highest Salary/WIND_FUNC.sql
@@ -1,1 +1,15 @@
+WITH Salary_rnk AS (
+    SELECT
+        D.name AS Department, 
+        E.name AS Employee, 
+        E.salary AS Salary,
+        DENSE_RANK() OVER (
+            PARTITION BY D.name
+            ORDER BY E.salary DESC
+        ) AS rnk
+    FROM Employee AS E INNER JOIN Department AS D ON E.departmentId = D.id
+)
 
+SELECT Department, Employee, Salary
+FROM Salary_rnk
+WHERE rnk = 1
