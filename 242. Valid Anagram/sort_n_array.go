e88b0cea0d2b1a081641f1a0e83bd73f9d81c38d
AlekseiKireev <55439084+AlekseiKireev@users.noreply.github.com>
2026-01-19 19:20:59 +0300

    Create sort_n_array.go

---
+++ 242. Valid Anagram/sort_n_array.go
@@ -0,0 +1,20 @@
+func isAnagram(s string, t string) bool {
+	if len(s) != len(t) {
+		return false
+	}
+
+    // dataRepeatChar := [128]rune{}
+    var dataRepeatChar [128]rune 
+	for i := 0; i < len(s); i++ {
+        dataRepeatChar[s[i]] +=1
+        dataRepeatChar[t[i]] -=1
+	}
+
+    for _, v := range dataRepeatChar{
+	if v != 0 {
+		return false
+	}        
+    }
+
+    return true
+}
