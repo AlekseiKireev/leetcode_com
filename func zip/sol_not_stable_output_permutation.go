c6dabcd8d83aa70a9c7ade16ad1244facc16c194
AlexKir102099 <55439084+AlexKir102099@users.noreply.github.com>
2026-02-01 19:24:44 +0300

    Create sol_not_stable_output_permutation.go

---
+++ func zip/sol_not_stable_output_permutation.go
@@ -0,0 +1,32 @@
+// You can edit this code!
+// Click here and start typing.
+package main
+
+import "fmt"
+
+func main() {
+	s1, s2 := []int{1, 2, 3}, []int{4, 5, 6, 7, 8}
+	fmt.Println(zip(s1, s2)) // [[1 4] [2 5] [3 6]]
+	fmt.Println(zip(s2, s1)) // [[4 1] [5 2] [6 3]]
+}
+func zip(s1 []int, s2 []int) [][]int {
+	minLen := len(s1)
+	if len(s2) < minLen {
+		minLen = len(s2)
+	}
+
+	// так делать не стоит: доп. затраты на заполнение значениями по умолчанию
+	// ввиду указания длины
+	/*
+		ans := make([][]int, minLen, minLen)
+		for i := 0; i < minLen; i++ {
+			ans[i] = []int{s1[i], s2[i]}
+		}
+	*/
+	ans := make([][]int, 0, minLen)
+	for i := 0; i < minLen; i++ {
+		ans = append(ans, []int{s1[i], s2[i]})
+	}
+	return ans
+
+}
