3b051a1cad00bf4217612ebfa29914f6713ffffc
AlexKir102099 <55439084+AlexKir102099@users.noreply.github.com>
2026-02-02 19:28:39 +0300

    Update my_code_one_passes.go

--- 1. Two Sum/my_code_one_passes.go
+++ 1. Two Sum/my_code_one_passes.go
@@ -6,8 +6,8 @@ func twoSum(nums []int, target int) []int {
 								
 		// NumToIdx[num] = i // nums = [3,2,4] target = 6 Output [0,0] Expected [1,2]
 							 // Input nums = [3,3] target = 6 Expected [0,1]
-		if complement_idx, ok := NumToIdx[target-num]; ok {
-			return []int{complement_idx, i}
+		if complementIdx, ok := NumToIdx[target-num]; ok { // num == nums[i], target = nums[i] + nums[complementIdx]
+			return []int{complementIdx, i}
 		}
 
 		NumToIdx[num] = i
