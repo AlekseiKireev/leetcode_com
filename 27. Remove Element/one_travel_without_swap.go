8315d8c9315387ba5669bd75438f4bbbf8047677
AlexKir102099 <55439084+AlexKir102099@users.noreply.github.com>
2026-02-02 21:28:48 +0300

    Update one_travel_without_swap.go

--- 27. Remove Element/one_travel_without_swap.go
+++ 27. Remove Element/one_travel_without_swap.go
@@ -1,7 +1,7 @@
 func removeElement(nums []int, val int) (PtrOnFirstVal int) {
 
     for i := 0; i < len(nums); i++{
-        if nums[i] != val {
+        if nums[i] != val { // != -->  [1,2,3,4]
             // nums[updLen++] = nums[i] // syntax error: unexpected ++, expected comma, : or ]
             nums[PtrOnFirstVal] = nums[i]
             PtrOnFirstVal++
